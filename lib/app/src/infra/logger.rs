f384f465df92194dfd76cca93679f44b8c383895
Yota Toyama <raviqqe@gmail.com>
2021-06-17 04:36:32 +0000

    Remove logger

--- lib/app/src/infra/logger.rs
+++
@@ -1,3 +0,0 @@
-pub trait Logger {
-    fn log(&self, log: &str) -> Result<(), Box<dyn std::error::Error>>;
-}
--- lib/app/src/infra/mod.rs
+++ lib/app/src/infra/mod.rs
@@ -1,9 +1,7 @@
 mod file_path;
 mod file_path_displayer;
 mod file_system;
-mod logger;
 
 pub use file_path::*;
 pub use file_path_displayer::*;
 pub use file_system::*;
-pub use logger::*;
