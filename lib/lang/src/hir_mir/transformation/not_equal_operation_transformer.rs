65dd4dca8e02ae07d0134c53697d6173cb32470a
Yota Toyama <raviqqe@gmail.com>
2021-06-25 19:08:09 +0000

    Transform not-equal operation

--- lib/lang/src/hir_mir/transformation/not_equal_operation_transformer.rs
+++ lib/lang/src/hir_mir/transformation/not_equal_operation_transformer.rs
@@ -2,20 +2,21 @@ use crate::hir::*;
 
 pub fn transform(operation: &EqualityOperation) -> Expression {
     if operation.operator() == EqualityOperator::NotEqual {
-        todo!()
-        // If::new(
-        //     EqualityOperation::new(
-        //         operation.type_().clone(),
-        //         EqualityOperator::Equal,
-        //         operation.lhs().clone(),
-        //         operation.rhs().clone(),
-        //         position.clone(),
-        //     ),
-        //     Boolean::new(false, position.clone()),
-        //     Boolean::new(true, position.clone()),
-        //     position.clone(),
-        // )
-        // .into()
+        let position = operation.position();
+
+        If::new(
+            EqualityOperation::new(
+                operation.type_().cloned(),
+                EqualityOperator::Equal,
+                operation.lhs().clone(),
+                operation.rhs().clone(),
+                position.clone(),
+            ),
+            Boolean::new(false, position.clone()),
+            Boolean::new(true, position.clone()),
+            position.clone(),
+        )
+        .into()
     } else {
         operation.clone().into()
     }
