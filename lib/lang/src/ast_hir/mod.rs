a71b72ceb84089399f01528e161eb3c52a0bb9aa
Yota Toyama <raviqqe@gmail.com>
2021-06-30 03:01:56 +0000

    Compile prelude

--- lib/lang/src/ast_hir/mod.rs
+++ lib/lang/src/ast_hir/mod.rs
@@ -1,6 +1,7 @@
 mod error;
 mod import_renamer;
 mod module_compiler;
+mod prelude_module_modifier;
 mod utilities;
 
 use crate::{
@@ -20,7 +21,7 @@ pub fn compile(
     module_interfaces: &HashMap<ast::ModulePath, interface::Module>,
     prelude_module_interfaces: &[interface::Module],
 ) -> Result<hir::Module, CompileError> {
-    // TODO Do not pass module interfaces to a module compiler.
+    // TODO Do not pass module interfaces to a module compiler but merge them later.
     let module = module_compiler::compile(
         module,
         &module_interfaces
@@ -33,6 +34,15 @@ pub fn compile(
 
     let module = definition_qualifier::qualify(&module, prefix);
     let module = type_qualifier::qualify(&module, prefix);
+
+    Ok(module)
+}
+
+pub fn compile_prelude(module: &ast::Module, prefix: &str) -> Result<hir::Module, CompileError> {
+    let module = module_compiler::compile(module, &[])?;
+    let module = definition_qualifier::qualify(&module, prefix);
+    let module = type_qualifier::qualify(&module, prefix);
+    let module = prelude_module_modifier::modify(&module);
 
     Ok(module)
 }
