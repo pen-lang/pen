c957035ced3598b4a2e288757140333ddfa74c13
Yota Toyama <raviqqe@gmail.com>
2021-07-02 22:15:40 +0000

    Add string module

--- lib/lang/src/hir_mir/string_type_configuration.rs
+++ lib/lang/src/hir_mir/string_type_configuration.rs
@@ -5,7 +5,7 @@ use std::collections::HashMap;
 #[cfg(test)]
 pub static STRING_TYPE_CONFIGURATION: Lazy<StringTypeConfiguration> =
     Lazy::new(|| StringTypeConfiguration {
-        equal_function_name: "equalStrings".into(),
+        equal_function_name: "_equalStrings".into(),
     });
 
 #[derive(Clone, Debug)]
