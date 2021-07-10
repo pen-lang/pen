611c5824b2f4d31a1a6e663ab45e6f6508ddf172
Yota Toyama <raviqqe@gmail.com>
2021-07-10 17:32:42 +0000

    Add core library

---
+++ lib/core/ffi/src/lib.rs
@@ -0,0 +1,2 @@
+mod number;
+mod string;
---
+++ lib/core/ffi/src/string.rs
@@ -0,0 +1,13 @@
+#[no_mangle]
+extern "C" fn _pen_join_strings(one: ffi::ByteString, other: ffi::ByteString) -> ffi::ByteString {
+    one.join(&other)
+}
+
+#[no_mangle]
+extern "C" fn _pen_slice_string(
+    string: ffi::ByteString,
+    start: ffi::Number,
+    end: ffi::Number,
+) -> ffi::ByteString {
+    string.slice(start, end)
+}
