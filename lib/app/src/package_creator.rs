a55978e4f89a6a5433ae126296144383d99c4b02
Yota Toyama <raviqqe@gmail.com>
2021-07-14 15:42:16 +0000

    Refactor package creator

--- lib/app/src/package_creator.rs
+++ lib/app/src/package_creator.rs
@@ -1,24 +1,55 @@
-
-use crate::infra::PackageConfiguration;
 use crate::{
-    infra::{FilePath, Infrastructure},
+    infra::{FilePath, Infrastructure, PackageConfiguration},
+    ApplicationConfiguration,
 };
 use std::error::Error;
 
-pub fn create(
+pub fn create_application(
     infrastructure: &Infrastructure,
-    module_file: &FilePath,
     module_content: &str,
-    package_configuration: &PackageConfiguration,
+    system_package_url: &url::Url,
+    application_configuration: &ApplicationConfiguration,
     package_directory: &FilePath,
 ) -> Result<(), Box<dyn Error>> {
-    infrastructure
-        .package_configuration_writer
-        .write(package_configuration, package_directory)?;
+    infrastructure.package_configuration_writer.write(
+        &PackageConfiguration {
+            dependencies: vec![(
+                application_configuration.system_package_name.clone(),
+                system_package_url.clone(),
+            )]
+            .into_iter()
+            .collect(),
+        },
+        package_directory,
+    )?;
 
-    infrastructure
-        .file_system
-        .write(module_file, module_content.as_bytes())?;
+    infrastructure.file_system.write(
+        &package_directory.join(&FilePath::new([
+            &application_configuration.main_module_basename
+        ])),
+        module_content.as_bytes(),
+    )?;
+
+    Ok(())
+}
+
+pub fn create_library(
+    infrastructure: &Infrastructure,
+    module_filename: &str,
+    module_content: &str,
+    package_directory: &FilePath,
+) -> Result<(), Box<dyn Error>> {
+    infrastructure.package_configuration_writer.write(
+        &PackageConfiguration {
+            dependencies: Default::default(),
+        },
+        package_directory,
+    )?;
+
+    infrastructure.file_system.write(
+        &package_directory.join(&FilePath::new([module_filename])),
+        module_content.as_bytes(),
+    )?;
 
     Ok(())
 }
