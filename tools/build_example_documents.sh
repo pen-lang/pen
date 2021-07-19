9ff6b2e3ace4d5f0ecb5e8392505cf1dd8e20416
Yota Toyama <raviqqe@gmail.com>
2021-07-19 16:00:24 +0000

    Add example documents

---
+++ tools/build_example_documents.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -ex
+
+base_directory=$(dirname $0)/..
+document_directory=$base_directory/doc/content/examples
+
+gherkin2markdown $base_directory/features $document_directory
+rm -r $document_directory/smoke
+
+for file in $(find $document_directory); do
+  if [ ! -f $file -o $(basename $file) = '_index.md' ]; then
+    continue
+  fi
+
+  title=$(grep '^# ' $file | sed 's/# //')
+
+  cat >$file.tmp <<EOF
+---
+title: $title
+---
+
+EOF
+
+  cat $file >>$file.tmp
+  mv $file.tmp $file
+done
