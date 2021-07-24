e28167d5a0587c99a78df9f7def62626167b339a
Yota Toyama <raviqqe@gmail.com>
2021-07-24 23:53:06 +0000

    Fix rust target

--- lib/infra/src/default_target_finder.rs
+++ lib/infra/src/default_target_finder.rs
@@ -2,12 +2,14 @@ use crate::{command_runner, llvm_command_finder, InfrastructureError};
 use std::{error::Error, process::Command};
 
 pub fn find() -> Result<String, Box<dyn Error>> {
-    Ok(regex::Regex::new(r".*Default target: (.*)\n.*")?
-        .captures(&command_runner::run(
-            Command::new(&llvm_command_finder::find("llc")?).arg("--version"),
-        )?)
+    let output =
+        command_runner::run(Command::new(&llvm_command_finder::find("llc")?).arg("--version"))?;
+    let target = regex::Regex::new(r".*Default target: (.*)\n.*")?
+        .captures(&output)
         .and_then(|captures| captures.get(1))
         .ok_or(InfrastructureError::DefaultTargetDetection)?
-        .as_str()
-        .into())
+        .as_str();
+
+    // HACK Map a given target to a known Rust target in the best effort way.
+    Ok(target.replace("-pc-linux-gnu", "-unknown-linux-gnu"))
 }
