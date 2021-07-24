71fd280811cacc5e3013938def323f5d7c91d315
Yota Toyama <raviqqe@gmail.com>
2021-07-24 23:35:15 +0000

    Refactor

--- lib/infra/src/default_target_finder.rs
+++ lib/infra/src/default_target_finder.rs
@@ -2,11 +2,10 @@ use crate::{command_runner, llvm_command_finder, InfrastructureError};
 use std::{error::Error, process::Command};
 
 pub fn find() -> Result<String, Box<dyn Error>> {
-    let output =
-        command_runner::run(Command::new(&llvm_command_finder::find("llc")?).arg("--version"))?;
-
     Ok(regex::Regex::new(r".*Default target: (.*)\n.*")?
-        .captures(&output)
+        .captures(&command_runner::run(
+            Command::new(&llvm_command_finder::find("llc")?).arg("--version"),
+        )?)
         .and_then(|captures| captures.get(1))
         .ok_or_else(|| InfrastructureError::DefaultTargetDetection)?
         .as_str()
