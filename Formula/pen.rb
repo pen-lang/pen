ed15ca094397a80b2744cb941f66fb5533886392
Yota Toyama <raviqqe@gmail.com>
2021-07-24 18:01:44 +0000

    Add homebrew tap

---
+++ Formula/pen.rb
@@ -0,0 +1,21 @@
+class Pen < Formula
+  version '0.1.4'
+  desc 'Pen programming language'
+  homepage 'https://github.com/pen-lang/pen'
+  url "https://github.com/pen-lang/pen/archive/refs/tags/v#{version}.tar.gz"
+  sha256 '555161445bbecbaa646c144d47c64c993906e224a6bcdf9538c8c1e0fedf0ba3'
+  license 'MIT'
+
+  conflicts_with 'pen'
+
+  depends_on 'llvm'
+  depends_on 'rust' => :build
+
+  def install
+    system 'cargo', 'install', '--path', 'cmd/pen', *std_cargo_args
+  end
+
+  test do
+    system "#{bin}/pen", '--version'
+  end
+end
