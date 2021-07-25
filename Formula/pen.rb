ee514ce0870b107cac6dd911b50aa75860eb2fa4
Yota Toyama <raviqqe@gmail.com>
2021-07-25 00:08:18 +0000

    Bump formulae version

--- Formula/pen.rb
+++ Formula/pen.rb
@@ -1,9 +1,9 @@
 class Pen < Formula
-  version '0.1.4'
+  version '0.1.5'
   desc 'Pen programming language'
   homepage 'https://github.com/pen-lang/pen'
   url "https://github.com/pen-lang/pen/archive/refs/tags/v#{version}.tar.gz"
-  sha256 '555161445bbecbaa646c144d47c64c993906e224a6bcdf9538c8c1e0fedf0ba3'
+  sha256 '73a397b9dd2b66b1c07087bf0aa0ae5d322cebf66ad5b41826758d709d011b53'
   license 'MIT'
 
   conflicts_with 'pen'
