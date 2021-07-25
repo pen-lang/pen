382a83178b38f484ed5b13aa9bcd71b91f7ccffd
Yota Toyama <raviqqe@gmail.com>
2021-07-25 02:41:35 +0000

    Fix hash

--- Formula/pen.rb
+++ Formula/pen.rb
@@ -3,7 +3,7 @@ class Pen < Formula
   desc 'Pen programming language'
   homepage 'https://github.com/pen-lang/pen'
   url "https://github.com/pen-lang/pen/archive/refs/tags/v#{version}.tar.gz"
-  sha256 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
+  sha256 '072a9ea14b5023abc20c53efecb2bcdcd3934f1650c65904962080f74b7ea9ed'
   license 'MIT'
 
   conflicts_with 'pen'
