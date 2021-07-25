1cc0566aedda5731ce9912df58e30f56fd501136
Yota Toyama <raviqqe@gmail.com>
2021-07-25 16:02:53 +0000

    Update formulae

--- Formula/pen.rb
+++ Formula/pen.rb
@@ -1,9 +1,9 @@
 class Pen < Formula
-  version '0.1.7'
+  version '0.1.8'
   desc 'Pen programming language'
   homepage 'https://github.com/pen-lang/pen'
   url "https://github.com/pen-lang/pen/archive/refs/tags/v#{version}.tar.gz"
-  sha256 '072a9ea14b5023abc20c53efecb2bcdcd3934f1650c65904962080f74b7ea9ed'
+  sha256 '9c5f180d1b9e86b6a2d9a714b83b0fe2817262a6fda15e4cae71f6a05d614c28'
   license 'MIT'
 
   conflicts_with 'pen'
