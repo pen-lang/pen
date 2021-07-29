2e8b838e997fecb0504e38f5badf3f857d0a7c18
Yota Toyama <raviqqe@gmail.com>
2021-07-29 01:44:39 +0000

    Add open file API

--- lib/os/ffi/src/file.rs
+++ lib/os/ffi/src/file.rs
@@ -1,11 +1,15 @@
-use crate::result::FfiResult;
-
 use super::type_information;
+use crate::result::FfiResult;
 use std::fs::File;
+use std::path::Path;
+use std::str;
 use std::sync::Arc;
 use std::sync::LockResult;
 use std::sync::RwLock;
 use std::sync::RwLockWriteGuard;
+
+const UTF8_DECODE_ERROR: f64 = 0.0;
+const OPEN_FILE_ERROR: f64 = 1.0;
 
 #[derive(Clone, Debug)]
 pub struct FfiFile {
@@ -24,4 +28,25 @@ impl FfiFile {
     pub fn get_mut(&self) -> LockResult<RwLockWriteGuard<'_, File>> {
         Ok(self.file.write()?)
     }
+}
+
+impl From<File> for FfiFile {
+    fn from(file: File) -> Self {
+        Self::new(file)
+    }
+}
+
+#[no_mangle]
+extern "C" fn _pen_os_open_file(path: ffi::ByteString) -> ffi::Arc<FfiResult<FfiFile>> {
+    FfiResult::ok(
+        match File::open(&Path::new(&match str::from_utf8(path.as_slice()) {
+            Ok(path) => path,
+            Err(_) => return FfiResult::error(UTF8_DECODE_ERROR).into(),
+        })) {
+            Ok(file) => file,
+            Err(_) => return FfiResult::error(OPEN_FILE_ERROR).into(),
+        }
+        .into(),
+    )
+    .into()
 }
