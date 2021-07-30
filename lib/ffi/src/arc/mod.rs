40126a88198082cf06bbde8e758d19e9bb766784
Yota Toyama <raviqqe@gmail.com>
2021-07-29 19:27:19 -0700

    Fix Arc clone/drop bugs (#160)
    * Fix new bug
    
    * Test clone
    
    * Fix drop bug
    
    * Bump version
    
    * Update lock file
    
    * Update lock file
    
    * Refactor
    
    * Bump version
    
    * Update dependencies

--- lib/ffi/src/arc/mod.rs
+++ lib/ffi/src/arc/mod.rs
@@ -3,7 +3,7 @@ mod arc_buffer;
 
 use arc_block::*;
 pub use arc_buffer::*;
-use std::{alloc::Layout, marker::PhantomData, ops::Deref};
+use std::{alloc::Layout, marker::PhantomData, ops::Deref, ptr::write};
 
 #[derive(Debug)]
 #[repr(C)]
@@ -17,7 +17,7 @@ impl<T> Arc<T> {
         let mut block = ArcBlock::new(Layout::new::<T>());
 
         unsafe {
-            *(block.ptr_mut() as *mut T) = payload;
+            write(block.ptr_mut() as *mut T, payload);
         }
 
         Self {
@@ -59,8 +59,7 @@ impl<T> Drop for Arc<T> {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    fn drop<T>(_: T) {}
+    use std::mem::{drop, forget};
 
     #[test]
     fn create() {
@@ -72,6 +71,23 @@ mod tests {
         let arc = Arc::new(0);
         drop(arc.clone());
         drop(arc);
+    }
+
+    #[test]
+    fn new_box() {
+        forget(Arc::new(Box::new(0)));
+    }
+
+    #[test]
+    fn clone_box() {
+        let x = Arc::new(Box::new(0));
+        forget(x.clone());
+        forget(x);
+    }
+
+    #[test]
+    fn drop_box() {
+        Arc::new(Box::new(0));
     }
 
     #[test]
