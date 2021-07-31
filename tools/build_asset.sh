48b49eb6a88b3bfa9e75c314d76b1facbcb3e32c
Yota Toyama <raviqqe@gmail.com>
2021-07-31 18:59:29 +0000

    Simplify script

--- tools/build_asset.sh
+++ tools/build_asset.sh
@@ -7,26 +7,14 @@ if [ $# -ne 1 ]; then
 fi
 
 version=$1
-base_directory=$(cd $(dirname $0) && pwd)/..
-asset_directory=$base_directory/tmp/asset
 target=$(rustc -vV | grep host: | sed 's/host: //')
-tarball=$base_directory/pen-$version-$target.tar.gz
+tarball=pen-$version-$target.tar.gz
 
-rm -rf $asset_directory
-mkdir -p $asset_directory
+cd $(dirname $0)/..
 
-(
-  cd $base_directory
-
-  cp -r README.md LICENSE.md LICENSE-MIT LICENSE-APACHE lib $asset_directory
-
-  mkdir $asset_directory/bin
-  cp target/release/pen $asset_directory/bin
-)
-
-(
-  cd $asset_directory
-  tar czf $tarball *
-)
+tar czf $tarball \
+  README.md LICENSE.md LICENSE-MIT LICENSE-APACHE \
+  lib \
+  target/release/pen
 
 echo $tarball
