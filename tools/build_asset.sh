b90508679ae0e789d49f8a07896257577b2cccf7
Yota Toyama <raviqqe@gmail.com>
2021-07-31 12:18:00 -0700

    Fix release action bug (#165)
    * Fix release action bug
    
    * Add build asset script
    
    * Simplify script
    
    * Use xz
    
    * Include doc

---
+++ tools/build_asset.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+if [ $# -ne 1 ]; then
+  exit 1
+fi
+
+version=$1
+target=$(rustc -vV | grep host: | sed 's/host: //')
+tarball=pen-$version-$target.tar.xz
+
+cd $(dirname $0)/..
+
+tar caf $tarball \
+  README.md LICENSE.md LICENSE-MIT LICENSE-APACHE \
+  doc lib target/release/pen
+
+echo $tarball
