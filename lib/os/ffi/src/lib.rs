1ad845801fc8d09d7e4be168c2f8e1ad79134819
Yota Toyama <raviqqe@gmail.com>
2021-08-03 00:58:29 +0000

    Remove unused codes

--- lib/os/ffi/src/lib.rs
+++ lib/os/ffi/src/lib.rs
@@ -4,15 +4,9 @@ mod open_file_options;
 mod result;
 mod stdio;
 
-use result::FfiResult;
 #[cfg(not(test))]
 use std::os::raw::c_int;
-use std::{
-    alloc::Layout,
-    fs::File,
-    io::{Read, Write},
-    os::{raw::c_void, unix::io::FromRawFd},
-};
+use std::{alloc::Layout, os::raw::c_void};
 
 const DEBUG_ENVIRONMENT_VARIABLE: &str = "PEN_DEBUG";
 const DEFAULT_ALIGNMENT: usize = 8;
@@ -89,40 +83,4 @@ pub extern "C" fn main() -> c_int {
 #[cfg(not(test))]
 extern "C" fn exit(_: *mut ffi::cps::Stack, code: f64) -> ffi::cps::Result {
     std::process::exit(code as i32)
-}
-
-#[no_mangle]
-extern "C" fn _pen_os_fd_read(
-    fd: ffi::Number,
-    buffer_size: ffi::Number,
-) -> ffi::Arc<FfiResult<ffi::ByteString>> {
-    let mut file = unsafe { File::from_raw_fd(f64::from(fd) as i32) };
-    let mut buffer = vec![0; f64::from(buffer_size) as usize];
-
-    let count = match file.read(&mut buffer) {
-        Ok(count) => count,
-        Err(error) => return ffi::Arc::new(error.into()),
-    };
-    buffer.resize(count, 0);
-
-    std::mem::forget(file);
-
-    FfiResult::ok(ffi::ByteString::from(buffer)).into()
-}
-
-#[no_mangle]
-extern "C" fn _pen_os_fd_write(
-    fd: ffi::Number,
-    buffer: ffi::ByteString,
-) -> ffi::Arc<FfiResult<ffi::Number>> {
-    let mut file = unsafe { File::from_raw_fd(f64::from(fd) as i32) };
-
-    let count = match file.write(buffer.as_slice()) {
-        Ok(count) => count,
-        Err(error) => return ffi::Arc::new(error.into()),
-    };
-
-    std::mem::forget(file);
-
-    FfiResult::ok((count as f64).into()).into()
 }
