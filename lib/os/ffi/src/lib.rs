d339230f2daf8c31319eb72f5fc081e95168ed8b
Yota Toyama <raviqqe@gmail.com>
2021-08-03 06:16:19 +0000

    Separate heap module

--- lib/os/ffi/src/lib.rs
+++ lib/os/ffi/src/lib.rs
@@ -1,16 +1,11 @@
 mod error;
 mod file;
+mod heap;
 mod open_file_options;
 mod result;
 mod stdio;
 mod utilities;
 
-#[cfg(not(test))]
-use std::os::raw::c_int;
-use std::{alloc::Layout, os::raw::c_void};
-
-const DEBUG_ENVIRONMENT_VARIABLE: &str = "PEN_DEBUG";
-const DEFAULT_ALIGNMENT: usize = 8;
 #[cfg(not(test))]
 const INITIAL_STACK_CAPACITY: usize = 256;
 
@@ -22,58 +17,9 @@ extern "C" {
     ) -> ffi::cps::Result;
 }
 
-#[no_mangle]
-pub extern "C" fn _pen_malloc(size: usize) -> *mut c_void {
-    let pointer =
-        (unsafe { std::alloc::alloc(Layout::from_size_align(size, DEFAULT_ALIGNMENT).unwrap()) })
-            as *mut c_void;
-
-    if std::env::var(DEBUG_ENVIRONMENT_VARIABLE).is_ok() {
-        eprintln!("malloc: {} -> {:x}", size, pointer as usize);
-    }
-
-    pointer
-}
-
-#[no_mangle]
-pub extern "C" fn _pen_realloc(old_pointer: *mut c_void, size: usize) -> *mut c_void {
-    // Layouts are expected to be ignored by the global allocator.
-    let new_pointer = (unsafe {
-        std::alloc::realloc(
-            old_pointer as *mut u8,
-            Layout::from_size_align(0, DEFAULT_ALIGNMENT).unwrap(),
-            size,
-        )
-    }) as *mut c_void;
-
-    if std::env::var(DEBUG_ENVIRONMENT_VARIABLE).is_ok() {
-        eprintln!(
-            "realloc: {:x}, {} -> {:x}",
-            old_pointer as usize, size, new_pointer as usize
-        );
-    }
-
-    new_pointer
-}
-
-/// # Safety
-///
-/// Pointers returned from `_pen_malloc` or `_pen_realloc` must be passed.
-#[no_mangle]
-pub unsafe extern "C" fn _pen_free(pointer: *mut u8) {
-    if std::env::var(DEBUG_ENVIRONMENT_VARIABLE).is_ok() {
-        eprintln!("free: {:x}", pointer as usize);
-    }
-
-    std::alloc::dealloc(
-        pointer,
-        Layout::from_size_align(0, DEFAULT_ALIGNMENT).unwrap(),
-    )
-}
-
 #[cfg(not(test))]
 #[no_mangle]
-pub extern "C" fn main() -> c_int {
+pub extern "C" fn main() -> std::os::raw::c_int {
     let mut stack = ffi::cps::Stack::new(INITIAL_STACK_CAPACITY);
 
     unsafe { _pen_os_main(&mut stack, exit, ffi::None::new()) };
