f1276ddca0bac812c5e0cec790972b57a7a5345a
Yota Toyama <raviqqe@gmail.com>
2021-08-03 05:31:22 +0000

    Start error code from 1

--- lib/os/ffi/src/error.rs
+++ lib/os/ffi/src/error.rs
@@ -1,5 +1,5 @@
-pub const UTF8_DECODE_ERROR: f64 = 0.0;
 pub const OPEN_FILE_ERROR: f64 = 1.0;
 pub const LOCK_FILE_ERROR: f64 = 2.0;
 pub const READ_FILE_ERROR: f64 = 3.0;
 pub const WRITE_FILE_ERROR: f64 = 4.0;
+pub const UTF8_DECODE_ERROR: f64 = 5.0;
