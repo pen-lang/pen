8b53e01661c9d9e0d6b66e64838420f6fe38eb33
Yota Toyama <raviqqe@gmail.com>
2021-08-04 19:51:53 -0700

    Run integration tests on different OS (#180)
    * Run integration tests on different OS
    
    * Fix LLVM install step
    
    * Fix more steps
    
    * Fix install steps
    
    * Disable memory leak tests on macOS
    
    * Fix integration test setup on macOS
    
    * Fix path
    
    * Refactor
    
    * Revert "Refactor"
    
    This reverts commit 71c6f2399c7781eaaade9accf9d5fe6de14a93cf.
    
    * Disable static option
    
    * Run build and test jobs on different OSes
    
    * Split lint workflow
    
    * Fix syntax error
    
    * Refactor
    
    * Fix test workflow

---
+++ tools/ci/set_up_macos.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e
+
+brew install ninja llvm@12
+
+llvm_prefix=$(brew --prefix)/opt/llvm@12
+
+echo LLVM_SYS_120_PREFIX=$llvm_prefix >>$GITHUB_ENV
+echo PATH=$llvm_prefix/bin:$PATH >>$GITHUB_ENV
