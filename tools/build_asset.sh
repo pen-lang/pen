1a35381b535701f635a34ce479b61703aad7e924
Yota Toyama <raviqqe@gmail.com>
2021-08-05 22:19:59 -0700

    Fix release workflow again (#190)
    * TEST
    
    * Set version
    
    * Revert "TEST"
    
    This reverts commit b3f4f93257e459c267b5fc1ce363b7932ecd721b.
    
    * Refactor
    
    * Set version environment variable

--- tools/build_asset.sh
+++ tools/build_asset.sh
@@ -2,11 +2,7 @@
 
 set -e
 
-if [ $# -ne 1 ]; then
-  exit 1
-fi
-
-version=$1
+version=$($(dirname $0)/version.sh)
 target=$(rustc -vV | grep host: | sed 's/host: //')
 tarball=pen-$version-$target.tar.xz
 
