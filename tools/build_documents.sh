b19487552b298094a3674ae767fc5a85fb2120dd
Yota Toyama <raviqqe@gmail.com>
2021-08-07 23:36:19 +0000

    Add custom favicon

--- tools/build_documents.sh
+++ tools/build_documents.sh
@@ -7,3 +7,5 @@ base_directory=$(dirname $0)/..
 go run github.com/raviqqe/gherkin2markdown \
   $base_directory/features \
   $base_directory/doc/src/examples
+
+curl -fsSL https://svgshare.com/i/ZvF.svg >$base_directory/doc/src/favicon.svg
