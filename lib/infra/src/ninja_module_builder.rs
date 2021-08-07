68580ba7399cecc12ae91c2f91475f82c1697b67
Yota Toyama <raviqqe@gmail.com>
2021-08-07 01:15:45 +0000

    Refactor

--- lib/infra/src/ninja_module_builder.rs
+++ lib/infra/src/ninja_module_builder.rs
@@ -17,9 +17,9 @@ impl app::infra::ModuleBuilder for NinjaModuleBuilder {
     fn build(&self, build_script_file: &app::infra::FilePath) -> Result<(), Box<dyn Error>> {
         command_runner::run(
             std::process::Command::new("sh")
-                .arg("-c")
+                .arg("-ec")
                 .arg(format!(
-                    "set -e; ninja -f {} | (grep -v ^ninja: || :)",
+                    "ninja -f {} | (grep -v ^ninja: || :)",
                     self.file_path_converter
                         .convert_to_os_path(build_script_file)
                         .display()
