165a1f4b32412890c12c8164f5502c21af753ee6
Yota Toyama <raviqqe@gmail.com>
2021-08-07 21:08:13 +0000

    Get arguments

--- lib/os/ffi/src/argument.rs
+++ lib/os/ffi/src/argument.rs
@@ -16,3 +16,13 @@ extern "C" fn _pen_os_get_arguments() -> ffi::Arc<Array> {
 extern "C" fn _pen_ffi_any_to_string(any: ffi::Any) -> ffi::ByteString {
     ffi::ByteString::from_any(any).unwrap_or_else(|| ffi::ByteString::from(""))
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn get_arguments() {
+        _pen_os_get_arguments();
+    }
+}
