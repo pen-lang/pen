12659ef77f968d02dd2e296d1ca7c24d39f24bc2
Yota Toyama <raviqqe@gmail.com>
2021-08-10 02:01:51 +0000

    Fix parser

--- lib/lang/src/parse/parsers.rs
+++ lib/lang/src/parse/parsers.rs
@@ -163,11 +163,7 @@ fn type_definition<'a>() -> impl Parser<Stream<'a>, Output = TypeDefinition> {
         position(),
         keyword("type"),
         identifier(),
-        between(
-            sign("{"),
-            sign("}"),
-            sep_end_by((identifier(), type_()), sign(",")),
-        ),
+        between(sign("{"), sign("}"), many((identifier(), type_()))),
     )
         .map(|(position, _, name, elements): (_, _, _, Vec<_>)| {
             TypeDefinition::new(
