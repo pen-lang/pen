8b2591ed8e822f64009dd602bb30e85fb9b9a511
Yota Toyama <raviqqe@gmail.com>
2021-08-11 15:26:38 +0000

    Remove directory

--- lib/os/ffi/src/directory.rs
+++ lib/os/ffi/src/directory.rs
@@ -34,3 +34,12 @@ extern "C" fn _pen_os_create_directory(path: ffi::ByteString) -> ffi::Arc<FfiRes
 fn create_directory(path: ffi::ByteString) -> Result<(), OsError> {
     Ok(fs::create_dir(utilities::decode_path(&path)?)?)
 }
+
+#[no_mangle]
+extern "C" fn _pen_os_remove_directory(path: ffi::ByteString) -> ffi::Arc<FfiResult<ffi::None>> {
+    ffi::Arc::new(remove_directory(path).into())
+}
+
+fn remove_directory(path: ffi::ByteString) -> Result<(), OsError> {
+    Ok(fs::remove_dir(utilities::decode_path(&path)?)?)
+}
