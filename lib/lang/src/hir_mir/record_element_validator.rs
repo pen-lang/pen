d41f6dd9a9619fc956c6455d9652ae259da4a0a9
Yota Toyama <raviqqe@gmail.com>
2021-08-12 14:37:12 +0000

    Implement validator

--- lib/lang/src/hir_mir/record_element_validator.rs
+++ lib/lang/src/hir_mir/record_element_validator.rs
@@ -1,8 +1,20 @@
-use super::CompileError;
+use super::{type_context::TypeContext, CompileError};
 use crate::hir::*;
+use std::collections::HashMap;
 
-pub fn validate(_module: &Module) -> Result<(), CompileError> {
-    // TODO
+pub fn validate(module: &Module, _type_context: &TypeContext) -> Result<(), CompileError> {
+    let _ = are_records_open(module.type_definitions());
 
     Ok(())
+}
+
+fn are_records_open(type_definitions: &[TypeDefinition]) -> HashMap<String, bool> {
+    type_definitions
+        .iter()
+        .map(|definition| (definition.name().into(), is_record_open(definition)))
+        .collect()
+}
+
+fn is_record_open(definition: &TypeDefinition) -> bool {
+    definition.is_open() || !definition.is_external()
 }
