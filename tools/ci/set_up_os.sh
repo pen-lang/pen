6aa4c7301796d551afd7c0064e2621ff489eaa6c
Yota Toyama <raviqqe@gmail.com>
2021-08-18 03:30:42 +0000

    Refactor OS setup on CI

--- tools/ci/set_up_os.sh
+++ tools/ci/set_up_os.sh
@@ -2,9 +2,19 @@
 
 set -e
 
-brew install coreutils ninja llvm@12
+if [ $# -ne 1 ]; then
+  exit 1
+fi
+
+os=$1
+
+brew install llvm@12 ninja
 
 llvm_prefix=$(brew --prefix)/opt/llvm@12
 
 echo LLVM_SYS_120_PREFIX=$llvm_prefix >>$GITHUB_ENV
 echo PATH=$llvm_prefix/bin:$PATH >>$GITHUB_ENV
+
+if echo $os | grep ubuntu; then
+  brew install valgrind
+fi
