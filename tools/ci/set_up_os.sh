d2f532ca4d4e65f445c4dcacdc0fba6e0e8eb3cb
Yota Toyama <raviqqe@gmail.com>
2021-08-18 03:38:15 +0000

    Fix bug

--- tools/ci/set_up_os.sh
+++ tools/ci/set_up_os.sh
@@ -15,6 +15,6 @@ llvm_prefix=$(brew --prefix)/opt/llvm@12
 echo LLVM_SYS_120_PREFIX=$llvm_prefix >>$GITHUB_ENV
 echo PATH=$llvm_prefix/bin:$PATH >>$GITHUB_ENV
 
-if echo $os | grep ubuntu; then
+if [ $RUNNER_OS = Linux ]; then
   brew install valgrind
 fi
