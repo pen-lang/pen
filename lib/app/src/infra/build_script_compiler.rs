ebf4228dcb27cf61c019560339fea575dfa0fba4
Yota Toyama <raviqqe@gmail.com>
2021-08-21 19:31:19 +0000

    Refactor

--- lib/app/src/infra/build_script_compiler.rs
+++ lib/app/src/infra/build_script_compiler.rs
@@ -2,6 +2,13 @@ use super::{FilePath, MainModuleTarget, ModuleTarget};
 use std::error::Error;
 
 pub trait BuildScriptCompiler {
+    fn compile_rules(
+        &self,
+        prelude_interface_files: &[FilePath],
+        output_directory: &FilePath,
+        target_triple: Option<&str>,
+    ) -> Result<String, Box<dyn Error>>;
+
     #[allow(clippy::too_many_arguments)]
     fn compile_main(
         &self,
@@ -12,13 +19,6 @@ pub trait BuildScriptCompiler {
         package_directory: &FilePath,
         rule_build_script_file: &FilePath,
         child_build_script_files: &[FilePath],
-    ) -> Result<String, Box<dyn Error>>;
-
-    fn compile_rules(
-        &self,
-        prelude_interface_files: &[FilePath],
-        output_directory: &FilePath,
-        target_triple: Option<&str>,
     ) -> Result<String, Box<dyn Error>>;
 
     fn compile_external(
