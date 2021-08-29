7293d348527953b29ed6f8548561e2f8daad9457
Yota Toyama <raviqqe@gmail.com>
2021-08-28 18:58:11 -0700

    Fix nested recursive closures (#294)
    * Reproduce bug
    
    * Fix typo
    
    * Run integration test locally
    
    * Add missing tests
    
    * Refactor
    
    * Fix test
    
    * Fix linting
    
    * Comment on recursive let-recursive expressions
    
    * Comment more
    
    * Fix typo
    
    * Comment even more
    
    * Fix bug
    
    * Update dictionary
    
    * Check memory leak

--- lib/mir/src/ir/let_recursive.rs
+++ lib/mir/src/ir/let_recursive.rs
@@ -1,6 +1,19 @@
 use super::{definition::Definition, expression::Expression};
 use std::sync::Arc;
 
+// Function definitions in let-recursive expressions are recursive for the
+// following reasons although we don't have any recursive function
+// definitions in practice.
+//
+// - Variants of typed lambda calculus used in other languages like OCaml are
+//   also implemented like this.
+// - In reference counting, we need to access closure objects inside their
+//   functions themselves to drop them properly because they are moved into
+//   function calls.
+//   - This requirement can be potentially removed by dropping every functions
+//     used in function calls inside the functions themselves.
+//     - This is safe because only global function definitions can be
+//       recursive.
 #[derive(Clone, Debug, PartialEq)]
 pub struct LetRecursive {
     definition: Arc<Definition>,
