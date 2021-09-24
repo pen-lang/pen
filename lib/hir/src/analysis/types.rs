2456d24791f47fd0254063e33fb7b85b14c562c1
Yota Toyama <raviqqe@gmail.com>
2021-09-23 20:42:31 -0700

    Validate type existence (#404)
    Close #150.

--- lib/hir/src/analysis/types.rs
+++ lib/hir/src/analysis/types.rs
@@ -1,9 +1,11 @@
 mod error;
 pub mod record_element_resolver;
 pub mod type_canonicalizer;
+pub mod type_collector;
 pub mod type_comparability_checker;
 pub mod type_difference_calculator;
 pub mod type_equality_checker;
+pub mod type_existence_validator;
 pub mod type_id_calculator;
 pub mod type_resolver;
 pub mod type_subsumption_checker;
