04d8231c9b6ef4f6615a36d1e6fd0421e85e1ef5
Yota Toyama <raviqqe@gmail.com>
2021-09-23 22:41:21 -0700

    Refactor (#406)

--- lib/app/src/package_build_script_compiler/module_target_collector.rs
+++ lib/app/src/package_build_script_compiler/module_target_collector.rs
@@ -13,17 +13,19 @@ pub fn collect_module_targets(
     Ok(module_finder::find(infrastructure, package_directory)?
         .iter()
         .map(|source_file| {
-            let (object_file, interface_file) = file_path_resolver::resolve_target_files(
-                output_directory,
-                source_file,
-                &infrastructure.file_path_configuration,
-            );
-
             ModuleTarget::new(
                 package_directory.clone(),
                 source_file.clone(),
-                object_file,
-                interface_file,
+                file_path_resolver::resolve_object_file(
+                    output_directory,
+                    source_file,
+                    &infrastructure.file_path_configuration,
+                ),
+                file_path_resolver::resolve_interface_file(
+                    output_directory,
+                    source_file,
+                    &infrastructure.file_path_configuration,
+                ),
             )
         })
         .collect::<Vec<_>>())
