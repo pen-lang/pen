8f95e96a8c2908b225f2875125824d3a389f4dd9
Yota Toyama <raviqqe@gmail.com>
2021-10-01 01:11:21 +0000

    Support calling convention in MIR

--- lib/mir/src/ir/foreign_definition.rs
+++ lib/mir/src/ir/foreign_definition.rs
@@ -1,14 +1,22 @@
+use super::CallingConvention;
+
 #[derive(Clone, Debug, PartialEq)]
 pub struct ForeignDefinition {
     name: String,
     foreign_name: String,
+    calling_convention: CallingConvention,
 }
 
 impl ForeignDefinition {
-    pub fn new(name: impl Into<String>, foreign_name: impl Into<String>) -> Self {
+    pub fn new(
+        name: impl Into<String>,
+        foreign_name: impl Into<String>,
+        calling_convention: CallingConvention,
+    ) -> Self {
         Self {
             name: name.into(),
             foreign_name: foreign_name.into(),
+            calling_convention,
         }
     }
 
@@ -18,5 +26,9 @@ impl ForeignDefinition {
 
     pub fn foreign_name(&self) -> &str {
         &self.foreign_name
+    }
+
+    pub fn calling_convention(&self) -> CallingConvention {
+        self.calling_convention
     }
 }
