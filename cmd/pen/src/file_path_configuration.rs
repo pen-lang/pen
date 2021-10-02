a49199d6f8530091c9292daa502638251854c55d
Yota Toyama <raviqqe@gmail.com>
2021-10-02 20:54:36 +0000

    Change test interface file extension

--- cmd/pen/src/file_path_configuration.rs
+++ cmd/pen/src/file_path_configuration.rs
@@ -14,7 +14,7 @@ pub const FILE_PATH_CONFIGURATION: app::infra::FilePathConfiguration =
         source_file_extension: "pen",
         object_file_extension: "o",
         interface_file_extension: "i",
-        test_interface_file_extension: "t",
+        test_interface_file_extension: "test.json",
         archive_file_extension: "a",
         build_script_file_extension: "ninja",
         test_file_extension: "test.pen",
