99b695b4ddbd77f467b3199e1d3d5a20caf8aa4f
Yota Toyama <raviqqe@gmail.com>
2021-10-04 02:00:20 +0000

    Fix

--- cmd/pen/src/test_runner.rs
+++ cmd/pen/src/test_runner.rs
@@ -8,10 +8,9 @@ use std::sync::Arc;
 
 pub fn run() -> Result<(), Box<dyn std::error::Error>> {
     let main_package_directory = main_package_directory_finder::find()?;
-    let file_path_converter = Arc::new(infra::FilePathConverter::new(
-        main_package_directory.clone(),
-    ));
-    let infrastructure = infrastructure::create(file_path_converter, &main_package_directory)?;
+    let file_path_converter = Arc::new(infra::FilePathConverter::new(&main_package_directory));
+    let infrastructure =
+        infrastructure::create(file_path_converter.clone(), &main_package_directory)?;
     let main_package_directory =
         file_path_converter.convert_to_file_path(&main_package_directory)?;
     let output_directory = main_package_directory.join(&app::infra::FilePath::new([
