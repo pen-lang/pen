ba3e388cd6fb3f0cb47d5e6a2037ef4274349079
Yota Toyama <raviqqe@gmail.com>
2021-10-08 03:17:22 +0000

    Fix topological sort

--- lib/app/src/external_package_topological_sorter.rs
+++ lib/app/src/external_package_topological_sorter.rs
@@ -35,7 +35,7 @@ fn sort_external_packages(
 
     for (url, dependencies) in dependencies {
         for dependency_url in dependencies.values() {
-            graph.add_edge(indices[dependency_url], indices[url], ());
+            graph.add_edge(indices[url], indices[dependency_url], ());
         }
     }
 
@@ -44,4 +44,43 @@ fn sort_external_packages(
         .into_iter()
         .map(|index| graph[index].clone())
         .collect())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sort_packages() {
+        assert_eq!(
+            sort_external_packages(
+                &vec![
+                    (url::Url::parse("file:///foo").unwrap(), Default::default()),
+                    (
+                        url::Url::parse("file:///bar").unwrap(),
+                        vec![
+                            ("Foo".into(), url::Url::parse("file:///foo").unwrap()),
+                            ("Baz".into(), url::Url::parse("file:///baz").unwrap())
+                        ]
+                        .into_iter()
+                        .collect()
+                    ),
+                    (
+                        url::Url::parse("file:///baz").unwrap(),
+                        vec![("Foo".into(), url::Url::parse("file:///foo").unwrap()),]
+                            .into_iter()
+                            .collect()
+                    )
+                ]
+                .into_iter()
+                .collect()
+            )
+            .unwrap(),
+            vec![
+                url::Url::parse("file:///bar").unwrap(),
+                url::Url::parse("file:///baz").unwrap(),
+                url::Url::parse("file:///foo").unwrap(),
+            ]
+        );
+    }
 }
