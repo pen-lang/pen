e5ef0b21ba7cd72d829afc77af358b306ed5f740
Yota Toyama <raviqqe@gmail.com>
2021-10-08 04:04:12 +0000

    Fix bug

--- lib/infra/src/json_package_configuration_reader.rs
+++ lib/infra/src/json_package_configuration_reader.rs
@@ -1,18 +1,12 @@
 use super::json_package_configuration::JsonPackageConfiguration;
 use crate::{package_script_finder, FilePathConverter};
-use std::{
-    collections::HashMap,
-    error::Error,
-    path::{Path, PathBuf},
-    sync::Arc,
-};
+use std::{collections::HashMap, error::Error, sync::Arc};
 
 pub struct JsonPackageConfigurationReader {
     file_system: Arc<dyn app::infra::FileSystem>,
     file_path_converter: Arc<FilePathConverter>,
     build_configuration_filename: &'static str,
     ffi_build_script_basename: &'static str,
-    main_package_directory_path: PathBuf,
 }
 
 impl JsonPackageConfigurationReader {
@@ -21,14 +15,12 @@ impl JsonPackageConfigurationReader {
         file_path_converter: Arc<FilePathConverter>,
         build_configuration_filename: &'static str,
         ffi_build_script_basename: &'static str,
-        main_package_directory_path: impl AsRef<Path>,
     ) -> Self {
         Self {
             file_system,
             file_path_converter,
             build_configuration_filename,
             ffi_build_script_basename,
-            main_package_directory_path: main_package_directory_path.as_ref().into(),
         }
     }
 }
@@ -38,9 +30,13 @@ impl app::infra::PackageConfigurationReader for JsonPackageConfigurationReader {
         &self,
         package_directory: &app::infra::FilePath,
     ) -> Result<HashMap<String, url::Url>, Box<dyn Error>> {
-        let main_package_file_url =
-            url::Url::from_directory_path(&self.main_package_directory_path.canonicalize()?)
-                .unwrap();
+        let package_file_url = url::Url::from_directory_path(
+            &self
+                .file_path_converter
+                .convert_to_os_path(package_directory)
+                .canonicalize()?,
+        )
+        .unwrap();
 
         Ok(
             serde_json::from_str::<JsonPackageConfiguration>(&self.file_system.read_to_string(
@@ -48,7 +44,7 @@ impl app::infra::PackageConfigurationReader for JsonPackageConfigurationReader {
                     self.build_configuration_filename,
                 ])),
             )?)?
-            .dependencies(&main_package_file_url)?,
+            .dependencies(&package_file_url)?,
         )
     }
 
