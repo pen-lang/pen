b3d854b8afd0a29b3cd181409acca77add8e7bcb
Yota Toyama <raviqqe@gmail.com>
2021-10-09 19:27:33 +0000

    Rename argument

--- lib/hir/src/ir/expression.rs
+++ lib/hir/src/ir/expression.rs
@@ -84,8 +84,8 @@ impl From<RecordConstruction> for Expression {
 }
 
 impl From<RecordDeconstruction> for Expression {
-    fn from(field: RecordDeconstruction) -> Self {
-        Self::RecordDeconstruction(field)
+    fn from(deconstruction: RecordDeconstruction) -> Self {
+        Self::RecordDeconstruction(deconstruction)
     }
 }
 
