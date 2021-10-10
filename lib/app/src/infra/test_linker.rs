ab9de92b8d74105f690764202265e004347090ad
Yota Toyama <raviqqe@gmail.com>
2021-10-10 20:48:47 +0000

    Write main.rs

--- lib/app/src/infra/test_linker.rs
+++ lib/app/src/infra/test_linker.rs
@@ -4,8 +4,8 @@ use std::error::Error;
 pub trait TestLinker {
     fn link(
         &self,
+        package_test_information: &test_info::Package,
         archive_files: &[FilePath],
-        test_information_file: &FilePath,
         test_file: &FilePath,
         output_directory: &FilePath,
     ) -> Result<(), Box<dyn Error>>;
--- lib/app/src/test_linker.rs
+++ lib/app/src/test_linker.rs
@@ -1,5 +1,5 @@
 use crate::{
-    common::file_path_resolver,
+    common::{file_path_resolver, package_test_information_serializer},
     infra::{FilePath, Infrastructure},
 };
 use std::error::Error;
@@ -12,8 +12,12 @@ pub fn link(
     output_directory: &FilePath,
 ) -> Result<(), Box<dyn Error>> {
     infrastructure.test_linker.link(
+        &package_test_information_serializer::deserialize(
+            &infrastructure
+                .file_system
+                .read_to_vec(test_information_file)?,
+        )?,
         archive_files,
-        test_information_file,
         test_file,
         &file_path_resolver::resolve_test_directory(output_directory),
     )?;
