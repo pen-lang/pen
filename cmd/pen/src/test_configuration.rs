3089b88132b8d75600dcd2987add38f05e355eed
Yota Toyama <raviqqe@gmail.com>
2021-10-10 01:26:13 +0000

    Remove unused field

--- cmd/pen/src/test_configuration.rs
+++ cmd/pen/src/test_configuration.rs
@@ -2,7 +2,6 @@ use once_cell::sync::Lazy;
 
 pub static TEST_CONFIGURATION: Lazy<app::TestConfiguration> =
     Lazy::new(|| app::TestConfiguration {
-        test_package_name: "Test".into(),
         test_module_configuration: app::TestModuleConfiguration {
             test_function_prefix: "_pen_test_".into(),
         },
