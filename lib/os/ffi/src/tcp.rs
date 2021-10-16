784278b867a4860843437dd7e7717a6d9af5a272
Yota Toyama <raviqqe@gmail.com>
2021-10-16 04:06:38 +0000

    Add Tcp module

---
+++ lib/os/ffi/src/tcp.rs
@@ -0,0 +1,155 @@
+use crate::{error::OsError, result::FfiResult};
+use ffi::AnyLike;
+use std::io::{Read, Write};
+use std::{
+    net, str,
+    sync::{Arc, LockResult, RwLock, RwLockWriteGuard},
+};
+
+#[derive(Clone, Default)]
+pub struct TcpListener {
+    inner: ffi::Any,
+}
+
+impl TcpListener {
+    pub fn new(listener: net::TcpListener) -> ffi::Arc<Self> {
+        Self {
+            inner: TcpListenerInner::new(listener).into_any(),
+        }
+        .into()
+    }
+
+    pub fn lock(&self) -> Result<RwLockWriteGuard<net::TcpListener>, OsError> {
+        Ok(TcpListenerInner::as_inner(&self.inner).unwrap().get_mut()?)
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct TcpListenerInner {
+    listener: Arc<RwLock<net::TcpListener>>,
+}
+
+ffi::type_information!(ffi_tcp_listener, crate::tcp::TcpListenerInner);
+
+impl TcpListenerInner {
+    pub fn new(listener: net::TcpListener) -> Self {
+        Self {
+            listener: RwLock::new(listener).into(),
+        }
+    }
+
+    pub fn get_mut(&self) -> LockResult<RwLockWriteGuard<'_, net::TcpListener>> {
+        self.listener.write()
+    }
+}
+
+#[derive(Clone, Default)]
+pub struct TcpStream {
+    inner: ffi::Any,
+}
+
+impl TcpStream {
+    pub fn new(socket: net::TcpStream) -> ffi::Arc<Self> {
+        Self {
+            inner: TcpStreamInner::new(socket).into_any(),
+        }
+        .into()
+    }
+
+    pub fn lock(&self) -> Result<RwLockWriteGuard<net::TcpStream>, OsError> {
+        Ok(TcpStreamInner::as_inner(&self.inner).unwrap().get_mut()?)
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct TcpStreamInner {
+    socket: Arc<RwLock<net::TcpStream>>,
+}
+
+ffi::type_information!(ffi_tcp_socket, crate::tcp::TcpStreamInner);
+
+impl TcpStreamInner {
+    pub fn new(socket: net::TcpStream) -> Self {
+        Self {
+            socket: RwLock::new(socket).into(),
+        }
+    }
+
+    pub fn get_mut(&self) -> LockResult<RwLockWriteGuard<'_, net::TcpStream>> {
+        self.socket.write()
+    }
+}
+
+#[no_mangle]
+extern "C" fn _pen_os_tcp_bind(
+    address: ffi::ByteString,
+) -> ffi::Arc<FfiResult<ffi::Arc<TcpListener>>> {
+    ffi::Arc::new(bind(address).into())
+}
+
+fn bind(address: ffi::ByteString) -> Result<ffi::Arc<TcpListener>, OsError> {
+    Ok(TcpListener::new(net::TcpListener::bind(str::from_utf8(address.as_slice())?)?).into())
+}
+
+#[no_mangle]
+extern "C" fn _pen_os_tcp_connect(address: ffi::ByteString) -> ffi::Arc<FfiResult<ffi::None>> {
+    ffi::Arc::new(connect(address).into())
+}
+
+fn connect(address: ffi::ByteString) -> Result<(), OsError> {
+    net::TcpStream::connect(str::from_utf8(address.as_slice())?)?;
+
+    Ok(())
+}
+
+#[no_mangle]
+extern "C" fn _pen_os_tcp_accept(
+    listener: ffi::Arc<TcpListener>,
+) -> ffi::Arc<FfiResult<ffi::Arc<TcpStream>>> {
+    ffi::Arc::new(accept(listener).into())
+}
+
+fn accept(listener: ffi::Arc<TcpListener>) -> Result<ffi::Arc<TcpStream>, OsError> {
+    let (stream, _) = listener.lock()?.accept()?;
+
+    Ok(TcpStream::new(stream).into())
+}
+
+#[no_mangle]
+extern "C" fn _pen_os_tcp_receive(
+    socket: ffi::Arc<TcpStream>,
+    limit: ffi::Number,
+) -> ffi::Arc<FfiResult<ffi::ByteString>> {
+    ffi::Arc::new(receive(socket, limit).into())
+}
+
+fn receive(socket: ffi::Arc<TcpStream>, limit: ffi::Number) -> Result<ffi::ByteString, OsError> {
+    let mut buffer = vec![0; f64::from(limit) as usize];
+    let size = socket.lock()?.read(&mut buffer)?;
+
+    buffer.truncate(size);
+
+    Ok(buffer.into())
+}
+
+#[no_mangle]
+extern "C" fn _pen_os_tcp_send(
+    socket: ffi::Arc<TcpStream>,
+    data: ffi::ByteString,
+) -> ffi::Arc<FfiResult<ffi::Number>> {
+    ffi::Arc::new(send(socket, data).into())
+}
+
+fn send(socket: ffi::Arc<TcpStream>, data: ffi::ByteString) -> Result<ffi::Number, OsError> {
+    Ok((socket.lock()?.write(data.as_slice())? as f64).into())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn bind_socket() {
+        bind("127.0.0.1:8082".into()).unwrap();
+    }
+}
