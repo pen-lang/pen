c637e1455c944efbd395b2c7f1fa624aedcbb7f3
Yota Toyama <raviqqe@gmail.com>
2021-10-27 05:24:52 +0000

    Fix build

--- cmd/test/src/debug.rs
+++ cmd/test/src/debug.rs
@@ -1,6 +1,8 @@
+use std::{env, str};
+
 #[no_mangle]
 pub extern "C" fn _pen_debug(message: ffi::ByteString) -> ffi::None {
-    if env::get("PEN_DEBUG").is_ok() {
+    if env::var("PEN_DEBUG").is_ok() {
         eprintln!("{}", str::from_utf8(message.as_slice()).unwrap());
     }
 
