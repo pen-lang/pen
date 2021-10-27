41b34686b43dbbdfed5f2ef59efa50ee7bd7a708
Yota Toyama <raviqqe@gmail.com>
2021-10-27 04:53:36 +0000

    Fix build

--- cmd/test/src/debug.rs
+++ cmd/test/src/debug.rs
@@ -1,5 +1,5 @@
 #[no_mangle]
-pub extern "C" fn _pen_malloc(size: usize) -> ffi::None {
+pub extern "C" fn _pen_malloc(message: ffi::ByteString) -> ffi::None {
     if env::get("PEN_DEBUG").is_ok() {
         eprintln!("{}", str::from_utf8(message.as_slice()).unwrap());
     }
