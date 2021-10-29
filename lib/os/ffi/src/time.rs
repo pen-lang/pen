3ae3a233b88791c28480080e79269fc60ad5ac6b
Yota Toyama <raviqqe@gmail.com>
2021-10-28 21:57:16 -0700

    Sleep function (#503)
    - Add sleep function
    - Add integration test

--- lib/os/ffi/src/time.rs
+++ lib/os/ffi/src/time.rs
@@ -1,4 +1,4 @@
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::{thread::sleep, time::{Duration, SystemTime, UNIX_EPOCH}};
 
 #[no_mangle]
 extern "C" fn _pen_os_get_time() -> ffi::Number {
@@ -7,4 +7,11 @@ extern "C" fn _pen_os_get_time() -> ffi::Number {
         .unwrap_or_else(|error| error.duration())
         .as_millis() as f64)
         .into()
+}
+
+#[no_mangle]
+extern "C" fn _pen_os_sleep(milliseconds: ffi::Number) -> ffi::None {
+    sleep(Duration::from_millis(f64::from(milliseconds) as u64));
+
+    ffi::None::new()
 }
