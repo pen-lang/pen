db3c99b058f8e683ddd91b7b84f4cd69a83d9117
Yota Toyama <raviqqe@gmail.com>
2021-11-06 23:21:34 +0000

    Fix update script

--- tools/update_ffi_crate.sh
+++ tools/update_ffi_crate.sh
@@ -3,10 +3,11 @@
 set -e
 
 root_directory=$(dirname $0)/..
+ffi_version=$(grep 'version = ' lib/ffi/Cargo.toml | grep -o '[0-9.]\+' | head -n 1)
 
 for directory in . cmd/test lib/os/ffi; do
   (
     cd $root_directory/$directory
-    cargo update --aggressive -p pen-ffi
+    cargo update --aggressive -p pen-ffi:$ffi_version
   )
 done
