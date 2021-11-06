db63179e90d7775d1e4646643d9af6dd4f509d5a
Yota Toyama <raviqqe@gmail.com>
2021-11-06 23:04:36 +0000

    Bump version

--- tools/update_ffi_crate.sh
+++ tools/update_ffi_crate.sh
@@ -7,6 +7,6 @@ root_directory=$(dirname $0)/..
 for directory in . cmd/test lib/os/ffi; do
   (
     cd $root_directory/$directory
-    cargo update
+    cargo update --aggressive -p pen-ffi
   )
 done
