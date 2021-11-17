0ba1815473c02b438b7cbf53c45b5c476152a5c5
Yota Toyama <raviqqe@gmail.com>
2021-11-17 04:30:03 +0000

    Update update-ffi-crate script

--- tools/update_ffi_crate.sh
+++ tools/update_ffi_crate.sh
@@ -4,7 +4,7 @@ set -e
 
 root_directory=$(dirname $0)/..
 
-for directory in . cmd/test lib/os/ffi; do
+for directory in . cmd/test lib/os/ffi lib/os-async/ffi; do
   (
     cd $root_directory/$directory
     cargo update
