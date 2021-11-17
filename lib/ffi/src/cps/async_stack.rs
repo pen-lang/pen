c7ae4d23953827cfa4c1a5df5d186720e7c550c6
Yota Toyama <raviqqe@gmail.com>
2021-11-17 03:35:44 +0000

    Fix async stack

--- lib/ffi/src/cps/async_stack.rs
+++ lib/ffi/src/cps/async_stack.rs
@@ -1,7 +1,7 @@
 use super::Stack;
 use std::{
-    future::{Future, Ready},
-    ops::Deref,
+    future::Future,
+    ops::{Deref, DerefMut},
     task::Context,
 };
 
@@ -26,18 +26,14 @@ impl<'a> AsyncStack<'a> {
     }
 
     pub fn suspend(&mut self, future: impl Future) {
-        // TODO
-        // self.push(future);
+        self.stack.push(future);
         self.suspended = true;
     }
 
-    pub fn resume(&mut self) -> Option<impl Future> {
+    pub fn resume<F: Future>(&mut self) -> Option<F> {
         if self.suspended {
             self.suspended = false;
-            let future: Option<Ready<()>> = None;
-            future
-            // TODO
-            // self.pop()
+            Some(self.stack.pop())
         } else {
             None
         }
@@ -49,5 +45,43 @@ impl<'a> Deref for AsyncStack<'a> {
 
     fn deref(&self) -> &Stack {
         &self.stack
+    }
+}
+
+impl<'a> DerefMut for AsyncStack<'a> {
+    fn deref_mut(&mut self) -> &mut Stack {
+        &mut self.stack
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::ptr::null;
+    use std::task::{RawWaker, RawWakerVTable, Waker};
+
+    const RAW_WAKER_DATA: () = ();
+    const RAW_WAKER_V_TABLE: RawWakerVTable =
+        RawWakerVTable::new(clone_waker, do_nothing, do_nothing, do_nothing);
+
+    fn create_waker() -> Waker {
+        unsafe { Waker::from_raw(RawWaker::new(&RAW_WAKER_DATA, &RAW_WAKER_V_TABLE)) }
+    }
+
+    fn clone_waker(_: *const ()) -> RawWaker {
+        RawWaker::new(null(), &RAW_WAKER_V_TABLE)
+    }
+
+    fn do_nothing(_: *const ()) {}
+
+    #[test]
+    fn push_f64() {
+        let waker = create_waker();
+        let mut context = Context::from_waker(&waker);
+        let mut stack = AsyncStack::new(1, &mut context);
+
+        stack.push(42.0f64);
+
+        assert_eq!(stack.pop::<f64>(), 42.0);
     }
 }
