34e119cc0e44feb1796f93da27f0bb8d77f7f0ba
Yota Toyama <raviqqe@gmail.com>
2021-11-17 04:32:35 +0000

    Add run_all_crates script

--- tools/run_all_crates.sh
+++ tools/run_all_crates.sh
@@ -2,11 +2,15 @@
 
 set -e
 
+if [ $# -eq 0 ]; then
+  exit 1
+fi
+
 root_directory=$(dirname $0)/..
 
 for directory in . cmd/test lib/os/ffi lib/os-async/ffi; do
   (
     cd $root_directory/$directory
-    cargo update
+    "$@"
   )
 done
