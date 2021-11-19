48adcf86f51c360ebf2b344fd96e7eb3b4f6b62a
Yota Toyama <raviqqe@gmail.com>
2021-11-19 04:29:56 +0000

    Bump version

--- lib/infra/src/llvm_command_finder.rs
+++ lib/infra/src/llvm_command_finder.rs
@@ -2,7 +2,7 @@ use crate::command_finder;
 use std::{error::Error, path::PathBuf};
 
 const MINIMUM_VERSION: usize = 11;
-const MAXIMUM_VERSION: usize = 12;
+const MAXIMUM_VERSION: usize = 13;
 
 pub fn find(command: &str) -> Result<PathBuf, Box<dyn Error>> {
     for version in (MINIMUM_VERSION..=MAXIMUM_VERSION).rev() {
