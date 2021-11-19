25e4139eaf89fa728a0f13153e88ca3f6d920862
Yota Toyama <raviqqe@gmail.com>
2021-11-19 04:14:42 +0000

    Fix script

--- tools/ci/set_up_os.sh
+++ tools/ci/set_up_os.sh
@@ -6,7 +6,7 @@ llvm_version=13
 
 brew install jq llvm@$llvm_version ninja
 
-llvm_prefix=$(brew --prefix)/opt/llvm@1$llvm_version
+llvm_prefix=$(brew --prefix)/opt/llvm@$llvm_version
 
 echo LLVM_SYS_${llvm_version}0_PREFIX=$llvm_prefix >>$GITHUB_ENV
 echo PATH=$llvm_prefix/bin:$PATH >>$GITHUB_ENV
