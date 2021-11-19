ff08059cc3cc2762f5c2f3e814b9e21b1517a87d
Yota Toyama <raviqqe@gmail.com>
2021-11-19 05:35:54 +0000

    Revert "Install gcc"
    This reverts commit 83302d593296ab9ebcb9383fbcfa3626b41f16e1.

--- tools/ci/set_up_os.sh
+++ tools/ci/set_up_os.sh
@@ -4,12 +4,12 @@ set -e
 
 llvm_version=13
 
-brew install gcc jq llvm@$llvm_version ninja
+brew install jq llvm@$llvm_version ninja
 
 llvm_prefix=$(brew --prefix)/opt/llvm@$llvm_version
 
 echo LLVM_SYS_${llvm_version}0_PREFIX=$llvm_prefix >>$GITHUB_ENV
-echo PATH=$llvm_prefix/bin:$(brew --prefix)/opt/gcc/bin:$PATH >>$GITHUB_ENV
+echo PATH=$llvm_prefix/bin:$PATH >>$GITHUB_ENV
 
 if [ $RUNNER_OS = Linux ]; then
   sudo apt install libc6-dbg # for valgrind
