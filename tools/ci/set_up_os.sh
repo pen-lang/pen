85512d1f16262f8e23aabf2051bf79de2e88c6c8
Yota Toyama <raviqqe@gmail.com>
2021-11-19 04:01:00 +0000

    Update dependencies

--- tools/ci/set_up_os.sh
+++ tools/ci/set_up_os.sh
@@ -2,12 +2,9 @@
 
 set -e
 
-brew install jq llvm@12 ninja
+brew install jq llvm ninja
 
-llvm_prefix=$(brew --prefix)/opt/llvm@12
-
-echo LLVM_SYS_120_PREFIX=$llvm_prefix >>$GITHUB_ENV
-echo PATH=$llvm_prefix/bin:$PATH >>$GITHUB_ENV
+echo PATH=$(brew --prefix)/opt/llvm/bin:$PATH >>$GITHUB_ENV
 
 if [ $RUNNER_OS = Linux ]; then
   sudo apt install libc6-dbg # for valgrind
