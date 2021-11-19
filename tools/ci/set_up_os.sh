e24562a009ce7bff50cfa354737d18e07b94c484
Yota Toyama <raviqqe@gmail.com>
2021-11-19 01:37:12 -0800

    Update LLVM to 13 (#538)
    * Update dependencies
    
    * Fix warnings
    
    * Fix build
    
    * Fix script
    
    * Bump version
    
    * TEST
    
    * Revert "TEST"
    
    This reverts commit 51f89c77f9bb4d3879b40049a66efcc86e91d6dc.
    
    * Use clang
    
    * Remove targets from top-level rust-toolchain.toml
    
    * Revert "Use clang"
    
    This reverts commit 07a1159e58bace20c4710a73e3dcb302e24f0e8f.
    
    * Install gcc
    
    * Revert "Install gcc"
    
    This reverts commit 2e64f53c208641308a3a36e852b57d52ff14c9f9.
    
    * Install gcc
    
    * Try using Ubuntu 18.04
    
    * Revert "Install gcc"
    
    This reverts commit 83302d593296ab9ebcb9383fbcfa3626b41f16e1.
    
    * Use Ubuntu 18.04 more
    
    * Clean homebrew cache after setup
    
    * Revert "Clean homebrew cache after setup"
    
    This reverts commit 0e3445ec2aa0441cf439c81f8ae31960b3eaee48.
    
    * Revert "Remove targets from top-level rust-toolchain.toml"
    
    This reverts commit b52d2b97376ac9c7c1290bc1ac4013eb36715c24.

--- tools/ci/set_up_os.sh
+++ tools/ci/set_up_os.sh
@@ -2,11 +2,13 @@
 
 set -e
 
-brew install jq llvm@12 ninja
+llvm_version=13
 
-llvm_prefix=$(brew --prefix)/opt/llvm@12
+brew install jq llvm@$llvm_version ninja
 
-echo LLVM_SYS_120_PREFIX=$llvm_prefix >>$GITHUB_ENV
+llvm_prefix=$(brew --prefix)/opt/llvm@$llvm_version
+
+echo LLVM_SYS_${llvm_version}0_PREFIX=$llvm_prefix >>$GITHUB_ENV
 echo PATH=$llvm_prefix/bin:$PATH >>$GITHUB_ENV
 
 if [ $RUNNER_OS = Linux ]; then
