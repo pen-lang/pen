53dd2d766a5df03bd2bcbb633b3cce40fa9fa9a3
Yota Toyama <raviqqe@gmail.com>
2021-11-20 04:57:10 +0000

    Fix cucumber home directory

--- features/support/aruba.rb
+++ features/support/aruba.rb
@@ -2,5 +2,7 @@ require 'aruba/cucumber'
 
 Aruba.configure do |config|
   config.exit_timeout = 60
-  config.home_directory = ENV['HOME']
+  config.home_directory = File.join(
+    config.root_directory, config.working_directory
+  )
 end
