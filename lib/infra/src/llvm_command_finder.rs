373882f4b3410a6d3f23bc4beafdf09d0f037718
Yota Toyama <raviqqe@gmail.com>
2021-11-20 02:25:56 +0000

    Fix llvm command finding

--- lib/infra/src/llvm_command_finder.rs
+++ lib/infra/src/llvm_command_finder.rs
@@ -1,15 +1,12 @@
 use crate::command_finder;
 use std::{error::Error, path::PathBuf};
 
-const MINIMUM_VERSION: usize = 11;
-const MAXIMUM_VERSION: usize = 13;
+const LLVM_VERSION: usize = 13;
 
 pub fn find(command: &str) -> Result<PathBuf, Box<dyn Error>> {
-    for version in (MINIMUM_VERSION..=MAXIMUM_VERSION).rev() {
-        if let Ok(path) = command_finder::find(&format!("{}-{}", command, version)) {
-            return Ok(path);
-        }
+    if let Ok(path) = command_finder::find(command) {
+        return Ok(path);
     }
 
-    command_finder::find(command)
+    command_finder::find(&format!("{}-{}", command, LLVM_VERSION))
 }
