7c77a624551439bbb4d2b2b9784bca2e9b811a34
Yota Toyama <raviqqe@gmail.com>
2021-11-20 05:06:18 +0000

    Change home directory

--- features/support/aruba.rb
+++ features/support/aruba.rb
@@ -3,6 +3,6 @@ require 'aruba/cucumber'
 Aruba.configure do |config|
   config.exit_timeout = 60
   config.home_directory = File.join(
-    config.root_directory, config.working_directory
+    config.root_directory, 'tmp/home'
   )
 end
