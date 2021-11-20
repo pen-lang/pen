8239297118305af3d9d281217df5ddda37f87119
Yota Toyama <raviqqe@gmail.com>
2021-11-20 05:23:49 +0000

    Install rust toolchains first

--- tools/integration_test.sh
+++ tools/integration_test.sh
@@ -2,6 +2,16 @@
 
 set -e
 
+for channel in stable nightly; do
+  for target in \
+    i686-unknown-linux-musl \
+    x86_64-unknown-linux-musl \
+    aarch64-unknown-linux-musl \
+    wasm32-wasi; do
+    rustup target add --toolchain $channel $target
+  done
+done
+
 cd $(dirname $0)/..
 
 PEN_ROOT=$PWD PATH=$PWD/target/release:$PWD/tools:$PATH cucumber --publish-quiet "$@"
