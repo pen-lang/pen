be5c171a744a92b5185b14d72fc25ad4abb9c1fe
Yota Toyama <raviqqe@gmail.com>
2021-11-21 05:34:51 +0000

    Read from stdin asynchronously

--- lib/os-async/ffi/library/src/stdio.rs
+++ lib/os-async/ffi/library/src/stdio.rs
@@ -1,20 +1,38 @@
 use super::utilities;
 use crate::result::FfiResult;
-use std::io::{stderr, stdin, stdout};
+use std::{future::Future, task::Poll};
+use tokio::io::stdin;
 
 // TODO Make those asynchronous.
 
 #[no_mangle]
-extern "C" fn _pen_os_read_stdin() -> ffi::Arc<FfiResult<ffi::ByteString>> {
-    ffi::Arc::new(utilities::read(&mut stdin()).into())
+extern "C" fn _pen_read_stdin(
+    stack: &mut ffi::cps::AsyncStack,
+    continuation: extern "C" fn(
+        &mut ffi::cps::AsyncStack,
+        ffi::Arc<FfiResult<ffi::ByteString>>,
+    ) -> ffi::cps::Result,
+) -> ffi::cps::Result {
+    let mut stdin = stdin();
+    let mut future = if let Some(future) = stack.resume() {
+        future
+    } else {
+        Box::pin(utilities::read(&mut stdin))
+    };
+
+    let value = match future.as_mut().poll(stack.context()) {
+        Poll::Ready(value) => value,
+        Poll::Pending => {
+            stack.suspend(future);
+
+            return ffi::cps::Result::new();
+        }
+    };
+
+    continuation(stack, ffi::Arc::new(value.into()))
 }
 
-#[no_mangle]
-extern "C" fn _pen_os_read_limit_stdin(limit: ffi::Number) -> ffi::Arc<FfiResult<ffi::ByteString>> {
-    ffi::Arc::new(utilities::read_limit(&mut stdin(), f64::from(limit) as usize).into())
-}
-
-#[no_mangle]
+/* #[no_mangle]
 extern "C" fn _pen_os_write_stdout(bytes: ffi::ByteString) -> ffi::Arc<FfiResult<ffi::Number>> {
     ffi::Arc::new(utilities::write(&mut stdout(), bytes).into())
 }
@@ -22,4 +40,4 @@ extern "C" fn _pen_os_write_stdout(bytes: ffi::ByteString) -> ffi::Arc<FfiResult
 #[no_mangle]
 extern "C" fn _pen_os_write_stderr(bytes: ffi::ByteString) -> ffi::Arc<FfiResult<ffi::Number>> {
     ffi::Arc::new(utilities::write(&mut stderr(), bytes).into())
-}
+} */
