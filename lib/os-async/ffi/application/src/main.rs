9df44e0bba86faea84834ec5a2f027be535558d9
Yota Toyama <raviqqe@gmail.com>
2021-11-21 22:30:22 +0000

    Do not use raw pointers

--- lib/os-async/ffi/application/src/main.rs
+++ lib/os-async/ffi/application/src/main.rs
@@ -16,14 +16,14 @@ const INITIAL_STACK_CAPACITY: usize = 256;
 #[link(name = "main")]
 extern "C" {
     fn _pen_os_main(
-        stack: *mut ffi::cps::AsyncStack,
+        stack: &mut ffi::cps::AsyncStack,
         continuation: extern "C" fn(*mut ffi::cps::AsyncStack, f64) -> ffi::cps::Result,
     ) -> ffi::cps::Result;
 }
 
 #[cfg(test)]
 unsafe extern "C" fn _pen_os_main(
-    _: *mut ffi::cps::AsyncStack,
+    _: &mut ffi::cps::AsyncStack,
     _: extern "C" fn(*mut ffi::cps::AsyncStack, f64) -> ffi::cps::Result,
 ) -> ffi::cps::Result {
     ffi::cps::Result::new()
@@ -44,6 +44,6 @@ fn main_wrapper(context: &mut Context<'_>) -> Poll<()> {
     unreachable!()
 }
 
-extern "C" fn exit(_: *mut ffi::cps::AsyncStack, code: f64) -> ffi::cps::Result {
+extern "C" fn exit(_: &mut ffi::cps::AsyncStack, code: f64) -> ffi::cps::Result {
     std::process::exit(code as i32)
 }
