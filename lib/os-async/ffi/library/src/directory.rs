e8e743dabf3e4b75cf0b73b016c293fc3dd47a6d
Yota Toyama <raviqqe@gmail.com>
2021-12-05 02:55:53 +0000

    Read directory

--- lib/os-async/ffi/library/src/directory.rs
+++ lib/os-async/ffi/library/src/directory.rs
@@ -3,9 +3,35 @@ use tokio::fs;
 
 #[ffi::bindgen]
 async fn _pen_os_read_directory(
-    _path: ffi::ByteString,
+    path: ffi::ByteString,
 ) -> ffi::Arc<FfiResult<ffi::Arc<ffi::extra::StringArray>>> {
-    todo!()
+    ffi::Arc::new(read_directory(path).await.into())
+}
+
+async fn read_directory(
+    path: ffi::ByteString,
+) -> Result<ffi::Arc<ffi::extra::StringArray>, OsError> {
+    let mut read_dir = fs::read_dir(utilities::decode_path(&path)?).await?;
+    let mut entries = vec![];
+
+    while let Some(entry) = read_dir.next_entry().await? {
+        entries.push(entry);
+    }
+
+    Ok(ffi::Arc::new(
+        entries
+            .into_iter()
+            .map(|entry| {
+                Ok(ffi::ByteString::from(
+                    entry
+                        .file_name()
+                        .into_string()
+                        .map_err(|_| OsError::Utf8Decode)?,
+                ))
+            })
+            .collect::<Result<Vec<_>, OsError>>()?
+            .into(),
+    ))
 }
 
 #[ffi::bindgen]
