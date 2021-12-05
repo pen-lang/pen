6be10e8bdbd6fb21c3335637dd25ef3ddbd4d9ab
Yota Toyama <raviqqe@gmail.com>
2021-12-05 02:33:39 +0000

    Add more directory utility functions

--- lib/os-async/ffi/library/src/directory.rs
+++ lib/os-async/ffi/library/src/directory.rs
@@ -1,18 +1,27 @@
-use crate::result::FfiResult;
+use crate::{error::OsError, result::FfiResult, utilities};
+use tokio::fs;
 
-#[no_mangle]
-extern "C" fn _pen_os_read_directory(
+#[ffi::bindgen]
+async fn _pen_os_read_directory(
     _path: ffi::ByteString,
 ) -> ffi::Arc<FfiResult<ffi::Arc<ffi::extra::StringArray>>> {
     todo!()
 }
 
-#[no_mangle]
-extern "C" fn _pen_os_create_directory(_path: ffi::ByteString) -> ffi::Arc<FfiResult<ffi::None>> {
-    todo!()
+#[ffi::bindgen]
+async fn _pen_os_create_directory(path: ffi::ByteString) -> ffi::Arc<FfiResult<ffi::None>> {
+    ffi::Arc::new(create_directory(path).await.into())
 }
 
-#[no_mangle]
-extern "C" fn _pen_os_remove_directory(_path: ffi::ByteString) -> ffi::Arc<FfiResult<ffi::None>> {
-    todo!()
+async fn create_directory(path: ffi::ByteString) -> Result<(), OsError> {
+    Ok(fs::create_dir(utilities::decode_path(&path)?).await?)
+}
+
+#[ffi::bindgen]
+async fn _pen_os_remove_directory(path: ffi::ByteString) -> ffi::Arc<FfiResult<ffi::None>> {
+    ffi::Arc::new(remove_directory(path).await.into())
+}
+
+async fn remove_directory(path: ffi::ByteString) -> Result<(), OsError> {
+    Ok(fs::remove_dir(utilities::decode_path(&path)?).await?)
 }
