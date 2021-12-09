2b3c302897583316257b589881d3f2a64816d675
Yota Toyama <raviqqe@gmail.com>
2021-12-09 05:53:40 +0000

    Fix scripts

--- tools/ci/set_up_os.sh
+++ tools/ci/set_up_os.sh
@@ -2,16 +2,9 @@
 
 set -e
 
-llvm_version=13
+brew install jq llvm@13 ninja sccache
 
-brew install jq llvm@$llvm_version ninja sccache
-
-llvm_prefix=$(brew --prefix)/opt/llvm@$llvm_version
-
-echo LLVM_SYS_${llvm_version}0_PREFIX=$llvm_prefix >>$GITHUB_ENV
-echo PATH=$llvm_prefix/bin:$PATH >>$GITHUB_ENV
-
-if [ $RUNNER_OS = Linux ]; then
+if [ $(uname) = Linux ]; then
   sudo apt install libc6-dbg # for valgrind
   brew install valgrind
 fi
