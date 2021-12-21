751aadf7e3740040b8b946f74bc5a720e9e4db7f
Yota Toyama <raviqqe@gmail.com>
2021-12-21 13:21:54 -0800

    Fix ingration tests (#569)
    * Increase integration test timeout
    
    * Fix linting
    
    * Remove unused method

--- lib/hir-mir/src/string_type_configuration.rs
+++ lib/hir-mir/src/string_type_configuration.rs
@@ -1,6 +1,5 @@
 #[cfg(test)]
 use once_cell::sync::Lazy;
-use std::collections::BTreeMap;
 
 #[cfg(test)]
 pub static STRING_TYPE_CONFIGURATION: Lazy<StringTypeConfiguration> =
@@ -11,16 +10,4 @@ pub static STRING_TYPE_CONFIGURATION: Lazy<StringTypeConfiguration> =
 #[derive(Clone, Debug)]
 pub struct StringTypeConfiguration {
     pub equal_function_name: String,
-}
-
-impl StringTypeConfiguration {
-    pub fn qualify(&self, names: &BTreeMap<String, String>) -> Self {
-        Self {
-            equal_function_name: self.qualify_name(&self.equal_function_name, names),
-        }
-    }
-
-    fn qualify_name(&self, name: &str, names: &BTreeMap<String, String>) -> String {
-        names.get(name).cloned().unwrap_or_else(|| name.into())
-    }
 }
