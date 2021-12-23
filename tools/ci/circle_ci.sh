37a7d528e0d9c77470b6d7d0d7f1105cb022157d
Yota Toyama <raviqqe@gmail.com>
2021-12-23 14:25:49 +0000

    Revert "Uninstall llvm@13 from homebrew"
    This reverts commit 953141e8ac6ac8dac15260e1c47c3b8bc19aee97.

--- tools/ci/circle_ci.sh
+++ tools/ci/circle_ci.sh
@@ -8,7 +8,12 @@ if [ $(uname) = Linux ]; then
   sudo snap install --candidate --classic sccache
   curl -fsS https://apt.llvm.org/llvm.sh | sudo bash -s -- 13
 else
-  brew install ninja sccache
+  brew install llvm@13 ninja sccache
+
+  llvm_prefix=$(brew --prefix llvm@13)
+
+  echo export LLVM_SYS_130_PREFIX=$llvm_prefix >>$BASH_ENV
+  echo export PATH=$llvm_prefix/bin:\$PATH >>$BASH_ENV
 fi
 
 curl -fsS https://sh.rustup.rs | sh -s -- -y
