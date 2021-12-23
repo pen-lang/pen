953141e8ac6ac8dac15260e1c47c3b8bc19aee97
Yota Toyama <raviqqe@gmail.com>
2021-12-23 14:16:41 +0000

    Uninstall llvm@13 from homebrew

--- tools/ci/circle_ci.sh
+++ tools/ci/circle_ci.sh
@@ -8,12 +8,7 @@ if [ $(uname) = Linux ]; then
   sudo snap install --candidate --classic sccache
   curl -fsS https://apt.llvm.org/llvm.sh | sudo bash -s -- 13
 else
-  brew install llvm@13 ninja sccache
-
-  llvm_prefix=$(brew --prefix llvm@13)
-
-  echo export LLVM_SYS_130_PREFIX=$llvm_prefix >>$BASH_ENV
-  echo export PATH=$llvm_prefix/bin:\$PATH >>$BASH_ENV
+  brew install ninja sccache
 fi
 
 curl -fsS https://sh.rustup.rs | sh -s -- -y
