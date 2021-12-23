ca255fe92e9adf1df705ae75bc5dd81932552195
Yota Toyama <raviqqe@gmail.com>
2021-12-23 09:52:40 +0000

    Add macos builds

--- tools/ci/circle_ci.sh
+++ tools/ci/circle_ci.sh
@@ -2,8 +2,13 @@
 
 set -ex
 
-sudo apt update --fix-missing
-sudo apt install ninja-build
-sudo snap install --candidate --classic sccache
-curl -fsS https://apt.llvm.org/llvm.sh | sudo bash -s -- 13
+if [ $(uname) = Linux ]; then
+  sudo apt update --fix-missing
+  sudo apt install ninja-build
+  sudo snap install --candidate --classic sccache
+  curl -fsS https://apt.llvm.org/llvm.sh | sudo bash -s -- 13
+else
+  brew install ninja sccache
+fi
+
 curl -fsS https://sh.rustup.rs | sh -s -- -y
