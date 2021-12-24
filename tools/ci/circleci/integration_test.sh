7f5a3cf5197e1de99c346d0c34e045575647bbae
Yota Toyama <raviqqe@gmail.com>
2021-12-23 17:48:11 -0800

    Disable Circle CI builds (#582)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- tools/ci/circleci/integration_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -ex
-
-$(dirname $0)/../../integration_test.sh \
-  --format junit,fileattribute=true \
-  --out /tmp/cucumber \
-  $(circleci tests glob 'features/**/*.feature' |
-    circleci tests split --split-by timings)
