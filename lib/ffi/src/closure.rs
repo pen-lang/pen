a92e75582dd31f8b7193670df72d52d96ae39ef4
Yota Toyama <raviqqe@gmail.com>
2021-12-29 00:00:19 +0000

    Add call method

--- lib/ffi/src/closure.rs
+++ lib/ffi/src/closure.rs
@@ -1,27 +1,51 @@
-use std::os::raw::c_void;
+use std::marker::PhantomData;
 
 #[repr(C)]
 #[derive(Clone)]
-pub struct Closure {
-    entry_pointer: *const c_void,
+pub struct Closure<I, O, F: Copy + Fn(I) -> O> {
+    entry_pointer: F,
     drop_function: extern "C" fn(*mut u8),
     arity: usize,
+    _input: PhantomData<I>,
+    _output: PhantomData<O>,
 }
 
-impl Closure {
-    pub fn new(entry_pointer: *const c_void, arity: usize) -> Self {
+impl<I, O, F: Copy + Fn(I) -> O> Closure<I, O, F> {
+    pub fn new(entry_pointer: F, arity: usize) -> Self {
         Self {
             entry_pointer,
             drop_function: drop_nothing,
             arity,
+            _input: Default::default(),
+            _output: Default::default(),
         }
+    }
+
+    pub fn call(&self, arguments: I) -> O {
+        (self.entry_pointer)(arguments)
     }
 }
 
 extern "C" fn drop_nothing(_: *mut u8) {}
 
-impl Drop for Closure {
+impl<I, O, F: Copy + Fn(I) -> O> Drop for Closure<I, O, F> {
     fn drop(&mut self) {
         (self.drop_function)(self as *mut Self as *mut u8);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn call_closure() {
+        fn foo(x: usize, y: usize) -> usize {
+            x + y
+        }
+
+        let closure = Closure::<(usize, usize), usize, _>::new(foo, 2);
+
+        assert_eq!(closure(42, 42), 84);
     }
 }
