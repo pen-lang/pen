07eeccb06c392e85819672848dd98e41d7ae321d
Yota Toyama <raviqqe@gmail.com>
2021-12-29 00:08:25 +0000

    Revert "Add call method"
    This reverts commit a92e75582dd31f8b7193670df72d52d96ae39ef4.

--- lib/ffi/src/closure.rs
+++ lib/ffi/src/closure.rs
@@ -1,51 +1,27 @@
-use std::marker::PhantomData;
+use std::os::raw::c_void;
 
 #[repr(C)]
 #[derive(Clone)]
-pub struct Closure<I, O, F: Copy + Fn(I) -> O> {
-    entry_pointer: F,
+pub struct Closure {
+    entry_pointer: *const c_void,
     drop_function: extern "C" fn(*mut u8),
     arity: usize,
-    _input: PhantomData<I>,
-    _output: PhantomData<O>,
 }
 
-impl<I, O, F: Copy + Fn(I) -> O> Closure<I, O, F> {
-    pub fn new(entry_pointer: F, arity: usize) -> Self {
+impl Closure {
+    pub fn new(entry_pointer: *const c_void, arity: usize) -> Self {
         Self {
             entry_pointer,
             drop_function: drop_nothing,
             arity,
-            _input: Default::default(),
-            _output: Default::default(),
         }
-    }
-
-    pub fn call(&self, arguments: I) -> O {
-        (self.entry_pointer)(arguments)
     }
 }
 
 extern "C" fn drop_nothing(_: *mut u8) {}
 
-impl<I, O, F: Copy + Fn(I) -> O> Drop for Closure<I, O, F> {
+impl Drop for Closure {
     fn drop(&mut self) {
         (self.drop_function)(self as *mut Self as *mut u8);
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn call_closure() {
-        fn foo(x: usize, y: usize) -> usize {
-            x + y
-        }
-
-        let closure = Closure::<(usize, usize), usize, _>::new(foo, 2);
-
-        assert_eq!(closure(42, 42), 84);
     }
 }
