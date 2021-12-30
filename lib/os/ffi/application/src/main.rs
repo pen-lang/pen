e591e23a3c4e74bdab599169da47523b6b8efadc
Yota Toyama <raviqqe@gmail.com>
2021-12-30 22:49:42 +0000

    Add todo

--- lib/os/ffi/application/src/main.rs
+++ lib/os/ffi/application/src/main.rs
@@ -39,6 +39,7 @@ fn main() {
                 stack.set_context(context);
 
                 let (step, continue_) = trampoline;
+                // TODO Pass a closure environment (or a null pointer) for the first call.
                 unsafe { step(&mut stack, continue_) };
 
                 if let Some(code) = *stack.storage() {
