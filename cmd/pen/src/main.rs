f21b53627bc5d9409f2b655e18a9c138407e060a
Yota Toyama <raviqqe@gmail.com>
2022-01-03 20:42:53 +0000

    Fix build

--- cmd/pen/src/main.rs
+++ cmd/pen/src/main.rs
@@ -29,223 +29,196 @@ fn run() -> Result<(), Box<dyn std::error::Error>> {
         .version(clap::crate_version!())
         .setting(clap::AppSettings::SubcommandRequired)
         .arg(
-            clap::Arg::with_name("verbose")
-                .short("v")
+            clap::Arg::new("verbose")
+                .short('v')
                 .long("verbose")
                 .global(true)
                 .help("Uses verbose output"),
         )
         .subcommand(
-            clap::SubCommand::with_name("build")
+            clap::App::new("build")
                 .about("Builds a package")
                 .arg(build_target_triple_argument().possible_values(CROSS_COMPILE_TARGETS)),
         )
-        .subcommand(clap::SubCommand::with_name("test").about("Tests modules in a package"))
+        .subcommand(clap::App::new("test").about("Tests modules in a package"))
         .subcommand(
-            clap::SubCommand::with_name("create")
+            clap::App::new("create")
                 .about("Creates a package")
                 .arg(
-                    clap::Arg::with_name("library")
-                        .short("l")
+                    clap::Arg::new("library")
+                        .short('l')
                         .long("library")
                         .help("Creates a library package instead of an application one"),
                 )
                 .arg(
-                    clap::Arg::with_name("directory")
+                    clap::Arg::new("directory")
                         .required(true)
                         .help("Sets a package directory"),
                 ),
         )
         .subcommand(
-            clap::SubCommand::with_name("compile")
+            clap::App::new("compile")
                 .setting(clap::AppSettings::Hidden)
                 .about("Compiles a module")
-                .arg(clap::Arg::with_name("source file").required(true))
-                .arg(clap::Arg::with_name("dependency file").required(true))
-                .arg(clap::Arg::with_name("object file").required(true))
-                .arg(clap::Arg::with_name("interface file").required(true))
+                .arg(clap::Arg::new("source file").required(true))
+                .arg(clap::Arg::new("dependency file").required(true))
+                .arg(clap::Arg::new("object file").required(true))
+                .arg(clap::Arg::new("interface file").required(true))
                 .arg(build_target_triple_argument()),
         )
         .subcommand(
-            clap::SubCommand::with_name("compile-main")
+            clap::App::new("compile-main")
                 .setting(clap::AppSettings::Hidden)
                 .about("Compiles a main module")
                 .arg(
-                    clap::Arg::with_name("main function interface file")
-                        .short("f")
+                    clap::Arg::new("main function interface file")
+                        .short('f')
                         .long("main-function-interface-file")
                         .required(true)
                         .takes_value(true),
                 )
-                .arg(clap::Arg::with_name("source file").required(true))
-                .arg(clap::Arg::with_name("dependency file").required(true))
-                .arg(clap::Arg::with_name("object file").required(true))
+                .arg(clap::Arg::new("source file").required(true))
+                .arg(clap::Arg::new("dependency file").required(true))
+                .arg(clap::Arg::new("object file").required(true))
                 .arg(build_target_triple_argument()),
         )
         .subcommand(
-            clap::SubCommand::with_name("compile-prelude")
+            clap::App::new("compile-prelude")
                 .setting(clap::AppSettings::Hidden)
                 .about("Compiles a prelude module")
-                .arg(clap::Arg::with_name("source file").required(true))
-                .arg(clap::Arg::with_name("object file").required(true))
-                .arg(clap::Arg::with_name("interface file").required(true))
+                .arg(clap::Arg::new("source file").required(true))
+                .arg(clap::Arg::new("object file").required(true))
+                .arg(clap::Arg::new("interface file").required(true))
                 .arg(build_target_triple_argument()),
         )
         .subcommand(
-            clap::SubCommand::with_name("compile-test")
+            clap::App::new("compile-test")
                 .setting(clap::AppSettings::Hidden)
                 .about("Compiles a test module")
-                .arg(clap::Arg::with_name("source file").required(true))
-                .arg(clap::Arg::with_name("dependency file").required(true))
-                .arg(clap::Arg::with_name("object file").required(true))
-                .arg(clap::Arg::with_name("test information file").required(true))
+                .arg(clap::Arg::new("source file").required(true))
+                .arg(clap::Arg::new("dependency file").required(true))
+                .arg(clap::Arg::new("object file").required(true))
+                .arg(clap::Arg::new("test information file").required(true))
                 .arg(build_target_triple_argument()),
         )
         .subcommand(
-            clap::SubCommand::with_name("resolve-dependency")
+            clap::App::new("resolve-dependency")
                 .setting(clap::AppSettings::Hidden)
                 .about("Resolves module dependency")
                 .arg(
-                    clap::Arg::with_name("package directory")
-                        .short("p")
+                    clap::Arg::new("package directory")
+                        .short('p')
                         .long("package-directory")
                         .required(true)
                         .takes_value(true),
                 )
                 .arg(
-                    clap::Arg::with_name("output directory")
-                        .short("o")
+                    clap::Arg::new("output directory")
+                        .short('o')
                         .long("output-directory")
                         .required(true)
                         .takes_value(true),
                 )
                 .arg(
-                    clap::Arg::with_name("prelude interface file")
-                        .short("i")
+                    clap::Arg::new("prelude interface file")
+                        .short('i')
                         .long("prelude-interface-file")
-                        .multiple(true)
+                        .multiple_values(true)
                         .number_of_values(1)
                         .takes_value(true),
                 )
-                .arg(clap::Arg::with_name("source file").required(true))
-                .arg(clap::Arg::with_name("object file").required(true))
-                .arg(clap::Arg::with_name("dependency file").required(true))
-                .arg(clap::Arg::with_name("build script dependency file").required(true)),
+                .arg(clap::Arg::new("source file").required(true))
+                .arg(clap::Arg::new("object file").required(true))
+                .arg(clap::Arg::new("dependency file").required(true))
+                .arg(clap::Arg::new("build script dependency file").required(true)),
         )
         .subcommand(
-            clap::SubCommand::with_name("compile-package-test-information")
+            clap::App::new("compile-package-test-information")
                 .setting(clap::AppSettings::Hidden)
                 .about("Compiles a package test information")
                 .arg(
-                    clap::Arg::with_name("package test information file")
-                        .short("o")
+                    clap::Arg::new("package test information file")
+                        .short('o')
                         .required(true)
                         .takes_value(true),
                 )
-                .arg(clap::Arg::with_name("test information file").multiple(true)),
+                .arg(clap::Arg::new("test information file").multiple_values(true)),
         )
         .subcommand(
-            clap::SubCommand::with_name("link-test")
+            clap::App::new("link-test")
                 .setting(clap::AppSettings::Hidden)
                 .about("Links tests")
                 .arg(
-                    clap::Arg::with_name("test file")
-                        .short("o")
+                    clap::Arg::new("test file")
+                        .short('o')
                         .required(true)
                         .takes_value(true),
                 )
                 .arg(
-                    clap::Arg::with_name("package test information file")
-                        .short("i")
+                    clap::Arg::new("package test information file")
+                        .short('i')
                         .required(true)
                         .takes_value(true),
                 )
                 .arg(
-                    clap::Arg::with_name("archive file")
-                        .multiple(true)
+                    clap::Arg::new("archive file")
+                        .multiple_values(true)
                         .required(true)
                         .takes_value(true),
                 ),
         )
         .get_matches()
         .subcommand()
+        .unwrap()
     {
         ("build", matches) => {
-            let matches = matches.unwrap();
-
             package_builder::build(matches.value_of("target"), matches.is_present("verbose"))
         }
         ("test", _) => test_runner::run(),
-        ("create", matches) => {
-            let matches = matches.unwrap();
-
-            package_creator::create(
-                matches.value_of("directory").unwrap(),
-                matches.is_present("library"),
-            )
-        }
-        ("compile", matches) => {
-            let matches = matches.unwrap();
-
-            module_compiler::compile(
-                matches.value_of("source file").unwrap(),
-                matches.value_of("dependency file").unwrap(),
-                matches.value_of("object file").unwrap(),
-                matches.value_of("interface file").unwrap(),
-                matches.value_of("target"),
-            )
-        }
-        ("compile-main", matches) => {
-            let matches = matches.unwrap();
-
-            main_module_compiler::compile(
-                matches.value_of("source file").unwrap(),
-                matches.value_of("dependency file").unwrap(),
-                matches.value_of("object file").unwrap(),
-                matches.value_of("main function interface file").unwrap(),
-                matches.value_of("target"),
-            )
-        }
-        ("compile-prelude", matches) => {
-            let matches = matches.unwrap();
-
-            prelude_module_compiler::compile(
-                matches.value_of("source file").unwrap(),
-                matches.value_of("object file").unwrap(),
-                matches.value_of("interface file").unwrap(),
-                matches.value_of("target"),
-            )
-        }
-        ("compile-test", matches) => {
-            let matches = matches.unwrap();
-
-            test_module_compiler::compile(
-                matches.value_of("source file").unwrap(),
-                matches.value_of("dependency file").unwrap(),
-                matches.value_of("object file").unwrap(),
-                matches.value_of("test information file").unwrap(),
-                matches.value_of("target"),
-            )
-        }
-        ("resolve-dependency", matches) => {
-            let matches = matches.unwrap();
-
-            dependency_resolver::resolve(
-                matches.value_of("source file").unwrap(),
-                matches.value_of("object file").unwrap(),
-                matches.value_of("dependency file").unwrap(),
-                matches.value_of("build script dependency file").unwrap(),
-                &matches
-                    .values_of("prelude interface file")
-                    .unwrap()
-                    .collect::<Vec<_>>(),
-                matches.value_of("package directory").unwrap(),
-                matches.value_of("output directory").unwrap(),
-            )
-        }
+        ("create", matches) => package_creator::create(
+            matches.value_of("directory").unwrap(),
+            matches.is_present("library"),
+        ),
+        ("compile", matches) => module_compiler::compile(
+            matches.value_of("source file").unwrap(),
+            matches.value_of("dependency file").unwrap(),
+            matches.value_of("object file").unwrap(),
+            matches.value_of("interface file").unwrap(),
+            matches.value_of("target"),
+        ),
+        ("compile-main", matches) => main_module_compiler::compile(
+            matches.value_of("source file").unwrap(),
+            matches.value_of("dependency file").unwrap(),
+            matches.value_of("object file").unwrap(),
+            matches.value_of("main function interface file").unwrap(),
+            matches.value_of("target"),
+        ),
+        ("compile-prelude", matches) => prelude_module_compiler::compile(
+            matches.value_of("source file").unwrap(),
+            matches.value_of("object file").unwrap(),
+            matches.value_of("interface file").unwrap(),
+            matches.value_of("target"),
+        ),
+        ("compile-test", matches) => test_module_compiler::compile(
+            matches.value_of("source file").unwrap(),
+            matches.value_of("dependency file").unwrap(),
+            matches.value_of("object file").unwrap(),
+            matches.value_of("test information file").unwrap(),
+            matches.value_of("target"),
+        ),
+        ("resolve-dependency", matches) => dependency_resolver::resolve(
+            matches.value_of("source file").unwrap(),
+            matches.value_of("object file").unwrap(),
+            matches.value_of("dependency file").unwrap(),
+            matches.value_of("build script dependency file").unwrap(),
+            &matches
+                .values_of("prelude interface file")
+                .unwrap()
+                .collect::<Vec<_>>(),
+            matches.value_of("package directory").unwrap(),
+            matches.value_of("output directory").unwrap(),
+        ),
         ("compile-package-test-information", matches) => {
-            let matches = matches.unwrap();
-
             package_test_information_compiler::compile(
                 &matches
                     .values_of("test information file")
@@ -254,25 +227,21 @@ fn run() -> Result<(), Box<dyn std::error::Error>> {
                 matches.value_of("package test information file").unwrap(),
             )
         }
-        ("link-test", matches) => {
-            let matches = matches.unwrap();
-
-            test_linker::link(
-                &matches
-                    .values_of("archive file")
-                    .unwrap()
-                    .collect::<Vec<_>>(),
-                matches.value_of("package test information file").unwrap(),
-                matches.value_of("test file").unwrap(),
-            )
-        }
+        ("link-test", matches) => test_linker::link(
+            &matches
+                .values_of("archive file")
+                .unwrap()
+                .collect::<Vec<_>>(),
+            matches.value_of("package test information file").unwrap(),
+            matches.value_of("test file").unwrap(),
+        ),
         _ => unreachable!(),
     }
 }
 
-fn build_target_triple_argument() -> clap::Arg<'static, 'static> {
-    clap::Arg::with_name("target")
-        .short("t")
+fn build_target_triple_argument() -> clap::Arg<'static> {
+    clap::Arg::new("target")
+        .short('t')
         .long("target")
         .takes_value(true)
         .help("Sets a target triple")
