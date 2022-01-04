16cbc5582eea00c7d8a4ea3c7f50ac96f75b09c0
Yota Toyama <raviqqe@gmail.com>
2022-01-04 02:36:48 +0000

    Fix build of os package

--- lib/os/ffi/application/src/spawn.rs
+++ lib/os/ffi/application/src/spawn.rs
@@ -4,6 +4,6 @@ use tokio::{spawn, task::JoinHandle};
 #[no_mangle]
 extern "C" fn _pen_spawn(
     closure: ffi::Arc<ffi::Closure>,
-) -> ffi::Arc<ffi::Closure<Pin<Box<JoinHandle<ffi::Any>>>>> {
+) -> ffi::Arc<ffi::Closure<Option<Pin<Box<JoinHandle<ffi::Any>>>>>> {
     ffi::future::to_closure(spawn(ffi::future::from_closure(closure)))
 }
