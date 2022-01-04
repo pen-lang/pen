9c916b3642f38469761636050380491316673330
Yota Toyama <raviqqe@gmail.com>
2022-01-04 04:31:21 +0000

    Refactor spawn function implementation in os package

--- lib/os/ffi/application/src/spawn.rs
+++ lib/os/ffi/application/src/spawn.rs
@@ -1,13 +1,9 @@
-use std::{future::Future, intrinsics::transmute};
+use std::future::Future;
 use tokio::spawn;
 
 #[no_mangle]
 extern "C" fn _pen_spawn(closure: ffi::Arc<ffi::Closure>) -> ffi::Arc<ffi::Closure> {
-    let closure = ffi::future::to_closure::<ffi::Any, _>(spawn_and_unwrap(
-        ffi::future::from_closure(closure),
-    ));
-
-    unsafe { transmute(closure) }
+    ffi::future::to_closure(spawn_and_unwrap(ffi::future::from_closure(closure)))
 }
 
 async fn spawn_and_unwrap<F: Future<Output = ffi::Any> + Send + 'static>(future: F) -> ffi::Any {
