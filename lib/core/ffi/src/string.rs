6da49ce955a26c8856d3107f5e08cd8f39b770a3
Yota Toyama <raviqqe@gmail.com>
2022-01-22 04:51:18 +0000

    Make string join more efficient

--- lib/core/ffi/src/string.rs
+++ lib/core/ffi/src/string.rs
@@ -1,6 +1,39 @@
+// TODO Move to a `pen-ffi` crate
+#[repr(C)]
+struct List {
+    node: ffi::Arc<ffi::Closure>,
+}
+
+#[repr(C)]
+struct FirstRest {
+    ok: bool,
+    first: ffi::Any,
+    rest: ffi::Arc<List>,
+}
+
+extern "C" {
+    fn _pen_core_first_rest(xs: ffi::Arc<List>) -> FirstRest;
+    fn _pen_core_to_string(xs: ffi::Any) -> ffi::ByteString;
+}
+
 #[ffi::bindgen]
-fn _pen_join_strings(one: ffi::ByteString, other: ffi::ByteString) -> ffi::ByteString {
-    one.join(&other)
+fn _pen_join_strings(mut one: ffi::Arc<List>, separator: ffi::ByteString) -> ffi::ByteString {
+    let mut first = true;
+    let mut string = vec![];
+
+    loop {
+        let first_rest = unsafe { _pen_core_first_rest(one.clone()) };
+
+        if !first_rest.ok {
+            return string.into();
+        } else if !first {
+            string.extend(separator.as_slice());
+        }
+
+        first = true;
+        string.extend(unsafe { _pen_core_to_string(first_rest.first) }.as_slice());
+        one = first_rest.rest;
+    }
 }
 
 #[ffi::bindgen]
