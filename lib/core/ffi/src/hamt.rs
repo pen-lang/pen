da5b047736525a915126ce59ef181ccd9a474766
Yota Toyama <raviqqe@gmail.com>
2022-01-29 22:11:53 +0000

    Fix linting

--- lib/core/ffi/src/hamt.rs
+++ lib/core/ffi/src/hamt.rs
@@ -1,6 +1,5 @@
 use core::hash::{Hash, Hasher};
 use siphasher::sip::SipHasher;
-use std::intrinsics::transmute;
 
 #[ffi::bindgen]
 fn _pen_core_hamt_hash_key(key: ffi::ByteString, layer: ffi::Number) -> ffi::Number {
@@ -9,7 +8,6 @@ fn _pen_core_hamt_hash_key(key: ffi::ByteString, layer: ffi::Number) -> ffi::Num
     key.hash(&mut hasher);
     (f64::from(layer) as u64).hash(&mut hasher);
 
-    let hash = hasher.finish();
     // Transmute a hash into f64 to return it in a proper register.
-    unsafe { transmute::<_, f64>(hash) }.into()
+    f64::from_bits(hasher.finish()).into()
 }
