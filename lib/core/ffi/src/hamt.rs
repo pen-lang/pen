7ce1a05144787424cf4dc30e9b3d91c6737ce672
Yota Toyama <raviqqe@gmail.com>
2022-01-29 22:08:12 +0000

    Fix hash function

--- lib/core/ffi/src/hamt.rs
+++ lib/core/ffi/src/hamt.rs
@@ -1,12 +1,15 @@
 use core::hash::{Hash, Hasher};
 use siphasher::sip::SipHasher;
+use std::intrinsics::transmute;
 
 #[ffi::bindgen]
-fn _pen_core_hamt_hash_key(key: ffi::ByteString, layer: ffi::Number) -> u64 {
+fn _pen_core_hamt_hash_key(key: ffi::ByteString, layer: ffi::Number) -> ffi::Number {
     let mut hasher = SipHasher::default();
 
     key.hash(&mut hasher);
     (f64::from(layer) as u64).hash(&mut hasher);
 
-    hasher.finish()
+    let hash = hasher.finish();
+    // Transmute a hash into f64 to return it in a proper register.
+    unsafe { transmute::<_, f64>(hash) }.into()
 }
