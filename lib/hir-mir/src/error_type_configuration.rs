fec5cf962448e647682a2cc0cfb22d9f909f96ee
Yota Toyama <raviqqe@gmail.com>
2022-01-30 19:12:01 -0800

    `pen test` subcommand without `Test` package (#714)
    * Add a test to run test without Test
    
    * Use successfully adverbs even before exit status checks
    
    * Rename test
    
    * Modify test function compilation
    
    * Fix test linker
    
    * Configure error source function
    
    * Remove test result module

--- lib/hir-mir/src/error_type_configuration.rs
+++ lib/hir-mir/src/error_type_configuration.rs
@@ -5,9 +5,11 @@ use once_cell::sync::Lazy;
 pub static ERROR_TYPE_CONFIGURATION: Lazy<ErrorTypeConfiguration> =
     Lazy::new(|| ErrorTypeConfiguration {
         error_type_name: "error".into(),
+        source_function_name: "source".into(),
     });
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct ErrorTypeConfiguration {
     pub error_type_name: String,
+    pub source_function_name: String,
 }
