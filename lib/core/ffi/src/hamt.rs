792dc1512ca50dee53324aea37bce6055abf3152
Yota Toyama <raviqqe@gmail.com>
2022-02-04 02:32:06 +0000

    Make map general

--- lib/core/ffi/src/hamt.rs
+++ lib/core/ffi/src/hamt.rs
@@ -2,11 +2,21 @@ use core::hash::{Hash, Hasher};
 use siphasher::sip::SipHasher;
 
 #[ffi::bindgen]
-fn _pen_core_hamt_hash_key(key: ffi::ByteString, layer: ffi::Number) -> ffi::Number {
+fn _pen_core_hamt_hash_key(hash: ffi::Number, layer: ffi::Number) -> ffi::Number {
     let mut hasher = SipHasher::default();
 
-    key.hash(&mut hasher);
+    f64::from(hash).to_bits().hash(&mut hasher);
     (f64::from(layer) as u64).hash(&mut hasher);
+
+    // Transmute a hash into f64 to return it in a proper register.
+    f64::from_bits(hasher.finish()).into()
+}
+
+#[ffi::bindgen]
+fn _pen_core_hamt_hash_string(string: ffi::ByteString) -> ffi::Number {
+    let mut hasher = SipHasher::default();
+
+    string.hash(&mut hasher);
 
     // Transmute a hash into f64 to return it in a proper register.
     f64::from_bits(hasher.finish()).into()
