7259a740fcabfbe46ac021f5a44e81bc891718fc
Yota Toyama <raviqqe@gmail.com>
2022-02-09 15:29:49 +0000

    Fix bug

--- lib/os/ffi/library/src/process.rs
+++ lib/os/ffi/library/src/process.rs
@@ -1,15 +1,11 @@
 use std::{process::exit, time::Duration};
 use tokio::time::sleep;
 
-#[no_mangle]
-extern "C" fn _pen_os_exit(
-    stack: &mut ffi::cps::AsyncStack<ffi::Number>,
-    _: ffi::cps::ContinuationFunction<ffi::None>,
-    code: ffi::Number,
-) -> ffi::cps::Result {
+#[ffi::bindgen]
+async fn _pen_os_exit(code: ffi::Number) {
     // HACK Wait for all I/O buffers to be flushed (hopefully.)
     sleep(Duration::from_millis(50)).await;
 
     // Resolve a main function immediately with an exit code.
-    exit(code)
+    exit(f64::from(code) as i32)
 }
