6687fbdf37e43e0bfccecf3e1baea237ad2613cb
Yota Toyama <raviqqe@gmail.com>
2022-02-09 16:42:01 +0000

    Refactor

--- lib/os/ffi/application/src/main.rs
+++ lib/os/ffi/application/src/main.rs
@@ -4,17 +4,22 @@ mod spawn;
 mod unreachable;
 mod utilities;
 
-type Stack = ffi::cps::AsyncStack<()>;
-type ContinuationFunction = ffi::cps::ContinuationFunction<ffi::None, ()>;
+type ContinuationFunction = ffi::cps::ContinuationFunction<ffi::None>;
 
 #[cfg(not(test))]
 #[link(name = "main")]
 extern "C" {
-    fn _pen_main(stack: &mut Stack, continuation: ContinuationFunction) -> ffi::cps::Result;
+    fn _pen_main(
+        stack: &mut ffi::cps::AsyncStack,
+        continuation: ContinuationFunction,
+    ) -> ffi::cps::Result;
 }
 
 #[cfg(test)]
-unsafe extern "C" fn _pen_main(_: &mut Stack, _: ContinuationFunction) -> ffi::cps::Result {
+unsafe extern "C" fn _pen_main(
+    _: &mut ffi::cps::AsyncStack,
+    _: ContinuationFunction,
+) -> ffi::cps::Result {
     ffi::cps::Result::new()
 }
 
