ba5f703b0e620df1cbfce91d6269aaea4c092e83
Yota Toyama <raviqqe@gmail.com>
2022-02-10 03:16:45 +0000

    Fix valgrind script again

--- tools/utilities.sh
+++ tools/utilities.sh
@@ -1,8 +1,10 @@
 test_valgrind_log() {
   grep 'All heap blocks were freed -- no leaks are possible' $1 ||
-    grep 'definitely lost: 0 bytes in 0 blocks' $1 &&
-    grep 'indirectly lost: 0 bytes in 0 blocks' $1 &&
-    grep '0 errors from 0 contexts' $1
+    (
+      grep 'definitely lost: 0 bytes in 0 blocks' $1 &&
+      grep 'indirectly lost: 0 bytes in 0 blocks' $1 &&
+      grep '0 errors from 0 contexts' $1
+    )
 }
 
 install_nightly_component() {
