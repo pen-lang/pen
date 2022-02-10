cb9b0287134594981c1c1f7f271798f3e5809e17
Yota Toyama <raviqqe@gmail.com>
2022-02-10 03:04:44 +0000

    Fix valgrind script bug

--- tools/utilities.sh
+++ tools/utilities.sh
@@ -1,5 +1,5 @@
 test_valgrind_log() {
-  grep 'All heap blocks were freed -- no leaks are possible' ||
+  grep 'All heap blocks were freed -- no leaks are possible' $1 ||
     grep 'definitely lost: 0 bytes in 0 blocks' $1 &&
     grep 'indirectly lost: 0 bytes in 0 blocks' $1 &&
     grep '0 errors from 0 contexts' $1
