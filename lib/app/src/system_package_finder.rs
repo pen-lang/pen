5c4d3908990f17149b59cf7ac76e43a13888de61
Yota Toyama <raviqqe@gmail.com>
2022-02-10 16:54:14 +0000

    Find multiple system packages

--- lib/app/src/system_package_finder.rs
+++ lib/app/src/system_package_finder.rs
@@ -3,6 +3,7 @@ use crate::{
     external_package_configuration_reader,
     infra::{FilePath, Infrastructure},
 };
+use fnv::FnvHashMap;
 use std::error::Error;
 
 // TODO Use a package type field.
@@ -10,7 +11,7 @@ pub fn find(
     infrastructure: &Infrastructure,
     package_directory: &FilePath,
     output_directory: &FilePath,
-) -> Result<url::Url, Box<dyn Error>> {
+) -> Result<FnvHashMap<String, url::Url>, Box<dyn Error>> {
     let urls = external_package_configuration_reader::read_main(
         infrastructure,
         package_directory,
