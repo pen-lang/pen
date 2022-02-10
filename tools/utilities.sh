079e47d0f302b86d17417fa725caf4e28afa40da
Yota Toyama <raviqqe@gmail.com>
2022-02-10 02:19:12 +0000

    Fix valgrind test

--- tools/utilities.sh
+++ tools/utilities.sh
@@ -1,5 +1,6 @@
 test_valgrind_log() {
-  grep 'definitely lost: 0 bytes in 0 blocks' $1 &&
+  grep 'All heap blocks were freed -- no leaks are possible' ||
+    grep 'definitely lost: 0 bytes in 0 blocks' $1 &&
     grep 'indirectly lost: 0 bytes in 0 blocks' $1 &&
     grep '0 errors from 0 contexts' $1
 }
