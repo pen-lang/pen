0c4966c48c72de0a9c29d48a9c947369fe292bfe
Yota Toyama <raviqqe@gmail.com>
2022-02-13 18:16:24 +0000

    Rename concurrency module

--- lib/os/ffi/application/src/spawn.rs
+++ lib/os/ffi/application/src/spawn.rs
@@ -9,3 +9,8 @@ extern "C" fn _pen_spawn(closure: ffi::Arc<ffi::Closure>) -> ffi::Arc<ffi::Closu
 async fn spawn_and_unwrap(future: impl Future<Output = ffi::Any> + Send + 'static) -> ffi::Any {
     spawn(future).await.unwrap()
 }
+
+#[no_mangle]
+extern "C" fn _pen_join(xs: ffi::Arc<ffi::List>) -> ffi::Arc<ffi::List> {
+    unsafe { _pen_os_join(xs) }
+}
