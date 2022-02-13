5291e3cc6b3d38eac56933ddc490f6b541065068
Yota Toyama <raviqqe@gmail.com>
2022-02-13 15:17:50 -0800

    Fix stdio buffering (#748)

--- lib/os/ffi/library/src/stdio.rs
+++ lib/os/ffi/library/src/stdio.rs
@@ -1,6 +1,15 @@
 use super::utilities;
 use crate::result::FfiResult;
-use tokio::io::{stderr, stdin, stdout};
+use core::ops::DerefMut;
+use once_cell::sync::Lazy;
+use tokio::{
+    io::{stderr, stdin, stdout, Stderr, Stdin, Stdout},
+    sync::Mutex,
+};
+
+static STDIN: Lazy<Mutex<Stdin>> = Lazy::new(|| Mutex::new(stdin()));
+static STDOUT: Lazy<Mutex<Stdout>> = Lazy::new(|| Mutex::new(stdout()));
+static STDERR: Lazy<Mutex<Stderr>> = Lazy::new(|| Mutex::new(stderr()));
 
 #[ffi::bindgen]
 async fn _pen_os_read_stdin() -> ffi::Arc<FfiResult<ffi::ByteString>> {
@@ -10,7 +19,7 @@ async fn _pen_os_read_stdin() -> ffi::Arc<FfiResult<ffi::ByteString>> {
 #[ffi::bindgen]
 async fn _pen_os_read_limit_stdin(limit: ffi::Number) -> ffi::Arc<FfiResult<ffi::ByteString>> {
     ffi::Arc::new(
-        utilities::read_limit(&mut stdin(), f64::from(limit) as usize)
+        utilities::read_limit(STDIN.lock().await.deref_mut(), f64::from(limit) as usize)
             .await
             .into(),
     )
@@ -18,10 +27,18 @@ async fn _pen_os_read_limit_stdin(limit: ffi::Number) -> ffi::Arc<FfiResult<ffi:
 
 #[ffi::bindgen]
 async fn _pen_os_write_stdout(bytes: ffi::ByteString) -> ffi::Arc<FfiResult<ffi::Number>> {
-    ffi::Arc::new(utilities::write(&mut stdout(), bytes).await.into())
+    ffi::Arc::new(
+        utilities::write(STDOUT.lock().await.deref_mut(), bytes)
+            .await
+            .into(),
+    )
 }
 
 #[ffi::bindgen]
 async fn _pen_os_write_stderr(bytes: ffi::ByteString) -> ffi::Arc<FfiResult<ffi::Number>> {
-    ffi::Arc::new(utilities::write(&mut stderr(), bytes).await.into())
+    ffi::Arc::new(
+        utilities::write(STDERR.lock().await.deref_mut(), bytes)
+            .await
+            .into(),
+    )
 }
