4c1cf0161b7b2efec5eb8bfb55957c1d26653612
Yota Toyama <raviqqe@gmail.com>
2022-02-14 01:59:44 +0000

    Prototype _pen_join in os package

--- lib/os/ffi/application/src/concurrency.rs
+++ lib/os/ffi/application/src/concurrency.rs
@@ -1,5 +1,19 @@
+use futures::Stream;
 use std::future::Future;
 use tokio::spawn;
+use tokio_stream::{StreamExt, StreamMap};
+
+#[repr(C)]
+struct FirstRest {
+    ok: bool,
+    first: ffi::Any,
+    rest: ffi::Arc<ffi::List>,
+}
+
+extern "C" {
+    fn _pen_os_first_rest(list: ffi::Arc<ffi::List>) -> ffi::Arc<FirstRest>;
+    fn _pen_os_to_list(list: ffi::Any) -> ffi::Arc<ffi::List>;
+}
 
 #[ffi::bindgen]
 fn _pen_spawn(closure: ffi::Arc<ffi::Closure>) -> ffi::Arc<ffi::Closure> {
@@ -11,6 +25,30 @@ async fn spawn_and_unwrap(future: impl Future<Output = ffi::Any> + Send + 'stati
 }
 
 #[ffi::bindgen]
-async fn _pen_join(_list: ffi::Arc<ffi::List>) -> ffi::Arc<ffi::List> {
-    todo!()
+async fn _pen_join(list: ffi::Arc<ffi::List>) -> ffi::Arc<ffi::List> {
+    let mut streams = vec![];
+    let mut first_rest = unsafe { _pen_os_first_rest(list) };
+
+    while first_rest.ok {
+        streams.push((
+            (),
+            convert_list_to_stream(unsafe { _pen_os_to_list(first_rest.first.clone()) }),
+        ));
+        first_rest = unsafe { _pen_os_first_rest(first_rest.rest.clone()) };
+    }
+
+    convert_stream_to_list(StreamMap::from_iter(streams).map(|(_, value)| value))
+}
+
+// TODO Move this to `pen_ffi::stream::to_stream()`.
+fn convert_list_to_stream(_list: ffi::Arc<ffi::List>) -> impl Stream<Item = ffi::Any> {
+    todo!();
+
+    #[allow(unreachable_code)]
+    futures::stream::iter(vec![])
+}
+
+// TODO Move this to `pen_ffi::stream::from_stream()`.
+fn convert_stream_to_list(_stream: impl Stream<Item = ffi::Any>) -> ffi::Arc<ffi::List> {
+    todo!();
 }
