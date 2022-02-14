003c9e22fb20d334096533a6c9ce093ae469328c
Yota Toyama <raviqqe@gmail.com>
2022-02-14 00:01:14 +0000

    Use ffi::bindgen in os-sync package

--- lib/os-sync/ffi/application/src/concurrency.rs
+++ lib/os-sync/ffi/application/src/concurrency.rs
@@ -2,12 +2,12 @@ extern "C" {
     fn _pen_os_join(xs: ffi::Arc<ffi::List>) -> ffi::Arc<ffi::List>;
 }
 
-#[no_mangle]
-extern "C" fn _pen_spawn(closure: ffi::Arc<ffi::Closure>) -> ffi::Arc<ffi::Closure> {
+#[ffi::bindgen]
+fn _pen_spawn(closure: ffi::Arc<ffi::Closure>) -> ffi::Arc<ffi::Closure> {
     closure
 }
 
-#[no_mangle]
-extern "C" fn _pen_join(xs: ffi::Arc<ffi::List>) -> ffi::Arc<ffi::List> {
+#[ffi::bindgen]
+fn _pen_join(xs: ffi::Arc<ffi::List>) -> ffi::Arc<ffi::List> {
     unsafe { _pen_os_join(xs) }
 }
