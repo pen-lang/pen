03b3ae55486eead3aeaf5b22c06cf8a64a511825
Yota Toyama <raviqqe@gmail.com>
2022-02-19 22:27:33 +0000

    Implement debug

--- lib/hir-mir/src/main_module_configuration.rs
+++ lib/hir-mir/src/main_module_configuration.rs
@@ -1,5 +1,6 @@
 use std::collections::BTreeMap;
 
+#[derive(Clone, Debug)]
 pub struct MainModuleConfiguration {
     pub source_main_function_name: String,
     pub object_main_function_name: String,
@@ -7,6 +8,7 @@ pub struct MainModuleConfiguration {
     pub contexts: BTreeMap<String, ContextConfiguration>,
 }
 
+#[derive(Clone, Debug)]
 pub struct ContextConfiguration {
     pub context_type_name: String,
     pub new_context_function_name: String,
