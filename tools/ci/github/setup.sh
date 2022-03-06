6b193377703721fd01e3c5f7fd172c3fe26deba5
Yota Toyama <raviqqe@gmail.com>
2022-03-06 17:20:29 +0000

    Update LLVM to 14

--- tools/ci/github/setup.sh
+++ tools/ci/github/setup.sh
@@ -2,16 +2,12 @@
 
 set -e
 
-llvm_version=13
+brew install jq ninja sccache
 
-brew install jq llvm@$llvm_version ninja sccache
-
-llvm_prefix=$(brew --prefix)/opt/llvm@$llvm_version
-
-echo LLVM_SYS_${llvm_version}0_PREFIX=$llvm_prefix >>$GITHUB_ENV
 echo PATH=$llvm_prefix/bin:$PATH >>$GITHUB_ENV
 
 if [ $(uname) = Linux ]; then
+  sudo apt install llvm-14
   sudo apt update --fix-missing
   sudo apt install libc6-dbg # for valgrind
   brew install valgrind
