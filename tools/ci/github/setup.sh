290ecde713224e52bf8e8522b22d431be92deb85
Yota Toyama <raviqqe@gmail.com>
2022-03-06 17:32:33 +0000

    Fix install

--- tools/ci/github/setup.sh
+++ tools/ci/github/setup.sh
@@ -8,8 +8,8 @@ echo PATH=$llvm_prefix/bin:$PATH >>$GITHUB_ENV
 
 if [ $(uname) = Linux ]; then
   wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
-  sudo apt install llvm-14
   sudo apt update --fix-missing
+  sudo apt install llvm-14
   sudo apt install libc6-dbg # for valgrind
   brew install valgrind
 fi
