3015a8a88640e06be40f40e0fa908f1099b7ee87
Yota Toyama <raviqqe@gmail.com>
2022-03-06 17:28:05 +0000

    Update

--- tools/ci/github/setup.sh
+++ tools/ci/github/setup.sh
@@ -7,6 +7,7 @@ brew install jq ninja sccache
 echo PATH=$llvm_prefix/bin:$PATH >>$GITHUB_ENV
 
 if [ $(uname) = Linux ]; then
+  wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
   sudo apt install llvm-14
   sudo apt update --fix-missing
   sudo apt install libc6-dbg # for valgrind
