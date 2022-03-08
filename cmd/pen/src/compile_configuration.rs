02f23c71c820c2faa20fde534a57ad1fb17e10c0
Yota Toyama <raviqqe@gmail.com>
2022-03-08 16:36:40 +0000

    Remove keys

--- cmd/pen/src/compile_configuration.rs
+++ cmd/pen/src/compile_configuration.rs
@@ -34,11 +34,11 @@ pub static COMPILE_CONFIGURATION: Lazy<Arc<app::module_compiler::CompileConfigur
                     first_rest_type_name: "_firstRest".into(),
                 },
                 map_type: app::module_compiler::MapTypeConfiguration {
-                    empty_function_name: "_emptyMap".into(),
+                    empty_function_name: "_newMap".into(),
                     equal_function_name: "_equalMaps".into(),
-                    map_type_name: "_anyMap".into(),
+                    map_type_name: "_map".into(),
                     merge_function_name: "_mergeMaps".into(),
-                    remove_function_name: "_removeMap".into(),
+                    remove_function_name: "_deleteMap".into(),
                     set_function_name: "_setMap".into(),
                     hash: app::module_compiler::HashConfiguration {
                         number_hash_function_name: "_hashNumber".into(),
