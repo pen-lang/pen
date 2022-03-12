3575b829dcebd55ee7aa007e95255dc579272a30
Yota Toyama <raviqqe@gmail.com>
2022-03-12 23:08:40 +0000

    Test record hash functions

--- lib/hir-mir/src/transformation/record_hash_function_transformer.rs
+++ lib/hir-mir/src/transformation/record_hash_function_transformer.rs
@@ -9,7 +9,8 @@ use hir::{
 };
 use position::Position;
 
-const RECORD_NAME: &str = "$x";
+const RECORD_HEADER_HASH: f64 = 3.0;
+const RECORD_NAME: &str = "$r";
 
 pub fn transform(context: &CompileContext, module: &Module) -> Result<Module, CompileError> {
     // We cannot define hash functions for record types if hash configuration is not
@@ -74,7 +75,10 @@ fn compile_hash_function_definition(
             vec![Argument::new(RECORD_NAME, record_type.clone())],
             hash_type.clone(),
             type_definition.fields().iter().rev().fold(
-                Ok(Expression::from(Number::new(0.0, position.clone()))),
+                Ok(Expression::from(Number::new(
+                    RECORD_HEADER_HASH,
+                    position.clone(),
+                ))),
                 |expression, field| -> Result<_, CompileError> {
                     Ok(Call::new(
                         Some(
@@ -131,4 +135,127 @@ fn compile_hash_function_declaration(type_definition: &TypeDefinition) -> Declar
 
 fn compile_hash_type(position: &Position) -> Type {
     types::Number::new(position.clone()).into()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::compile_configuration::COMPILE_CONFIGURATION;
+    use hir::test::ModuleFake;
+    use once_cell::sync::Lazy;
+    use position::{test::PositionFake, Position};
+    use pretty_assertions::assert_eq;
+
+    static HASH_TYPE: Lazy<Type> = Lazy::new(|| compile_hash_type(&Position::fake()).into());
+
+    fn transform_module(module: &Module) -> Result<Module, CompileError> {
+        transform(
+            &CompileContext::new(module, COMPILE_CONFIGURATION.clone().into()),
+            module,
+        )
+    }
+
+    #[test]
+    fn compile_hash_function() {
+        let type_definition = TypeDefinition::new(
+            "foo",
+            "foo",
+            vec![
+                types::RecordField::new("x", types::None::new(Position::fake())),
+                types::RecordField::new("y", types::None::new(Position::fake())),
+            ],
+            false,
+            false,
+            false,
+            Position::fake(),
+        );
+        let record_type = types::Record::new(type_definition.name(), Position::fake());
+
+        assert_eq!(
+            transform_module(&Module::empty().set_type_definitions(vec![type_definition.clone()])),
+            Ok(Module::empty()
+                .set_type_definitions(vec![type_definition])
+                .set_definitions(vec![Definition::new(
+                    "foo.$hash",
+                    "foo.$hash",
+                    Lambda::new(
+                        vec![Argument::new(RECORD_NAME, record_type.clone()),],
+                        HASH_TYPE.clone(),
+                        Call::new(
+                            Some(
+                                types::Function::new(
+                                    vec![HASH_TYPE.clone(), HASH_TYPE.clone()],
+                                    HASH_TYPE.clone(),
+                                    Position::fake()
+                                )
+                                .into()
+                            ),
+                            Variable::new(
+                                &COMPILE_CONFIGURATION.map_type.hash.combine_function_name,
+                                Position::fake()
+                            ),
+                            vec![
+                                Call::new(
+                                    Some(
+                                        types::Function::new(
+                                            vec![HASH_TYPE.clone(), HASH_TYPE.clone()],
+                                            HASH_TYPE.clone(),
+                                            Position::fake()
+                                        )
+                                        .into()
+                                    ),
+                                    Variable::new(
+                                        &COMPILE_CONFIGURATION.map_type.hash.combine_function_name,
+                                        Position::fake()
+                                    ),
+                                    vec![
+                                        Number::new(RECORD_HEADER_HASH, Position::fake()).into(),
+                                        Number::new(0.0, Position::fake()).into(),
+                                    ],
+                                    Position::fake()
+                                )
+                                .into(),
+                                Number::new(0.0, Position::fake()).into(),
+                            ],
+                            Position::fake()
+                        ),
+                        Position::fake(),
+                    ),
+                    None,
+                    false,
+                    Position::fake()
+                )]))
+        );
+    }
+
+    #[test]
+    fn compile_hash_function_declaration_for_external_type_definition() {
+        let type_definition = TypeDefinition::new(
+            "foo",
+            "foo",
+            vec![
+                types::RecordField::new("x", types::None::new(Position::fake())),
+                types::RecordField::new("y", types::None::new(Position::fake())),
+            ],
+            false,
+            false,
+            true,
+            Position::fake(),
+        );
+
+        assert_eq!(
+            transform_module(&Module::empty().set_type_definitions(vec![type_definition.clone()])),
+            Ok(Module::empty()
+                .set_type_definitions(vec![type_definition.clone()])
+                .set_declarations(vec![Declaration::new(
+                    "foo.$hash",
+                    types::Function::new(
+                        vec![types::Record::new(type_definition.name(), Position::fake()).into()],
+                        HASH_TYPE.clone(),
+                        Position::fake()
+                    ),
+                    Position::fake()
+                )]))
+        );
+    }
 }
