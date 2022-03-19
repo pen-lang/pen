568f35415bdb0426e0eb5f32e6fb2bc3510c0f4c
Yota Toyama <raviqqe@gmail.com>
2022-03-19 17:32:14 +0000

    Extend view

--- lib/core/ffi/src/view.rs
+++ lib/core/ffi/src/view.rs
@@ -1,24 +1,24 @@
 #[ffi::bindgen]
 fn _pen_core_view_has_prefix(
     string: ffi::ByteString,
-    index: ffi::Number,
+    start: ffi::Number,
+    end: ffi::Number,
     prefix: ffi::ByteString,
 ) -> ffi::Boolean {
     let string = string.as_slice();
-    let index = f64::from(index) as usize - 1;
+    let start = f64::from(start) as usize - 1;
     let prefix = prefix.as_slice();
 
-    (&string[index..(index + prefix.len()).min(string.len())] == prefix).into()
+    (&string[start..(start + prefix.len()).min(f64::from(end) as usize)] == prefix).into()
 }
 
 #[ffi::bindgen]
-fn _pen_core_view_length(string: ffi::ByteString, index: ffi::Number) -> ffi::Number {
-    (string.as_slice()[f64::from(index) as usize - 1..].len() as f64).into()
-}
-
-#[ffi::bindgen]
-fn _pen_core_view_to_string(string: ffi::ByteString, index: ffi::Number) -> ffi::ByteString {
-    string.as_slice()[f64::from(index) as usize - 1..].into()
+fn _pen_core_view_to_string(
+    string: ffi::ByteString,
+    start: ffi::Number,
+    end: ffi::Number,
+) -> ffi::ByteString {
+    string.as_slice()[f64::from(start) as usize - 1..f64::from(end) as usize].into()
 }
 
 #[cfg(test)]
@@ -30,6 +30,7 @@ mod tests {
         assert!(bool::from(_pen_core_view_has_prefix(
             "foo bar baz".into(),
             5.0.into(),
+            8.0.into(),
             "bar".into()
         )));
     }
@@ -39,22 +40,15 @@ mod tests {
         assert!(!bool::from(_pen_core_view_has_prefix(
             "foo".into(),
             1.0.into(),
+            3.0.into(),
             "bar".into()
         )));
     }
 
     #[test]
-    fn length() {
-        assert_eq!(
-            _pen_core_view_length("foo bar".into(), 5.0.into(),),
-            3.0.into()
-        );
-    }
-
-    #[test]
     fn to_string() {
         assert_eq!(
-            _pen_core_view_to_string("foo bar".into(), 5.0.into(),),
+            _pen_core_view_to_string("foo bar baz".into(), 5.0.into(), 7.0.into()),
             "bar".into()
         );
     }
