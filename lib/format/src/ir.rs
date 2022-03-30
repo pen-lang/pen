a2be4c96da68118d8bf5b4026a981bf064d13ec0
Yota Toyama <raviqqe@gmail.com>
2022-03-30 14:02:01 +0000

    Add ir module

--- lib/format/src/ir.rs
+++ lib/format/src/ir.rs
@@ -1,38 +1,7 @@
 pub mod build;
 pub mod format;
+mod ir;
 mod utils;
 
+pub use ir::*;
 pub use utils::*;
-
-// https://homepages.inf.ed.ac.uk/wadler/papers/prettier/prettier.pdf
-
-#[derive(Clone, Debug, PartialEq)]
-pub enum Document {
-    // TODO Remove this.
-    Comment(String),
-    Sequence(Vec<Document>),
-    // TODO Replace this with a function?
-    Flatten(Box<Document>),
-    HardLine,
-    SoftLine,
-    Indent(Box<Document>),
-    String(String),
-}
-
-impl From<&str> for Document {
-    fn from(string: &str) -> Self {
-        Self::String(string.into())
-    }
-}
-
-impl From<String> for Document {
-    fn from(string: String) -> Self {
-        Self::String(string)
-    }
-}
-
-impl From<Vec<Document>> for Document {
-    fn from(documents: Vec<Document>) -> Self {
-        Self::Sequence(documents)
-    }
-}
---
+++ lib/format/src/ir/ir.rs
@@ -0,0 +1,32 @@
+// https://homepages.inf.ed.ac.uk/wadler/papers/prettier/prettier.pdf
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum Document {
+    // TODO Remove this.
+    Comment(String),
+    Sequence(Vec<Document>),
+    // TODO Replace this with a function?
+    Flatten(Box<Document>),
+    HardLine,
+    SoftLine,
+    Indent(Box<Document>),
+    String(String),
+}
+
+impl From<&str> for Document {
+    fn from(string: &str) -> Self {
+        Self::String(string.into())
+    }
+}
+
+impl From<String> for Document {
+    fn from(string: String) -> Self {
+        Self::String(string)
+    }
+}
+
+impl From<Vec<Document>> for Document {
+    fn from(documents: Vec<Document>) -> Self {
+        Self::Sequence(documents)
+    }
+}
