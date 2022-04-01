1d580a3f5480b3623a50a66cc0637f546d7b545d
Yota Toyama <raviqqe@gmail.com>
2022-04-01 04:47:14 +0000

    Update

--- lib/format/src/ir/ir.rs
+++ lib/format/src/ir/ir.rs
@@ -2,8 +2,15 @@ use std::rc::Rc;
 
 // https://homepages.inf.ed.ac.uk/wadler/papers/prettier/prettier.pdf
 //
+// Unlike the Wadler's algorithm or some other formatters like prettier, we do
+// not need to search the best format given source codes. For example, we do
+// not have any "group" combinator.
+//
+// However, we are rather given the "best" format by all information available
+// in the source codes like `gofmt`.
+//
 // We need soft-line and if-break nodes to make nodes totally agnostic about if
-// parent nodes are broken or not.
+// parent nodes are broken or not. But that also makes IR more complex.
 // (e.g. handling trailing commas in function calls)
 
 #[derive(Clone, Debug, PartialEq)]
