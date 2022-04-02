4eac118f0cc1b40580eecd8d572bb6fdb8f048a8
Yota Toyama <raviqqe@gmail.com>
2022-04-02 19:00:42 +0000

    Compile type definitions

--- lib/doc/src/lib.rs
+++ lib/doc/src/lib.rs
@@ -2,15 +2,46 @@ mod ir;
 mod markdown;
 
 use ast::*;
-use ir::*;
 use ir::build::*;
+use ir::*;
 
 pub fn generate(path: &ModulePath, module: &Module, comments: &[Comment]) -> String {
     markdown::generate(&compile_module(path, module, comments))
 }
 
 fn compile_module(path: &ModulePath, module: &Module, comments: &[Comment]) -> Section {
-    section(text([code(format!("{}", path))]), [], [])
+    section(
+        text([code(format!("{}", path))]),
+        [],
+        [
+            compile_type_definitions(module.type_definitions(), comments),
+            compile_definitions(module.definitions(), comments),
+        ],
+    )
+}
+
+fn compile_type_definitions(definitions: &[TypeDefinition], comments: &[Comment]) -> Section {
+    section(
+        text([normal("Types")]),
+        if definitions.is_empty() {
+            vec![text([normal("No types are defined.")]).into()]
+        } else {
+            vec![]
+        },
+        [],
+    )
+}
+
+fn compile_definitions(definitions: &[Definition], comments: &[Comment]) -> Section {
+    section(
+        text([normal("Functions")]),
+        if definitions.is_empty() {
+            vec![text([normal("No functions are defined.")]).into()]
+        } else {
+            vec![]
+        },
+        [],
+    )
 }
 
 #[cfg(test)]
@@ -39,6 +70,14 @@ mod tests {
                 indoc!(
                     "
                     # `Foo'Bar`
+
+                    ## Types
+
+                    No types are defined.
+
+                    ## Functions
+
+                    No functions are defined.
                     "
                 )
             );
