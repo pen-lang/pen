e480ef721918812a41c33d2fc9bf92ea3288cadb
Yota Toyama <raviqqe@gmail.com>
2022-04-02 23:50:13 +0000

    Compile comment

--- lib/doc/src/lib.rs
+++ lib/doc/src/lib.rs
@@ -1,8 +1,12 @@
+#![allow(unstable_name_collisions)]
+
 mod ir;
 mod markdown;
 
 use ast::*;
 use ir::{build::*, *};
+use itertools::Itertools;
+use position::Position;
 
 pub fn generate(path: &ModulePath, module: &Module, comments: &[Comment]) -> String {
     markdown::generate(&compile_module(path, module, comments))
@@ -35,10 +39,16 @@ fn compile_type_definitions(definitions: &[TypeDefinition], comments: &[Comment]
 
 fn compile_type_definition(definition: &TypeDefinition, comments: &[Comment]) -> Section {
     match definition {
-        TypeDefinition::RecordDefinition(definition) => {
-            section(text([code(definition.name())]), [], [])
-        }
-        TypeDefinition::TypeAlias(alias) => section(text([code(alias.name())]), [], []),
+        TypeDefinition::RecordDefinition(definition) => section(
+            text([code(definition.name())]),
+            compile_block_comment(definition.position(), comments),
+            [],
+        ),
+        TypeDefinition::TypeAlias(alias) => section(
+            text([code(alias.name())]),
+            compile_block_comment(alias.position(), comments),
+            [],
+        ),
     }
 }
 
@@ -50,7 +60,37 @@ fn compile_definitions(definitions: &[Definition], comments: &[Comment]) -> Sect
         } else {
             vec![]
         },
+        definitions
+            .iter()
+            .map(|definition| compile_definition(definition, comments)),
+    )
+}
+
+fn compile_definition(definition: &Definition, comments: &[Comment]) -> Section {
+    section(
+        text([code(definition.name())]),
+        compile_block_comment(definition.position(), comments),
         [],
+    )
+}
+
+fn compile_block_comment(position: &Position, comments: &[Comment]) -> Option<Paragraph> {
+    let comments = &comments[..comments
+        .partition_point(|comment| comment.position().line_number() < position.line_number())];
+
+    if comments.is_empty() {
+        return None;
+    }
+
+    Some(
+        text(
+            comments
+                .iter()
+                .map(|comment| normal(comment.line().trim_end()))
+                .intersperse(normal("\n"))
+                .collect::<Vec<_>>(),
+        )
+        .into(),
     )
 }
 
@@ -59,6 +99,10 @@ mod tests {
     use super::*;
     use indoc::indoc;
     use position::{test::PositionFake, Position};
+
+    fn line_position(line_number: usize) -> Position {
+        Position::new("", line_number, 1, "")
+    }
 
     mod module {
         use super::*;
@@ -100,7 +144,7 @@ mod tests {
         }
 
         #[test]
-        fn generate_empty() {
+        fn generate_record_definition() {
             assert_eq!(
                 generate(
                     &RecordDefinition::new("Foo", vec![], Position::fake()).into(),
@@ -109,6 +153,92 @@ mod tests {
                 indoc!(
                     "
                     # `Foo`
+                    "
+                )
+            );
+        }
+    }
+
+    mod comment {
+        use super::*;
+
+        fn generate(definition: &TypeDefinition, comments: &[Comment]) -> String {
+            markdown::generate(&compile_type_definition(definition, comments))
+        }
+
+        #[test]
+        fn generate_comment() {
+            assert_eq!(
+                generate(
+                    &RecordDefinition::new("Foo", vec![], line_position(2)).into(),
+                    &[Comment::new("foo", line_position(1))]
+                ),
+                indoc!(
+                    "
+                    # `Foo`
+
+                    foo
+                    "
+                )
+            );
+        }
+
+        #[test]
+        fn trim_trailing_spaces() {
+            assert_eq!(
+                generate(
+                    &RecordDefinition::new("Foo", vec![], line_position(2)).into(),
+                    &[Comment::new("foo ", line_position(1))]
+                ),
+                indoc!(
+                    "
+                    # `Foo`
+
+                    foo
+                    "
+                )
+            );
+        }
+
+        #[test]
+        fn generate_comment_of_multiple_lines() {
+            assert_eq!(
+                generate(
+                    &RecordDefinition::new("Foo", vec![], line_position(3)).into(),
+                    &[
+                        Comment::new("foo", line_position(1)),
+                        Comment::new("bar", line_position(2))
+                    ]
+                ),
+                indoc!(
+                    "
+                    # `Foo`
+
+                    foo
+                    bar
+                    "
+                )
+            );
+        }
+
+        #[test]
+        fn generate_comment_of_multiple_paragraphs() {
+            assert_eq!(
+                generate(
+                    &RecordDefinition::new("Foo", vec![], line_position(4)).into(),
+                    &[
+                        Comment::new("foo", line_position(1)),
+                        Comment::new("", line_position(2)),
+                        Comment::new("bar", line_position(3))
+                    ]
+                ),
+                indoc!(
+                    "
+                    # `Foo`
+
+                    foo
+
+                    bar
                     "
                 )
             );
