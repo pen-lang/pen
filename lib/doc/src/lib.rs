0de502d4ea85c2534526cd365e0aea4e8dbcacd7
Yota Toyama <raviqqe@gmail.com>
2022-04-02 18:46:06 +0000

    Add module test

--- lib/doc/src/lib.rs
+++ lib/doc/src/lib.rs
@@ -4,10 +4,43 @@ mod markdown;
 use ast::*;
 use ir::*;
 
-pub fn generate(module: &Module, _comments: &[Comment]) -> String {
-    markdown::generate(&compile_module(module))
+pub fn generate(path: &ModulePath, module: &Module, comments: &[Comment]) -> String {
+    markdown::generate(&compile_module(path, module, comments))
 }
 
-fn compile_module(_module: &Module) -> Section {
+fn compile_module(path: &ModulePath, module: &Module, comments: &[Comment]) -> Section {
     todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use indoc::indoc;
+    use position::test::PositionFake;
+    use position::Position;
+
+    mod module {
+
+        use super::*;
+
+        fn generate(path: &ModulePath, module: &Module, comments: &[Comment]) -> String {
+            markdown::generate(&compile_module(path, module, comments))
+        }
+
+        #[test]
+        fn generate_empty() {
+            assert_eq!(
+                generate(
+                    &ExternalModulePath::new("Foo", vec!["Bar".into()]).into(),
+                    &Module::new(vec![], vec![], vec![], vec![], Position::fake()),
+                    &[]
+                ),
+                indoc!(
+                    "
+                    # `Foo'Bar`
+                    "
+                )
+            );
+        }
+    }
 }
