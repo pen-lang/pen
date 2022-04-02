cd184d19fba90d0bfb66c1a32464f7120f188b53
Yota Toyama <raviqqe@gmail.com>
2022-04-02 18:04:51 +0000

    Add compile module function

--- lib/doc/src/lib.rs
+++ lib/doc/src/lib.rs
@@ -2,7 +2,12 @@ mod ir;
 mod markdown;
 
 use ast::*;
+use ir::*;
 
-pub fn generate(_module: &Module, _comments: &[Comment]) -> String {
+pub fn generate(module: &Module, _comments: &[Comment]) -> String {
+    markdown::generate(&compile_module(module))
+}
+
+fn compile_module(_module: &Module) -> Section {
     todo!()
 }
