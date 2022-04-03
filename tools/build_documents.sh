9399c26139d868725d19a594ee2085c8860b178c
Yota Toyama <raviqqe@gmail.com>
2022-04-03 22:56:56 +0000

    Build documentation

--- tools/build_documents.sh
+++ tools/build_documents.sh
@@ -4,6 +4,18 @@ set -ex
 
 base_directory=$(dirname $0)/..
 
+generate_package_documentation() {
+  (
+    cd $base_directory/$1
+    pen document $2
+  )
+}
+
+generate_package_documentation lib/core Core
+generate_package_documentation lib/os Os
+generate_package_documentation lib/os-sync OsSync
+generate_package_documentation lib/test Test
+
 go run github.com/raviqqe/gherkin2markdown \
   $base_directory/features \
   $base_directory/doc/src/examples
