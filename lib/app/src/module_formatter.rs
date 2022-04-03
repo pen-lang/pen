dbe303b2a2035728eafa7dc5ed3ae07db97d4eee
Yota Toyama <raviqqe@gmail.com>
2022-04-03 20:12:47 +0000

    Revert accidental change

--- lib/app/src/module_formatter.rs
+++ lib/app/src/module_formatter.rs
@@ -2,12 +2,8 @@ use parse::{parse, parse_comments};
 use std::error::Error;
 
 pub fn format(source: &str, path: &str) -> Result<String, Box<dyn Error>> {
-    let a = parse(source, path)?;
-    let c = parse_comments(source, path)?;
-
-    for _ in 0..10000 {
-        format::format(&a, &c);
-    }
-
-    Ok(format::format(&a, &c))
+    Ok(format::format(
+        &parse(source, path)?,
+        &parse_comments(source, path)?,
+    ))
 }
