5cdff60f4324ba018a7605d80ce79ebb9b676756
Yota Toyama <raviqqe@gmail.com>
2022-04-03 18:28:37 +0000

    Output document

--- cmd/pen/src/package_documentation_generator.rs
+++ cmd/pen/src/package_documentation_generator.rs
@@ -1,5 +1,9 @@
 use crate::{infrastructure, main_package_directory_finder};
-use std::{error::Error, sync::Arc};
+use std::{
+    error::Error,
+    io::{stdout, Write},
+    sync::Arc,
+};
 
 const LANGUAGE_TAG: &str = "pen";
 
@@ -9,11 +13,14 @@ pub fn generate(package_name: &str) -> Result<(), Box<dyn Error>> {
         main_package_directory.clone(),
     ));
 
-    app::package_documentation_generator::generate(
-        &infrastructure::create(file_path_converter.clone(), &main_package_directory)?,
-        package_name,
-        &file_path_converter.convert_to_file_path(&main_package_directory)?,
-        LANGUAGE_TAG,
+    stdout().write_all(
+        app::package_documentation_generator::generate(
+            &infrastructure::create(file_path_converter.clone(), &main_package_directory)?,
+            package_name,
+            &file_path_converter.convert_to_file_path(&main_package_directory)?,
+            LANGUAGE_TAG,
+        )?
+        .as_bytes(),
     )?;
 
     Ok(())
