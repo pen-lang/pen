dba0a6c9df0a1a2bf4f66f436261c2984fd0b43a
Yota Toyama <raviqqe@gmail.com>
2022-04-04 03:48:23 +0000

    Fix build documents script

--- tools/build_documents.sh
+++ tools/build_documents.sh
@@ -2,8 +2,7 @@
 
 set -ex
 
-base_directory=$(dirname $0)/..
-document_directory=$base_directory/doc/src/references/standard-packages
+document_directory=doc/src/references/standard-packages
 
 os_package_description() {
   echo This package provides types and functions to interact with operating systems.
@@ -15,8 +14,14 @@ os_sync_package_description() {
   echo Functions in this package are synchronous differently from an '`Os`' package.
 }
 
+cd $(dirname $0)/..
+
+tools/build.sh
+
+export PATH=$PWD/target/release:$PATH
+
 (
-  cd $base_directory/lib/core
+  cd lib/core
   pen document \
     --name Core \
     --url pen:///core \
@@ -24,7 +29,7 @@ os_sync_package_description() {
 ) >$document_directory/core.md
 
 (
-  cd $base_directory/lib/os
+  cd lib/os
   pen document \
     --name Os \
     --url pen:///os \
@@ -32,7 +37,7 @@ os_sync_package_description() {
 ) >$document_directory/os.md
 
 (
-  cd $base_directory/lib/os-sync
+  cd lib/os-sync
   pen document \
     --name OsSync \
     --url pen:///os-sync \
@@ -40,15 +45,13 @@ os_sync_package_description() {
 ) >$document_directory/os-sync.md
 
 (
-  cd $base_directory/lib/test
+  cd lib/test
   pen document \
     --name Test \
     --url pen:///test \
     --description "This package provides test utilities."
 ) >$document_directory/test.md
 
-go run github.com/raviqqe/gherkin2markdown \
-  $base_directory/features \
-  $base_directory/doc/src/examples
+go run github.com/raviqqe/gherkin2markdown features doc/src/examples
 
-curl -fsSL https://pen-lang.s3.us-west-1.amazonaws.com/icon.svg >$base_directory/doc/src/favicon.svg
+curl -fsSL https://pen-lang.s3.us-west-1.amazonaws.com/icon.svg >doc/src/favicon.svg
