10fda85ea0b8b7863921f6a6a037cd2a5d5182c7
Yota Toyama <raviqqe@gmail.com>
2022-04-06 14:46:24 +0000

    Refactor module interfaces

--- lib/interface/src/lib.rs
+++ lib/interface/src/lib.rs
@@ -1,9 +1,9 @@
 mod declaration;
 mod module;
+mod record_definition;
 mod type_alias;
-mod type_definition;
 
 pub use declaration::*;
 pub use module::*;
+pub use record_definition::*;
 pub use type_alias::*;
-pub use type_definition::*;
