58c11ac89c3daa64c3d88e16d895accde460f19a
Yota Toyama <raviqqe@gmail.com>
2022-04-09 02:28:18 +0000

    Revert "Use LLVM 14 in macOS"
    This reverts commit 50304ca028828ade3b2ccc1fe99163a785ae01e9.

--- tools/ci/github/setup.sh
+++ tools/ci/github/setup.sh
@@ -10,11 +10,8 @@ if [ $(uname) = Linux ]; then
   curl -fsSL https://apt.llvm.org/llvm.sh | sudo bash /dev/stdin 14
   llvm_prefix=/usr/lib/llvm-14
 else
-  curl -fsSL https://github.com/llvm/llvm-project/releases/download/llvmorg-14.0.0/clang+llvm-14.0.0-x86_64-apple-darwin.tar.xz >llvm.tar.xz
-  tar xf llvm.tar.xz
-  rm llvm.tar.xz
-  mv clang+llvm* /tmp/llvm
-  llvm_prefix=/tmp/llvm
+  brew install llvm@$llvm_version
+  llvm_prefix=$(brew --prefix)/opt/llvm@$llvm_version
 fi
 
 echo LLVM_SYS_${llvm_version}0_PREFIX=$llvm_prefix >>$GITHUB_ENV
