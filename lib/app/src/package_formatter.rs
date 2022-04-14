9a0b18b3aac69f7f652aa6c17f3f684a2b5e5df4
Yota Toyama <raviqqe@gmail.com>
2022-04-14 04:12:13 +0000

    Format test modules

--- lib/app/src/package_formatter.rs
+++ lib/app/src/package_formatter.rs
@@ -1,6 +1,6 @@
 use crate::{
     infra::{FilePath, Infrastructure},
-    module_finder, module_formatter,
+    module_finder, module_formatter, test_module_finder,
 };
 use std::error::Error;
 
@@ -8,7 +8,10 @@ pub fn format(
     infrastructure: &Infrastructure,
     package_directory: &FilePath,
 ) -> Result<(), Box<dyn Error>> {
-    for paths in module_finder::find(infrastructure, package_directory)? {
+    for paths in module_finder::find(infrastructure, package_directory)?
+        .into_iter()
+        .chain(test_module_finder::find(infrastructure, package_directory)?)
+    {
         infrastructure.file_system.write(
             &paths,
             module_formatter::format(
