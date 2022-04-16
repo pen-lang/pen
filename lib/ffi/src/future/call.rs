e9e00b2ed9460703710f3e39e1cc465f27c70fb6
Yota Toyama <raviqqe@gmail.com>
2022-04-16 05:33:50 +0000

    Fix call macro

--- lib/ffi/src/future/call.rs
+++ lib/ffi/src/future/call.rs
@@ -4,7 +4,7 @@ macro_rules! call {
     (fn($($argument_type:ty),*) -> $result_type:ty, $closure:expr, $($argument:expr),* $(,)?) => {
         async {
             use core::{intrinsics::transmute, task::Poll};
-            use futures::future::poll_fn;
+            use $crate::future::__private::poll_fn;
             use $crate::{cps, Arc, Closure};
 
             const INITIAL_STACK_CAPACITY: usize = 64;
@@ -54,6 +54,10 @@ macro_rules! call {
             .await
         }
     };
+}
+
+pub mod __private {
+    pub use futures::future::poll_fn;
 }
 
 #[cfg(test)]
