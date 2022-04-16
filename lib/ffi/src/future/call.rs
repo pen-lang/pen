810d8dd332aaad989c8633001a9f25d435ba60c4
Yota Toyama <raviqqe@gmail.com>
2022-04-16 02:10:15 +0000

    Add call macro

---
+++ lib/ffi/src/future/call.rs
@@ -0,0 +1,114 @@
+#[macro_export]
+macro_rules! call {
+    (fn($($argument_type:ty),*) -> $result_type:ty, $closure:expr, $($argument:expr),*) => {
+        async {
+            use core::{intrinsics::transmute, task::Poll};
+            use futures::future::poll_fn;
+            use $crate::{
+                cps,
+                Arc, Closure,
+            };
+
+            type AsyncStack = cps::AsyncStack<$result_type>;
+
+            type StepFunction = cps::StepFunction<$result_type, $result_type>;
+
+            type ContinuationFunction = cps::ContinuationFunction<$result_type, $result_type>;
+
+            type InitialStepFunction<C> = extern "C" fn(
+                stack: &mut AsyncStack,
+                continuation: ContinuationFunction,
+                closure: Arc<Closure<C>>,
+                $($argument_type),*
+            ) -> cps::Result;
+
+            extern "C" fn resolve(stack: &mut AsyncStack, value: $result_type) -> cps::Result {
+                stack.resolve(value);
+
+                cps::Result::new()
+            }
+
+            const INITIAL_STACK_CAPACITY: usize = 64;
+
+            let mut trampoline: Option<(StepFunction, ContinuationFunction)> = None;
+            let mut stack = AsyncStack::new(INITIAL_STACK_CAPACITY);
+
+            poll_fn(move |context| {
+                stack.run_with_context(context, |stack| {
+                    if let Some((step, continue_)) = trampoline {
+                        step(stack, continue_);
+                    } else {
+                        unsafe {
+                            let entry_function = transmute::<_, InitialStepFunction<_>>(
+                                $closure.entry_function(),
+                            );
+                            entry_function(stack, resolve, $closure.clone(), $($argument),*);
+                        }
+                    }
+                });
+
+                if let Some(value) = stack.resolved_value() {
+                    value.into()
+                } else {
+                    trampoline = Some(stack.resume().unwrap());
+                    Poll::Pending
+                }
+            })
+            .await
+        }
+    };
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        cps::{self, AsyncStack, ContinuationFunction},
+        Arc, Closure, Number,
+    };
+
+    extern "C" fn thunk_entry_function(
+        stack: &mut AsyncStack,
+        continue_: ContinuationFunction<Number>,
+        closure: Arc<Closure<f64>>,
+    ) -> cps::Result {
+        continue_(stack, unsafe { *closure.payload() }.into())
+    }
+
+    #[tokio::test]
+    async fn convert_thunk() {
+        let value = 42.0;
+
+        assert_eq!(
+            call!(
+                fn() -> Number,
+                Arc::new(Closure::new(thunk_entry_function as *const u8, value)),
+            )
+            .await,
+            value.into()
+        );
+    }
+
+    extern "C" fn closure_entry_function(
+        stack: &mut AsyncStack,
+        continue_: ContinuationFunction<Number>,
+        _closure: Arc<Closure<()>>,
+        x: Number,
+    ) -> cps::Result {
+        continue_(stack, x)
+    }
+
+    #[tokio::test]
+    async fn convert_closure() {
+        let value = 42.0;
+
+        assert_eq!(
+            call!(
+                fn(Number) -> Number,
+                Arc::new(Closure::new(closure_entry_function as *const u8, ())),
+                value.into()
+            )
+            .await,
+            value.into()
+        );
+    }
+}
