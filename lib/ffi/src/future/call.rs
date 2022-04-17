fccce82cc557ee710308862958c2c814dc9e67f4
Yota Toyama <raviqqe@gmail.com>
2022-04-17 18:55:53 +0000

    Comment on unreachable

--- lib/ffi/src/future/call.rs
+++ lib/ffi/src/future/call.rs
@@ -45,7 +45,7 @@ macro_rules! call {
                 } else if let Some((step, continue_)) = trampoline.take() {
                     stack.run_with_context(context, |stack| step(stack, continue_));
                 } else {
-                    unreachable!()
+                    unreachable!("suspension did not fill trampoline functions")
                 }
 
                 if let Some(value) = stack.resolved_value() {
