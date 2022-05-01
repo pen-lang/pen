e33e55d49765cfaa4558b820daf2c364d59f0cd4
Yota Toyama <raviqqe@gmail.com>
2022-05-01 20:01:11 +0000

    Format files

--- packages/sql/ffi/src/error.rs
+++ packages/sql/ffi/src/error.rs
@@ -1,5 +1,7 @@
-use std::error::Error;
-use std::fmt::{self, Display, Formatter};
+use std::{
+    error::Error,
+    fmt::{self, Display, Formatter},
+};
 
 #[derive(Clone, Debug)]
 pub enum SqlError {
