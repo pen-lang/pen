805cc5db33e80cc6798448ed753fccdde06824bc
Yota Toyama <raviqqe@gmail.com>
2022-05-09 04:02:53 +0000

    Rename reuse id

--- lib/mir/src/ir/reuse_record.rs
+++ lib/mir/src/ir/reuse_record.rs
@@ -2,20 +2,20 @@ use super::Record;
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct ReuseRecord {
-    reuse_id: String,
+    block_id: String,
     record: Record,
 }
 
 impl ReuseRecord {
-    pub fn new(reuse_id: impl Into<String>, record: Record) -> Self {
+    pub fn new(block_id: impl Into<String>, record: Record) -> Self {
         Self {
-            reuse_id: reuse_id.into(),
+            block_id: block_id.into(),
             record,
         }
     }
 
-    pub fn reuse_id(&self) -> &str {
-        &self.reuse_id
+    pub fn block_id(&self) -> &str {
+        &self.block_id
     }
 
     pub fn record(&self) -> &Record {
