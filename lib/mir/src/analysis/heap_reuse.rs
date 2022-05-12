aeb465c5ae14915de1bf79d8043288d1b06d11ec
Yota Toyama <raviqqe@gmail.com>
2022-05-12 15:52:53 +0000

    Reuse heap

--- lib/mir/src/analysis/heap_reuse.rs
+++ lib/mir/src/analysis/heap_reuse.rs
@@ -2,7 +2,8 @@ mod conversion;
 mod error;
 mod heap_block_set;
 
-use self::{conversion::convert, error::ReuseError};
+use self::conversion::convert;
+pub use self::error::ReuseError;
 use crate::ir::*;
 
 pub fn reuse_heap(module: &Module) -> Result<Module, ReuseError> {
