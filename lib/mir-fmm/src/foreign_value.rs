0c9bb1c4ec5dc598e69e36485f50b5f29197c0ac
Yota Toyama <raviqqe@gmail.com>
2022-05-22 23:40:11 +0000

    Update

--- lib/mir-fmm/src/foreign_value.rs
+++ lib/mir-fmm/src/foreign_value.rs
@@ -1,4 +1,4 @@
-use crate::{type_, CompileError};
+use crate::{reference_count, type_, CompileError};
 use fnv::FnvHashMap;
 
 pub fn convert_to_foreign(
@@ -18,12 +18,12 @@ pub fn convert_to_foreign(
             } else if !type_::is_record_boxed(record_type, types)
                 && type_::is_foreign_record_boxed(record_type, types)
             {
-                box_(builder, value)
+                box_(builder, value)?
             } else {
                 return Err(CompileError::UnboxedForeignRecord);
             }
         }
-        mir::types::Type::Variant => box_(builder, value),
+        mir::types::Type::Variant => box_(builder, value)?,
         mir::types::Type::Boolean
         | mir::types::Type::ByteString
         | mir::types::Type::Function(_)
@@ -49,12 +49,12 @@ pub fn convert_from_foreign(
             } else if !type_::is_record_boxed(record_type, types)
                 && type_::is_foreign_record_boxed(record_type, types)
             {
-                unbox(builder, value)?
+                unbox(builder, value, type_, types)?
             } else {
                 return Err(CompileError::UnboxedForeignRecord);
             }
         }
-        mir::types::Type::Variant => unbox(builder, value)?,
+        mir::types::Type::Variant => unbox(builder, value, type_, types)?,
         mir::types::Type::Boolean
         | mir::types::Type::ByteString
         | mir::types::Type::Function(_)
@@ -66,21 +66,33 @@ pub fn convert_from_foreign(
 fn box_(
     builder: &fmm::build::InstructionBuilder,
     unboxed: fmm::build::TypedExpression,
-) -> fmm::build::TypedExpression {
-    let pointer = builder.allocate_heap(fmm::build::size_of(unboxed.type_().clone()));
+) -> Result<fmm::build::TypedExpression, CompileError> {
+    let pointer = reference_count::heap::allocate(builder, unboxed.type_().clone())?;
 
     builder.store(unboxed, pointer.clone());
 
-    pointer
+    Ok(pointer)
 }
 
 fn unbox(
     builder: &fmm::build::InstructionBuilder,
     pointer: fmm::build::TypedExpression,
+    type_: &mir::types::Type,
+    types: &FnvHashMap<String, mir::types::RecordBody>,
 ) -> Result<fmm::build::TypedExpression, CompileError> {
-    let unboxed = builder.load(pointer.clone())?;
+    let load = |builder: &fmm::build::InstructionBuilder| {
+        builder.load(fmm::build::bit_cast(
+            fmm::types::Pointer::new(type_::compile(type_, types)),
+            pointer.clone(),
+        ))
+    };
+    let unboxed = reference_count::clone(builder, &load(builder)?, type_, types)?;
 
-    builder.free_heap(pointer);
+    reference_count::pointer::drop(builder, &pointer, |builder| {
+        reference_count::drop(builder, &load(builder)?, type_, types)?;
+
+        Ok(())
+    })?;
 
     Ok(unboxed)
 }
