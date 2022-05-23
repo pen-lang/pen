a016bc4cc7cf0ad3433e8bb7f100b6c2c6939175
Yota Toyama <raviqqe@gmail.com>
2022-05-23 20:36:55 +0000

    Refactor

--- lib/mir-fmm/src/reference_count/record.rs
+++ lib/mir-fmm/src/reference_count/record.rs
@@ -6,77 +6,6 @@ use crate::{context::Context, record};
 
 const ARGUMENT_NAME: &str = "_record";
 
-pub fn clone_boxed(
-    builder: &fmm::build::InstructionBuilder,
-    record: &fmm::build::TypedExpression,
-) -> Result<fmm::build::TypedExpression, CompileError> {
-    pointer::clone(builder, record)
-}
-
-fn clone_unboxed(
-    context: &Context,
-    builder: &fmm::build::InstructionBuilder,
-    record: &fmm::build::TypedExpression,
-    record_type: &mir::types::Record,
-) -> Result<fmm::build::TypedExpression, CompileError> {
-    Ok(fmm::build::record(
-        context.types()[record_type.name()]
-            .fields()
-            .iter()
-            .enumerate()
-            .map(|(index, type_)| {
-                expression::clone(
-                    builder,
-                    &record::get_unboxed_field(builder, record, index)?,
-                    type_,
-                    context.types(),
-                )
-            })
-            .collect::<Result<_, _>>()?,
-    )
-    .into())
-}
-
-pub fn drop_boxed(
-    context: &Context,
-    builder: &fmm::build::InstructionBuilder,
-    record: &fmm::build::TypedExpression,
-    record_type: &mir::types::Record,
-) -> Result<(), CompileError> {
-    pointer::drop(builder, record, |builder| {
-        drop_unboxed(
-            context,
-            builder,
-            &record::load(context, builder, record, record_type)?,
-            record_type,
-        )
-    })?;
-
-    Ok(())
-}
-
-fn drop_unboxed(
-    context: &Context,
-    builder: &fmm::build::InstructionBuilder,
-    record: &fmm::build::TypedExpression,
-    record_type: &mir::types::Record,
-) -> Result<(), CompileError> {
-    for (index, type_) in context.types()[record_type.name()]
-        .fields()
-        .iter()
-        .enumerate()
-    {
-        expression::drop(
-            builder,
-            &record::get_unboxed_field(builder, record, index)?,
-            type_,
-            context.types(),
-        )?;
-    }
-
-    Ok(())
-}
-
 pub fn compile_clone_function(
     context: &Context,
     definition: &mir::ir::TypeDefinition,
@@ -177,6 +106,77 @@ pub fn compile_drop_or_reuse_function(
         fmm::types::CallingConvention::Target,
         fmm::ir::Linkage::Internal,
     )?;
+
+    Ok(())
+}
+
+fn clone_boxed(
+    builder: &fmm::build::InstructionBuilder,
+    record: &fmm::build::TypedExpression,
+) -> Result<fmm::build::TypedExpression, CompileError> {
+    pointer::clone(builder, record)
+}
+
+fn clone_unboxed(
+    context: &Context,
+    builder: &fmm::build::InstructionBuilder,
+    record: &fmm::build::TypedExpression,
+    record_type: &mir::types::Record,
+) -> Result<fmm::build::TypedExpression, CompileError> {
+    Ok(fmm::build::record(
+        context.types()[record_type.name()]
+            .fields()
+            .iter()
+            .enumerate()
+            .map(|(index, type_)| {
+                expression::clone(
+                    builder,
+                    &record::get_unboxed_field(builder, record, index)?,
+                    type_,
+                    context.types(),
+                )
+            })
+            .collect::<Result<_, _>>()?,
+    )
+    .into())
+}
+
+fn drop_boxed(
+    context: &Context,
+    builder: &fmm::build::InstructionBuilder,
+    record: &fmm::build::TypedExpression,
+    record_type: &mir::types::Record,
+) -> Result<(), CompileError> {
+    pointer::drop(builder, record, |builder| {
+        drop_unboxed(
+            context,
+            builder,
+            &record::load(context, builder, record, record_type)?,
+            record_type,
+        )
+    })?;
+
+    Ok(())
+}
+
+fn drop_unboxed(
+    context: &Context,
+    builder: &fmm::build::InstructionBuilder,
+    record: &fmm::build::TypedExpression,
+    record_type: &mir::types::Record,
+) -> Result<(), CompileError> {
+    for (index, type_) in context.types()[record_type.name()]
+        .fields()
+        .iter()
+        .enumerate()
+    {
+        expression::drop(
+            builder,
+            &record::get_unboxed_field(builder, record, index)?,
+            type_,
+            context.types(),
+        )?;
+    }
 
     Ok(())
 }
