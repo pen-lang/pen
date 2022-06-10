667dd96f8f0bb1bc46a6c5bac5e960778c7dd94b
Yota Toyama <raviqqe@gmail.com>
2022-06-10 08:55:07 -0700

    Follow up closure metadata  (#1074)
    * Update
    
    * Update metadata
    
    * Format
    
    * Update
    
    * Refactor
    
    * Refactor

--- lib/mir-fmm/src/closure/metadata.rs
+++ lib/mir-fmm/src/closure/metadata.rs
@@ -1,6 +1,15 @@
 use super::drop;
 use crate::{context::Context, reference_count, CompileError};
 
+// We do not need to compile closure metadata for thunks in the middle of
+// evaluation because of the following reasons.
+//
+// - While thunks are evaluated, evaluator threads keep references at least. So
+//   drop functions are never called.
+//   - Also, they are never expected to be dropped during evaluation.
+// - If thunks are already synchronized, we do not call synchronization
+//   functions in closure metadata.
+
 pub fn compile(
     context: &Context,
     definition: &mir::ir::FunctionDefinition,
