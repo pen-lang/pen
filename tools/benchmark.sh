47328b7fd5ffd2eb6fffe5028b42897168de2ed9
Yota Toyama <raviqqe@gmail.com>
2022-06-18 22:47:27 +0000

    Refactor

--- tools/benchmark.sh
+++ tools/benchmark.sh
@@ -4,24 +4,42 @@ set -e
 
 . $(dirname $0)/utilities.sh
 
+run_packages() {
+  for package_file in $(git ls-files | grep pen.json); do
+    (
+      directory=$(dirname $package_file)
+
+      echo package: $directory
+      cd $directory
+
+      "$@"
+    )
+  done
+}
+
+build() {
+  run_packages pen build
+
+  (
+    cd rust
+    cargo bench --no-run
+  )
+}
+
+run() {
+  run_packages hyperfine -w 3 ./app
+
+  (
+    cd rust
+    cargo bench -q
+  )
+}
+
 prepare_integration_test $(dirname $PWD/$0)/..
 
 cargo install hyperfine
 
 cd $(dirname $0)/../benchmark
 
-for package_file in $(git ls-files | grep pen.json); do
-  (
-    directory=$(dirname $package_file)
-
-    echo $directory
-    cd $directory
-
-    pen build
-
-    hyperfine -w 3 ./app
-  )
-done
-
-cd rust
-cargo bench
+build
+run
