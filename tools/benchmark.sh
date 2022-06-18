8ded2f60cc90872ceb0495a5d0226b98eebe1467
Yota Toyama <raviqqe@gmail.com>
2022-06-18 17:19:31 +0000

    Add benchmark workflow

---
+++ tools/benchmark.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -ex
+
+export PATH=$PWD/target/release:$PWD/tools:$PATH
+export RUSTC_WRAPPER=sccache
+export PEN_ROOT=$PWD
+
+cd $(dirname $0)/../benchmark
+
+for package_file in $(git ls-files | grep pen.json); do
+  (
+    cd $(dirname $package_file)
+    pen build
+    hyperfine ./app
+  )
+done
