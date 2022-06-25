ed58d883866471e196db63bd314fc9c2f34e5c7f
Yota Toyama <raviqqe@gmail.com>
2022-06-25 17:33:52 +0000

    Update

--- lib/hir/src/analysis/impossible_type_validator.rs
+++ lib/hir/src/analysis/impossible_type_validator.rs
@@ -1,6 +1,14 @@
 use super::AnalysisContext;
 use crate::{analysis::AnalysisError, ir::*};
 
-pub fn validate(_context: &AnalysisContext, _module: &Module) -> Result<(), AnalysisError> {
-    todo!()
+pub fn validate(_context: &AnalysisContext, module: &Module) -> Result<(), AnalysisError> {
+    for definition in module.type_definitions() {
+        validate_type_definition(definition)?;
+    }
+
+    Ok(())
+}
+
+fn validate_type_definition(_definition: &TypeDefinition) -> Result<(), AnalysisError> {
+    Ok(())
 }
