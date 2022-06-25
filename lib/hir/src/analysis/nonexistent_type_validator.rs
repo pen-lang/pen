034cfba1dd21d4c074ddc8dad61810c17a38d6f3
Yota Toyama <raviqqe@gmail.com>
2022-06-25 03:51:53 +0000

    Remove file

--- lib/hir/src/analysis/nonexistent_type_validator.rs
+++
@@ -1,6 +0,0 @@
-use super::AnalysisContext;
-use crate::{analysis::AnalysisError, ir::*};
-
-pub fn validate(_context: &AnalysisContext, _module: &Module) -> Result<(), AnalysisError> {
-    todo!()
-}
