7e00250e4afe9f2d4e01b06a269d2ddc70b6b604
Yota Toyama <raviqqe@gmail.com>
2022-06-25 01:42:53 +0000

    Update LLVM

--- tools/ci/github/setup.sh
+++ tools/ci/github/setup.sh
@@ -2,17 +2,11 @@
 
 set -e
 
-llvm_version=13
+llvm_version=14
 
-brew install jq mdbook ninja sccache
+brew install jq llvm@$llvm_version mdbook ninja sccache
 
-if [ $(uname) = Linux ]; then
-  curl -fsSL https://apt.llvm.org/llvm.sh | sudo bash /dev/stdin 14
-  llvm_prefix=/usr/lib/llvm-14
-else
-  brew install llvm@$llvm_version
-  llvm_prefix=$(brew --prefix)/opt/llvm@$llvm_version
-fi
+llvm_prefix=$(brew --prefix)/opt/llvm@$llvm_version
 
 echo LLVM_SYS_${llvm_version}0_PREFIX=$llvm_prefix >>$GITHUB_ENV
 echo PATH=$llvm_prefix/bin:$PATH >>$GITHUB_ENV
