ff430d5c8c59bbcc3552de4fff48a685b8be6dca
Yota Toyama <raviqqe@gmail.com>
2022-06-27 20:53:18 -0700

    Use pagetoc plugin (#1139)
    * Use pagetoc plugin
    
    * Use custom branch
    
    * Add todo
    
    * Fix spell check

--- tools/build_documents.sh
+++ tools/build_documents.sh
@@ -59,5 +59,9 @@ go run github.com/raviqqe/gherkin2markdown features doc/src/examples
 
 curl -fsSL https://pen-lang.s3.us-west-1.amazonaws.com/icon.svg >doc/src/favicon.svg
 
+# TODO Use the original crate after changes are merged.
+# spell-checker: disable-next-line
+cargo install --git https://github.com/raviqqe/mdbook-pagetoc --branch chore/hide-h5
+
 cd doc
 mdbook build
