a128aeb681730afc2d9b7264a7648cefb33e3c06
Yota Toyama <raviqqe@gmail.com>
2022-07-04 00:21:32 +0000

    Move prelude-test package

--- lib/hir/src/ir/built_in_call.rs
+++ lib/hir/src/ir/built_in_call.rs
@@ -1,5 +1,4 @@
-use super::built_in_function::BuiltInFunction;
-use super::expression::Expression;
+use super::{built_in_function::BuiltInFunction, expression::Expression};
 use crate::types::Type;
 use position::Position;
 
