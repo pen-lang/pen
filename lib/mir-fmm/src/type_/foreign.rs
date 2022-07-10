17edd0275f8cfc41c0781f8b334a8d538622483f
Yota Toyama <raviqqe@gmail.com>
2022-07-10 00:40:31 +0000

    Update comment

--- lib/mir-fmm/src/type_/foreign.rs
+++ lib/mir-fmm/src/type_/foreign.rs
@@ -55,8 +55,8 @@ fn is_record_boxed(
     record: &mir::types::Record,
     types: &FnvHashMap<String, mir::types::RecordBody>,
 ) -> bool {
-    // TODO Implement the full C calling convention and simply use the exactly the
-    // same strategy as native records.
+    // TODO Implement the full C calling convention and use the exactly the same
+    // strategy as native records.
     //
     // ```rust
     // type_::is_record_boxed(record, types)
