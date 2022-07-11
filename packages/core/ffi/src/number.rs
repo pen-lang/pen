47085a7a6fd6a205f052ffa5498ed6eab07a00fe
Yota Toyama <raviqqe@gmail.com>
2022-07-11 15:58:38 +0000

    Fix

--- packages/core/ffi/src/number.rs
+++ packages/core/ffi/src/number.rs
@@ -1,19 +1,49 @@
 use alloc::format;
 
 #[ffi::bindgen]
+fn _pen_core_absolute(number: ffi::Number) -> ffi::Number {
+    f64::from(number).absolute().into()
+}
+
+#[ffi::bindgen]
+fn _pen_core_ceil(number: ffi::Number) -> ffi::Number {
+    f64::from(number).ceil().into()
+}
+
+#[ffi::bindgen]
 fn _pen_core_convert_number_to_string(number: ffi::Number) -> ffi::ByteString {
     format!("{}", f64::from(number)).into()
 }
 
 #[ffi::bindgen]
-fn _pen_core_remainder(x: ffi::Number, y: ffi::Number) -> ffi::Number {
-    f64::from(x).rem_euclid(y.into()).into()
+fn _pen_core_floor(number: ffi::Number) -> ffi::Number {
+    f64::from(number).floor().into()
+}
+
+#[ffi::bindgen]
+fn _pen_core_infinity() -> ffi::Number {
+    f64::INFINITY.into()
+}
+
+#[ffi::bindgen]
+fn _pen_core_is_nan(x: ffi::Number) -> ffi::Boolean {
+    f64::from(x).is_nan().into()
+}
+
+#[ffi::bindgen]
+fn _pen_core_nan() -> ffi::Number {
+    f64::NAN.into()
 }
 
 #[ffi::bindgen]
 fn _pen_core_power(x: ffi::Number, y: ffi::Number) -> ffi::Number {
     // spell-checker: disable-next-line
     f64::from(x).powf(y.into()).into()
+}
+
+#[ffi::bindgen]
+fn _pen_core_remainder(x: ffi::Number, y: ffi::Number) -> ffi::Number {
+    f64::from(x).rem_euclid(y.into()).into()
 }
 
 #[ffi::bindgen]
@@ -24,31 +54,6 @@ fn _pen_core_round(number: ffi::Number) -> ffi::Number {
 #[ffi::bindgen]
 fn _pen_core_square_root(x: ffi::Number) -> ffi::Number {
     f64::from(x).sqrt().into()
-}
-
-#[ffi::bindgen]
-fn _pen_core_is_nan(x: ffi::Number) -> ffi::Boolean {
-    f64::from(x).is_nan().into()
-}
-
-#[ffi::bindgen]
-fn _pen_core_ceil(number: ffi::Number) -> ffi::Number {
-    f64::from(number).ceil().into()
-}
-
-#[ffi::bindgen]
-fn _pen_core_floor(number: ffi::Number) -> ffi::Number {
-    f64::from(number).floor().into()
-}
-
-#[ffi::bindgen]
-fn _pen_core_nan() -> ffi::Number {
-    f64::NAN.into()
-}
-
-#[ffi::bindgen]
-fn _pen_core_infinity() -> ffi::Number {
-    f64::INFINITY.into()
 }
 
 #[ffi::bindgen]
