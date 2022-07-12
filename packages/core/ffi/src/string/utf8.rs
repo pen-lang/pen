4691488262ce1b42d17a3983b185c95ea43f5c43
Yota Toyama <raviqqe@gmail.com>
2022-07-12 02:53:31 +0000

    Fix

--- packages/core/ffi/src/string/utf8.rs
+++ packages/core/ffi/src/string/utf8.rs
@@ -1,3 +1,4 @@
+use alloc::vec::Vec;
 use core::str;
 
 #[ffi::bindgen]
@@ -19,16 +20,14 @@ fn _pen_core_utf8_slice(
     let start = (f64::from(start) - 1.0).max(0.0) as usize;
     let end = f64::from(end).min(usize::MAX as f64) as usize;
 
-    let string = if let Ok(string) = str::from_utf8(string.as_slice()) {
-        string
+    if let Ok(string) = str::from_utf8(string.as_slice()) {
+        if string.is_empty() || start >= string.chars().count() || end <= start {
+            Default::default()
+        } else {
+            string[get_utf8_byte_index(string, start)..get_utf8_byte_index(string, end)].into()
+        }
     } else {
-        return ffi::ByteString::default();
-    };
-
-    if string.is_empty() || start >= string.chars().count() || end <= start {
-        ffi::ByteString::default()
-    } else {
-        string[get_utf8_byte_index(string, start)..get_utf8_byte_index(string, end)].into()
+        Default::default()
     }
 }
 
@@ -38,4 +37,71 @@ fn get_utf8_byte_index(string: &str, index: usize) -> usize {
         .nth(index)
         .map(|(index, _)| index)
         .unwrap_or_else(|| string.as_bytes().len())
+}
+
+#[ffi::bindgen]
+fn _pen_core_utf8_trim(string: ffi::ByteString) -> ffi::ByteString {
+    str::from_utf8(string.as_slice())
+        .unwrap_or("")
+        .trim()
+        .into()
+}
+
+#[ffi::bindgen]
+fn _pen_core_utf8_trim_end(string: ffi::ByteString) -> ffi::ByteString {
+    str::from_utf8(string.as_slice())
+        .unwrap_or("")
+        .trim_end()
+        .into()
+}
+
+#[ffi::bindgen]
+fn _pen_core_utf8_trim_end_match(
+    string: ffi::ByteString,
+    pattern: ffi::ByteString,
+) -> ffi::ByteString {
+    trim_match(string, pattern, |string, pattern| {
+        string.trim_end_matches(pattern)
+    })
+}
+
+#[ffi::bindgen]
+fn _pen_core_utf8_trim_match(string: ffi::ByteString, pattern: ffi::ByteString) -> ffi::ByteString {
+    trim_match(string, pattern, |string, pattern| {
+        string.trim_matches(&*pattern.chars().collect::<Vec<_>>())
+    })
+}
+
+#[ffi::bindgen]
+fn _pen_core_utf8_trim_start(string: ffi::ByteString) -> ffi::ByteString {
+    str::from_utf8(string.as_slice())
+        .unwrap_or("")
+        .trim_start()
+        .into()
+}
+
+#[ffi::bindgen]
+fn _pen_core_utf8_trim_start_match(
+    string: ffi::ByteString,
+    pattern: ffi::ByteString,
+) -> ffi::ByteString {
+    trim_match(string, pattern, |string, pattern| {
+        string.trim_start_matches(pattern)
+    })
+}
+
+fn trim_match(
+    string: ffi::ByteString,
+    pattern: ffi::ByteString,
+    callback: for<'a, 'b> fn(&'a str, &'b str) -> &'a str,
+) -> ffi::ByteString {
+    if let Ok(string) = str::from_utf8(string.as_slice()) {
+        if let Ok(pattern) = str::from_utf8(pattern.as_slice()) {
+            callback(string, pattern).into()
+        } else {
+            Default::default()
+        }
+    } else {
+        Default::default()
+    }
 }
