7c7d74f834e41854280094604bb272a9e143f3ce
Yota Toyama <raviqqe@gmail.com>
2022-07-23 05:31:34 +0000

    Fix concurrency.rs

--- packages/os/ffi/application/src/concurrency.rs
+++ packages/os/ffi/application/src/concurrency.rs
@@ -1,5 +1,5 @@
 use async_recursion::async_recursion;
-use futures::Stream;
+use futures::{pin_mut, Stream};
 use std::{future::Future, pin::Pin, sync::Arc};
 use tokio::{spawn, sync::RwLock, task::yield_now};
 use tokio_stream::{StreamExt, StreamMap};
@@ -14,45 +14,30 @@ async fn spawn_and_unwrap(future: impl Future<Output = ffi::Any> + Send + 'stati
 }
 
 #[ffi::bindgen]
-async fn _pen_race(list: ffi::Arc<ffi::List>) -> ffi::Arc<ffi::List> {
-    let mut streams = vec![];
-    let mut first_rest = unsafe { _pen_os_first_rest(list) };
+async fn _pen_yield() {
+    yield_now().await;
+}
 
-    while first_rest.ok {
+#[ffi::bindgen]
+async fn _pen_race(list: ffi::Arc<ffi::List>) -> ffi::Arc<ffi::List> {
+    let list = ffi::future::stream::from_list(list);
+
+    pin_mut!(list);
+
+    let mut streams = vec![];
+
+    while let Some(element) = list.next().await {
         streams.push((
             (),
-            Box::pin(convert_list_to_stream(unsafe {
-                _pen_os_to_list(ffi::future::from_closure(first_rest.first.clone()).await)
-            })),
+            Box::pin(ffi::future::stream::from_list(element.try_into().unwrap())),
         ));
-        first_rest = unsafe { _pen_os_first_rest(first_rest.rest.clone()) };
     }
 
     // TODO We should not wait for the first element to be ready.
-    convert_stream_to_list(tokio_stream::StreamExt::map(
-        StreamMap::from_iter(streams),
-        |(_, value)| value,
-    ))
+    convert_pinned_stream(Arc::new(RwLock::new(Box::pin(
+        StreamMap::from_iter(streams).map(|(_, value)| value),
+    ))))
     .await
-}
-
-// TODO Move this to `pen_ffi::stream::to_stream()`.
-fn convert_list_to_stream(list: ffi::Arc<ffi::List>) -> impl Stream<Item = ffi::Any> {
-    async_stream::stream! {
-        let mut first_rest = unsafe { _pen_os_first_rest(list) };
-
-        while first_rest.ok {
-            yield ffi::future::from_closure::<(), ffi::Any>(first_rest.first.clone()).await;
-            first_rest = unsafe { _pen_os_first_rest(first_rest.rest.clone()) };
-        }
-    }
-}
-
-// TODO Move this to `pen_ffi::stream::from_stream()`.
-async fn convert_stream_to_list(
-    stream: impl Stream<Item = ffi::Any> + Send + Sync + 'static,
-) -> ffi::Arc<ffi::List> {
-    convert_pinned_stream(Arc::new(RwLock::new(Box::pin(stream)))).await
 }
 
 #[async_recursion]
@@ -60,14 +45,8 @@ async fn convert_pinned_stream(
     stream: Arc<RwLock<Pin<Box<impl Stream<Item = ffi::Any> + Send + Sync + 'static>>>>,
 ) -> ffi::Arc<ffi::List> {
     if let Some(x) = stream.write().await.next().await {
-        let xs = ffi::future::to_closure(convert_pinned_stream(stream.clone()));
-        unsafe { _pen_os_create_list(x, xs) }
+        ffi::List::prepend(convert_pinned_stream(stream.clone()).await, x)
     } else {
-        unsafe { _pen_os_empty_list() }
+        ffi::List::new()
     }
-}
-
-#[ffi::bindgen]
-async fn _pen_yield() {
-    yield_now().await;
 }
