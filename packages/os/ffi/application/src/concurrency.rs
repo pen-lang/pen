a2e25a0d1695d9333cac2e9005f87e4cd6bb1649
Yota Toyama <raviqqe@gmail.com>
2022-07-23 21:16:11 +0000

    Refactor

--- packages/os/ffi/application/src/concurrency.rs
+++ packages/os/ffi/application/src/concurrency.rs
@@ -4,8 +4,7 @@ use tokio::{spawn, task::yield_now};
 #[ffi::bindgen]
 fn _pen_spawn(closure: ffi::Arc<ffi::Closure>) -> ffi::Arc<ffi::Closure> {
     ffi::future::to_closure(
-        spawn(async move { ffi::future::from_closure::<_, ffi::Any>(closure).await })
-            .map(Result::unwrap),
+        spawn(ffi::future::from_closure::<_, ffi::Any>(closure)).map(Result::unwrap),
     )
 }
 
