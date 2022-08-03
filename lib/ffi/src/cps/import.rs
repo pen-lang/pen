235222ef3264d2e71c2012dabeeac76bc6c367b1
Yota Toyama <raviqqe@gmail.com>
2022-08-03 16:05:43 +0000

    Disable warning

--- lib/ffi/src/cps/import.rs
+++ lib/ffi/src/cps/import.rs
@@ -7,6 +7,8 @@ macro_rules! import {
     };
     ($name:ident, async fn($($argument_name:ident: $argument_type:ty),* $(,)?) -> $result_type:ty $(,)?) => {
         extern "C" {
+            // It is fine for AsyncStack to be FFI-unsafe because the compiler touches only its Stack part.
+            #[allow(improper_ctypes)]
             fn $name(
                 stack: &mut $crate::cps::AsyncStack<$result_type>,
                 continue_: $crate::cps::ContinuationFunction<$result_type, $result_type>,
