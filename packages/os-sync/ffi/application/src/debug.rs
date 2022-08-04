686173f05251266844276fbefd5c8ac347c963d1
Yota Toyama <raviqqe@gmail.com>
2022-08-04 04:34:45 +0000

    Format files

--- packages/os-sync/ffi/application/src/debug.rs
+++ packages/os-sync/ffi/application/src/debug.rs
@@ -2,7 +2,7 @@ use std::str;
 
 #[no_mangle]
 extern "C" fn _pen_debug(message: ffi::ByteString) -> ffi::None {
-        eprintln!("{}", str::from_utf8(message.as_slice()).unwrap());
+    eprintln!("{}", str::from_utf8(message.as_slice()).unwrap());
 
     ffi::None::new()
 }
