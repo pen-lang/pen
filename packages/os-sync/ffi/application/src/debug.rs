e8b4460d6690a83a155e425337cf3c17c7adc537
Yota Toyama <raviqqe@gmail.com>
2022-08-04 04:19:51 +0000

    Disable PEN_DEBUG flag

--- packages/os-sync/ffi/application/src/debug.rs
+++ packages/os-sync/ffi/application/src/debug.rs
@@ -1,11 +1,8 @@
-use crate::utilities::is_debug;
 use std::str;
 
 #[no_mangle]
 extern "C" fn _pen_debug(message: ffi::ByteString) -> ffi::None {
-    if is_debug() {
         eprintln!("{}", str::from_utf8(message.as_slice()).unwrap());
-    }
 
     ffi::None::new()
 }
