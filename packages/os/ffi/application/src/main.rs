759eb85e44bab242947d71eb73de1f2f0e2be309
Yota Toyama <raviqqe@gmail.com>
2022-08-06 22:35:33 +0000

    Fix build

--- packages/os/ffi/application/src/main.rs
+++ packages/os/ffi/application/src/main.rs
@@ -7,19 +7,11 @@ mod utilities;
 use std::time::Duration;
 use tokio::time::sleep;
 
-#[cfg(not(test))]
 ffi::import!(_pen_main, async fn() -> ffi::None);
-
-#[cfg(test)]
-extern "C" fn _pen_main(
-    _: &mut ffi::cps::AsyncStack<ffi::None>,
-    _: ffi::cps::ContinuationFunction<ffi::None, ffi::None>,
-) {
-}
 
 #[tokio::main]
 async fn main() {
-    ffi::future::from_function(_pen_main).await;
+    _pen_main().await;
 
     // HACK Wait for all I/O buffers to be flushed (hopefully.)
     sleep(Duration::from_millis(50)).await;
