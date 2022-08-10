0cfc4f72530d87705f09ab8dd86e710748b6364e
Yota Toyama <raviqqe@gmail.com>
2022-08-10 01:51:51 +0000

    Fix encode text

--- packages/html/ffi/src/lib.rs
+++ packages/html/ffi/src/lib.rs
@@ -3,7 +3,7 @@
 use core::str;
 
 #[ffi::bindgen]
-fn _pen_html_escape_string(string: ffi::ByteString) -> ffi::ByteString {
+fn _pen_html_encode_text(string: ffi::ByteString) -> ffi::ByteString {
     html_escape::encode_text(str::from_utf8(string.as_slice()).unwrap_or_default())
         .as_bytes()
         .into()
