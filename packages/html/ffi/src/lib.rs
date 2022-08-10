428cd57c69b796ca978c8d5b2786ddf2f415a515
Yota Toyama <raviqqe@gmail.com>
2022-08-10 02:01:18 +0000

    Fix

--- packages/html/ffi/src/lib.rs
+++ packages/html/ffi/src/lib.rs
@@ -8,3 +8,10 @@ fn _pen_html_encode_text(string: ffi::ByteString) -> ffi::ByteString {
         .as_bytes()
         .into()
 }
+
+#[ffi::bindgen]
+fn _pen_html_encode_attribute(string: ffi::ByteString) -> ffi::ByteString {
+    html_escape::encode_quoted_attribute(str::from_utf8(string.as_slice()).unwrap_or_default())
+        .as_bytes()
+        .into()
+}
