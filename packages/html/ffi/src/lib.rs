f847416ec2cf4d43d422f8371be3e3ffd7d448b7
Yota Toyama <raviqqe@gmail.com>
2022-08-10 01:39:27 +0000

    Fix

---
+++ packages/html/ffi/src/lib.rs
@@ -0,0 +1,10 @@
+#![no_std]
+
+use core::str;
+
+#[ffi::bindgen]
+fn _pen_html_escape_string(string: ffi::ByteString) -> ffi::ByteString {
+    html_escape::encode_text(str::from_utf8(string.as_slice()).unwrap_or_default())
+        .as_bytes()
+        .into()
+}
