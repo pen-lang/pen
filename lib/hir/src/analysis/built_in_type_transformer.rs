12486147c645e6fbed89bc1fae1b7df950f98faa
Yota Toyama <raviqqe@gmail.com>
2022-08-12 15:23:53 +0000

    Fix bug

--- lib/hir/src/analysis/built_in_type_transformer.rs
+++ lib/hir/src/analysis/built_in_type_transformer.rs
@@ -3,42 +3,44 @@ use crate::{
     ir::*,
     types::{self, Type},
 };
-use fnv::FnvHashMap;
+use fnv::FnvHashSet;
 use position::Position;
 
 pub fn transform(module: &Module) -> Module {
-    let mut types = built_in_types();
-
     // This code should never be hit in the current implementation as all type
     // definitions' names have some kind of prefixes after conversion from AST
     // to HIR.
-    for definition in module.type_definitions() {
-        types.remove(definition.name());
-    }
+    let types = module
+        .type_definitions()
+        .iter()
+        .map(|definition| definition.name())
+        .chain(module.type_aliases().iter().map(|alias| alias.name()))
+        .collect::<FnvHashSet<_>>();
 
     type_transformer::transform(module, |type_| match type_ {
         Type::Reference(reference) => {
-            if let Some(type_) = types.get(reference.name()) {
+            if types.contains(reference.name()) {
                 type_.clone()
             } else {
-                reference.clone().into()
+                if let Some(type_) = built_in_type(reference.name(), reference.position()) {
+                    type_.clone()
+                } else {
+                    type_.clone()
+                }
             }
         }
         _ => type_.clone(),
     })
 }
 
-fn built_in_types() -> FnvHashMap<&'static str, Type> {
-    let position = Position::new("<built-in>", 1, 1, "");
-
-    [
-        ("any", types::Any::new(position.clone()).into()),
-        ("boolean", types::Boolean::new(position.clone()).into()),
-        ("error", types::Error::new(position.clone()).into()),
-        ("none", types::None::new(position.clone()).into()),
-        ("number", types::Number::new(position.clone()).into()),
-        ("string", types::ByteString::new(position.clone()).into()),
-    ]
-    .into_iter()
-    .collect()
+fn built_in_type(name: &str, position: &Position) -> Option<Type> {
+    Some(match name {
+        "any" => types::Any::new(position.clone()).into(),
+        "boolean" => types::Boolean::new(position.clone()).into(),
+        "error" => types::Error::new(position.clone()).into(),
+        "none" => types::None::new(position.clone()).into(),
+        "number" => types::Number::new(position.clone()).into(),
+        "string" => types::ByteString::new(position.clone()).into(),
+        _ => return None,
+    })
 }
