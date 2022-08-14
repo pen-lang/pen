f5b42439dba22aee83dd79c25b29d45e7d7f3ce1
Yota Toyama <raviqqe@gmail.com>
2022-08-14 23:49:54 +0000

    Use lambda lifting

--- lib/mir/src/analysis.rs
+++ lib/mir/src/analysis.rs
@@ -1,13 +1,12 @@
 mod environment_inference;
 mod free_variable;
-mod lambda_lifting;
+pub mod lambda_lifting;
 mod reference_count;
 mod type_check;
 mod variant_type_collection;
 
 pub use environment_inference::*;
 pub use free_variable::*;
-pub use lambda_lifting::*;
 pub use reference_count::*;
 pub use type_check::*;
 pub use variant_type_collection::*;
