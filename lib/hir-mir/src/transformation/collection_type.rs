2df95f75b191c6841ff15dbb7d3c3cd53ebf9e54
Yota Toyama <raviqqe@gmail.com>
2022-08-20 02:25:43 +0000

    Refactor type collection

--- lib/hir-mir/src/transformation/collection_type.rs
+++ lib/hir-mir/src/transformation/collection_type.rs
@@ -1,4 +1,9 @@
 use crate::{context::CompileContext, CompileError};
+use fnv::FnvHashSet;
+use hir::analysis::{
+    expression_visitor, type_canonicalizer, type_comparability_checker, type_visitor, AnalysisError,
+};
+use hir::ir::*;
 use hir::types::{self, Type};
 use position::Position;
 
@@ -27,4 +32,55 @@ pub fn transform_map_context(
         position.clone(),
     )
     .into())
+}
+
+pub fn collect_parameter_types(
+    context: &CompileContext,
+    module: &Module,
+) -> Result<FnvHashSet<Type>, AnalysisError> {
+    let mut types = FnvHashSet::default();
+
+    type_visitor::visit(module, |type_| match type_ {
+        Type::List(list_type) => {
+            types.insert(list_type.element());
+        }
+        Type::Map(map_type) => {
+            types.extend([map_type.key(), map_type.value()]);
+        }
+        _ => {}
+    });
+
+    expression_visitor::visit(module, |expression| match expression {
+        Expression::IfList(if_) => {
+            types.extend(if_.type_());
+        }
+        Expression::IfMap(if_) => {
+            types.extend([if_.key_type(), if_.value_type()].into_iter().flatten());
+        }
+        Expression::List(list) => {
+            types.insert(list.type_());
+        }
+        Expression::Map(map) => {
+            types.extend([map.key_type(), map.value_type()]);
+        }
+        _ => {}
+    });
+
+    Ok(types
+        .iter()
+        .map(|type_| {
+            let type_ = type_canonicalizer::canonicalize(type_, context.types())?;
+
+            Ok(
+                if type_comparability_checker::check(&type_, context.types(), context.records())? {
+                    Some(type_)
+                } else {
+                    None
+                },
+            )
+        })
+        .collect::<Result<FnvHashSet<_>, _>>()?
+        .into_iter()
+        .flatten()
+        .collect())
 }
--- lib/hir-mir/src/transformation/equal_operation/module.rs
+++ lib/hir-mir/src/transformation/equal_operation/module.rs
@@ -1,11 +1,6 @@
 use super::{function, operation};
-use crate::{context::CompileContext, error::CompileError};
-use fnv::FnvHashSet;
+use crate::{context::CompileContext, error::CompileError, transformation::collection_type};
 use hir::{
-    analysis::{
-        expression_visitor, type_canonicalizer, type_comparability_checker, type_visitor,
-        AnalysisError,
-    },
     ir::*,
     types::{self, Type},
 };
@@ -21,7 +16,7 @@ pub fn transform(context: &CompileContext, module: &Module) -> Result<Module, Co
             .iter()
             .cloned()
             .chain(
-                collect_parameter_types(context, module)?
+                collection_type::collect_parameter_types(context, module)?
                     .into_iter()
                     .map(|type_| compile_function_definition(context, &type_))
                     .collect::<Result<Vec<_>, _>>()?,
@@ -81,55 +76,4 @@ fn compile_function_definition(
         false,
         position.clone(),
     ))
-}
-
-fn collect_parameter_types(
-    context: &CompileContext,
-    module: &Module,
-) -> Result<FnvHashSet<Type>, AnalysisError> {
-    let mut types = FnvHashSet::default();
-
-    type_visitor::visit(module, |type_| match type_ {
-        Type::List(list_type) => {
-            types.insert(list_type.element());
-        }
-        Type::Map(map_type) => {
-            types.extend([map_type.key(), map_type.value()]);
-        }
-        _ => {}
-    });
-
-    expression_visitor::visit(module, |expression| match expression {
-        Expression::IfList(if_) => {
-            types.extend(if_.type_());
-        }
-        Expression::IfMap(if_) => {
-            types.extend([if_.key_type(), if_.value_type()].into_iter().flatten());
-        }
-        Expression::List(list) => {
-            types.insert(list.type_());
-        }
-        Expression::Map(map) => {
-            types.extend([map.key_type(), map.value_type()]);
-        }
-        _ => {}
-    });
-
-    Ok(types
-        .iter()
-        .map(|type_| {
-            let type_ = type_canonicalizer::canonicalize(type_, context.types())?;
-
-            Ok(
-                if type_comparability_checker::check(&type_, context.types(), context.records())? {
-                    Some(type_)
-                } else {
-                    None
-                },
-            )
-        })
-        .collect::<Result<FnvHashSet<_>, _>>()?
-        .into_iter()
-        .flatten()
-        .collect())
 }
