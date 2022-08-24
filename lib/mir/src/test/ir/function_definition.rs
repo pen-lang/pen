f92fe1aee5fbda71724ba8a63ba06163f8c5983e
Yota Toyama <raviqqe@gmail.com>
2022-08-24 15:18:26 +0000

    Add set_environment method

--- lib/mir/src/test/ir/function_definition.rs
+++ lib/mir/src/test/ir/function_definition.rs
@@ -1,6 +1,8 @@
 use crate::{ir::*, types::Type};
 
 pub trait FunctionDefinitionFake {
+    fn set_environment(&self, environment: Vec<Argument>) -> Self;
+
     fn fake_with_environment(
         name: impl Into<String>,
         environment: Vec<Argument>,
@@ -11,6 +13,17 @@ pub trait FunctionDefinitionFake {
 }
 
 impl FunctionDefinitionFake for FunctionDefinition {
+    fn set_environment(&self, environment: Vec<Argument>) -> Self {
+        Self::with_options(
+            self.name(),
+            environment,
+            self.arguments().to_vec(),
+            self.result_type().clone(),
+            self.body().clone(),
+            self.is_thunk(),
+        )
+    }
+
     fn fake_with_environment(
         name: impl Into<String>,
         environment: Vec<Argument>,
