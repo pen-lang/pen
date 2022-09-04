5f897ae7c112425242d0e53f3e77f27b838e365f
Yota Toyama <raviqqe@gmail.com>
2022-09-04 14:43:19 -0700

    Fix allocate variadic

--- lib/mir-fmm/src/reference_count/heap.rs
+++ lib/mir-fmm/src/reference_count/heap.rs
@@ -28,18 +28,22 @@ pub fn allocate_variadic(
     builder: &fmm::build::InstructionBuilder,
     length: impl Into<fmm::build::TypedExpression>,
 ) -> Result<fmm::build::TypedExpression, CompileError> {
+    let count_size = fmm::build::size_of(count::compile_type());
     let pointer = builder.allocate_heap(fmm::build::arithmetic_operation(
         fmm::ir::ArithmeticOperator::Add,
-        fmm::build::size_of(count::compile_type()),
+        count_size.clone(),
         length,
     )?);
 
     builder.store(
         count::compile_unique(),
-        fmm::build::record_address(pointer.clone(), 0)?,
+        fmm::build::bit_cast(
+            fmm::types::Pointer::new(count::compile_type()),
+            pointer.clone(),
+        ),
     );
 
-    Ok(fmm::build::record_address(pointer, 1)?.into())
+    Ok(fmm::build::pointer_address(pointer, count_size)?.into())
 }
 
 pub fn free(
