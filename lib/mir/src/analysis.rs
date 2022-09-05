925244d912bce301211ea9ce787d9cb9e055bb47
Yota Toyama <raviqqe@gmail.com>
2022-09-04 23:16:25 -0700

    Optimize string joins into batches (#1409)

--- lib/mir/src/analysis.rs
+++ lib/mir/src/analysis.rs
@@ -1,8 +1,10 @@
 pub mod alpha_conversion;
 pub mod environment_inference;
+mod expression_conversion;
 pub mod free_variable;
 pub mod lambda_lifting;
 pub mod normalization;
 pub mod reference_count;
+pub mod string_concatenation;
 pub mod type_check;
 pub mod variant_type_collection;
