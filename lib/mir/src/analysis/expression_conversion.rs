ebaa7a91a081fae404c5714e7d4892d440fd9f7f
Yota Toyama <raviqqe@gmail.com>
2022-09-25 20:44:42 -0700

    Fix expression conversion

--- lib/mir/src/analysis/expression_conversion.rs
+++ lib/mir/src/analysis/expression_conversion.rs
@@ -136,7 +136,12 @@ fn transform_expression(
             transform(operation.then()),
         )
         .into(),
-        Expression::TypeInformation(_) => todo!(),
+        Expression::TypeInformation(information) => TypeInformation::new(
+            information.types().to_vec(),
+            information.index(),
+            transform(information.variant()),
+        )
+        .into(),
         Expression::Variant(variant) => {
             Variant::new(variant.type_().clone(), transform(variant.payload())).into()
         }
