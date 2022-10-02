3c546d85344ad0bdca7dc8e4be136c33d2925bed
Yota Toyama <raviqqe@gmail.com>
2022-10-02 14:38:37 -0700

    Refactor type information

--- lib/mir/src/ir/type_information.rs
+++ lib/mir/src/ir/type_information.rs
@@ -3,26 +3,16 @@ use fnv::FnvHashMap;
 
 #[derive(Clone, Debug, Default, PartialEq, Eq)]
 pub struct TypeInformation {
-    types: Vec<types::Function>,
     information: FnvHashMap<Type, Vec<String>>,
     fallback: Vec<String>,
 }
 
 impl TypeInformation {
-    pub fn new(
-        types: Vec<types::Function>,
-        information: FnvHashMap<Type, Vec<String>>,
-        fallback: Vec<String>,
-    ) -> Self {
+    pub fn new(information: FnvHashMap<Type, Vec<String>>, fallback: Vec<String>) -> Self {
         Self {
-            types,
             information,
             fallback,
         }
-    }
-
-    pub fn types(&self) -> &[types::Function] {
-        &self.types
     }
 
     pub fn information(&self) -> &FnvHashMap<Type, Vec<String>> {
