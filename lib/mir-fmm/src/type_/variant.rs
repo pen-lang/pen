318c90b44465d20174ac7af4a9aa3206d9effd21
Yota Toyama <raviqqe@gmail.com>
2022-10-07 21:38:45 -0700

    Update

--- lib/mir-fmm/src/type_/variant.rs
+++ lib/mir-fmm/src/type_/variant.rs
@@ -37,10 +37,106 @@ pub fn is_payload_boxed(context: &Context, type_: &mir::types::Type) -> Result<b
 fn is_record_boxed(context: &Context, record: &mir::types::Record) -> bool {
     let body_type = &context.types()[record.name()];
 
-    // Variants always take two words.
     body_type.fields().len() > 1
-        || body_type
-            .fields()
-            .iter()
-            .any(|type_| matches!(type_, mir::types::Type::Variant))
+        || body_type.fields().iter().any(|type_| match type_ {
+            mir::types::Type::Record(record) => is_record_boxed(context, record),
+            // Variants always take two words.
+            mir::types::Type::Variant => true,
+            _ => false,
+        })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::configuration::CONFIGURATION;
+    use mir::test::ModuleFake;
+
+    mod record_box {
+        use super::*;
+
+        #[test]
+        fn check_empty_record() {
+            assert!(!is_record_boxed(
+                &Context::new(
+                    &mir::ir::Module::empty().set_type_definitions(vec![
+                        mir::ir::TypeDefinition::new("r", mir::types::RecordBody::new(vec![]))
+                    ]),
+                    CONFIGURATION.clone()
+                ),
+                &mir::types::Record::new("r")
+            ));
+        }
+
+        #[test]
+        fn check_record_with_field() {
+            assert!(!is_record_boxed(
+                &Context::new(
+                    &mir::ir::Module::empty().set_type_definitions(vec![
+                        mir::ir::TypeDefinition::new(
+                            "r",
+                            mir::types::RecordBody::new(vec![mir::types::Type::None])
+                        )
+                    ]),
+                    CONFIGURATION.clone()
+                ),
+                &mir::types::Record::new("r")
+            ));
+        }
+
+        #[test]
+        fn check_record_with_variant_field() {
+            assert!(is_record_boxed(
+                &Context::new(
+                    &mir::ir::Module::empty().set_type_definitions(vec![
+                        mir::ir::TypeDefinition::new(
+                            "r",
+                            mir::types::RecordBody::new(vec![mir::types::Type::Variant])
+                        )
+                    ]),
+                    CONFIGURATION.clone()
+                ),
+                &mir::types::Record::new("r")
+            ));
+        }
+
+        #[test]
+        fn check_record_with_field_of_record_with_variant_field() {
+            assert!(is_record_boxed(
+                &Context::new(
+                    &mir::ir::Module::empty().set_type_definitions(vec![
+                        mir::ir::TypeDefinition::new(
+                            "r",
+                            mir::types::RecordBody::new(vec![mir::types::Type::Variant])
+                        ),
+                        mir::ir::TypeDefinition::new(
+                            "s",
+                            mir::types::RecordBody::new(vec![mir::types::Record::new("r").into()])
+                        )
+                    ]),
+                    CONFIGURATION.clone()
+                ),
+                &mir::types::Record::new("s")
+            ));
+        }
+
+        #[test]
+        fn check_record_with_two_fields() {
+            assert!(is_record_boxed(
+                &Context::new(
+                    &mir::ir::Module::empty().set_type_definitions(vec![
+                        mir::ir::TypeDefinition::new(
+                            "r",
+                            mir::types::RecordBody::new(vec![
+                                mir::types::Type::None,
+                                mir::types::Type::None
+                            ])
+                        )
+                    ]),
+                    CONFIGURATION.clone()
+                ),
+                &mir::types::Record::new("r")
+            ));
+        }
+    }
 }
