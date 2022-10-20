0a2e4f9c61efdf129b17db38e5f6ef2e50fa28d6
Yota Toyama <raviqqe@gmail.com>
2022-10-19 17:46:39 -0700

    Fix

--- lib/mir/src/types/record.rs
+++ lib/mir/src/types/record.rs
@@ -1,11 +1,15 @@
+use std::sync::Arc;
+
 #[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Record {
-    name: String,
+    name: Arc<str>,
 }
 
 impl Record {
     pub fn new(name: impl Into<String>) -> Self {
-        Self { name: name.into() }
+        Self {
+            name: name.into().into(),
+        }
     }
 
     pub fn name(&self) -> &str {
