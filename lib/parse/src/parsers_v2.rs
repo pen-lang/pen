29b54a22bd7ad91d1941e4f509305e48b5d05175
Yota Toyama <raviqqe@gmail.com>
2022-10-30 16:02:30 -0700

    Update

--- lib/parse/src/parsers_v2.rs
+++ lib/parse/src/parsers_v2.rs
@@ -1,10 +1,29 @@
 use ast::*;
-use nom::IResult;
+use nom::{
+    bytes::complete::tag,
+    character::complete::{line_ending, none_of},
+    multi::many0,
+    IResult,
+};
+use nom_locate::LocatedSpan;
+use position::Position;
 
-struct Input<'a> {
-    string: &'a str,
-}
+type Input<'a> = LocatedSpan<&'a str, &'a str>;
 
 fn comment(input: Input) -> IResult<Input, Comment> {
-    let (input, foo) = input;
+    let position = position(input);
+
+    let (input, _) = tag("#")(input)?;
+    let (input, comment) = many0(none_of("\n\r"))(input)?;
+
+    Ok((input, Comment::new(String::from_iter(comment), position)))
+}
+
+fn position(input: Input) -> Position {
+    Position::new(
+        input.extra,
+        input.location_line() as usize,
+        input.get_column(),
+        String::from_utf8_lossy(input.get_line_beginning()),
+    )
 }
