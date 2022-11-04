4d222d26baeb4e05faadeea71d802ded67aae03e
Yota Toyama <raviqqe@gmail.com>
2022-11-03 20:14:07 -0700

    Refactor

--- packages/os/pen-link.sh
+++ packages/os/pen-link.sh
@@ -2,17 +2,6 @@
 
 set -e
 
-convert_library_path_to_flag() {
-  basename "$1" | sed 's/^lib\(.*\)\.a$/\1/'
-}
-
-print_lib_link_flags() {
-  for path in "$@"; do
-    echo "println!(\"cargo:rustc-link-lib=$(convert_library_path_to_flag "$path")\");"
-    echo "println!(\"cargo:rustc-link-search=$(dirname "$path")\");"
-  done
-}
-
 while getopts o:t: option; do
   case $option in
   o)
@@ -34,20 +23,9 @@ fi
 
 cd $(dirname $0)
 
-main_archive_path=$1
 shift
 
-ffi_directory=ffi/application
-
-cat <<EOF >$ffi_directory/build.rs
-fn main() {
-  println!("cargo:rustc-link-lib=static=$(convert_library_path_to_flag "$main_archive_path")");
-  println!("cargo:rustc-link-search=$(dirname "$main_archive_path")");
-  $(print_lib_link_flags "$@")
-}
-EOF
-
-cd $ffi_directory
+cd ffi/application
 
 cargo build --release --quiet $target_option
 
