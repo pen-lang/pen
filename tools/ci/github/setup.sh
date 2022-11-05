c2909da46324d0ce81edb84f53b915ca7e2b8ad5
Yota Toyama <raviqqe@gmail.com>
2022-11-05 15:02:39 -0700

    Revert "Revert "Revert "Fix macOS x86-64 on integration tests"""
    This reverts commit 1aa77161140d1bf1cf2971e6419dd68dc22b51a7.

--- tools/ci/github/setup.sh
+++ tools/ci/github/setup.sh
@@ -12,15 +12,8 @@ llvm_prefix=$(brew --prefix)/opt/llvm@$llvm_version
 echo LLVM_SYS_${llvm_version}0_PREFIX=$llvm_prefix >>$GITHUB_ENV
 echo PATH=$llvm_prefix/bin:$PATH >>$GITHUB_ENV
 
-case $(uname) in
-Darwin)
-  for component in rls; do
-    rustup component add $component --toolchain stable-x86_64-apple-darwin
-  done
-  ;;
-Linux)
+if [ $(uname) = Linux ]; then
   sudo apt update --fix-missing
   sudo apt install libc6-dbg # for valgrind
   brew install valgrind
-  ;;
-esac
+fi
