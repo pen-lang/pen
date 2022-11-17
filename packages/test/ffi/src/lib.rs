effcbf7b6a7c04aff570d7afedffdaae3c187034
Yota Toyama <raviqqe@gmail.com>
2022-11-16 19:15:14 -0800

    Fix

--- packages/test/ffi/src/lib.rs
+++ packages/test/ffi/src/lib.rs
@@ -2,8 +2,9 @@
 
 extern crate alloc;
 
-mod bit;
-mod character;
-mod number;
-mod string;
-mod view;
+use core::mem::transmute;
+
+#[ffi::bindgen]
+fn _pen_core_bit_and(x: ffi::Number, y: ffi::Number) -> ffi::Number {
+    unsafe { transmute(transmute::<_, u64>(x) & transmute::<_, u64>(y)) }
+}
