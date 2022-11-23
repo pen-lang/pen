e7ee25cd833fa89ca1ec68d1354b46302f1a62d0
Yota Toyama <raviqqe@gmail.com>
2022-11-22 19:12:03 -0800

    Add update-dependabot script

---
+++ tools/update-dependabot.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+set -e
+
+(
+  cat <<EOF
+version: 2
+updates:
+  - package-ecosystem: github-actions
+    directory: /
+    schedule:
+      interval: daily
+  - package-ecosystem: gomod
+    directory: /
+    schedule:
+      interval: daily
+  - package-ecosystem: bundler
+    directory: /
+    schedule:
+      interval: daily
+  - package-ecosystem: npm
+    directory: /doc
+    schedule:
+      interval: daily
+  - package-ecosystem: pip
+    directory: /doc
+    schedule:
+      interval: daily
+EOF
+
+  for file in $(git ls-files Cargo.lock '**/Cargo.lock'); do
+    cat <<EOF
+  - package-ecosystem: cargo
+    directory: $(dirname $file)
+    schedule:
+      interval: daily
+EOF
+  done
+) >$(dirname $0)/../.github/dependabot.yml
