2d4c526fd197f1117b5d8ff2b771883fb89091ab
Yota Toyama <raviqqe@gmail.com>
2022-11-22 19:28:23 -0800

    Freeze state after run

--- packages/test/ffi/src/lib.rs
+++ packages/test/ffi/src/lib.rs
@@ -1,42 +1,56 @@
 #![no_std]
 
 use alloc::sync::Arc;
+use core::sync::atomic::{AtomicBool, Ordering};
 use crossbeam::atomic::AtomicCell;
 
 extern crate alloc;
 
 #[repr(C)]
-pub struct State(ffi::Arc<ffi::Any>);
+struct State(ffi::Arc<ffi::Any>);
 
 #[ffi::any]
 #[derive(Clone)]
-struct StateInner {
-    cell: Arc<AtomicCell<ffi::Any>>,
+struct StateInner(Arc<StateInnerInner>);
+
+struct StateInnerInner {
+    cell: AtomicCell<ffi::Any>,
+    frozen: AtomicBool,
 }
 
 #[ffi::bindgen]
 fn _pen_test_state_new() -> State {
     State(ffi::Arc::new(
-        StateInner {
-            cell: AtomicCell::new(default_value()).into(),
-        }
+        StateInner(
+            StateInnerInner {
+                cell: AtomicCell::new(default_value()),
+                frozen: false.into(),
+            }
+            .into(),
+        )
         .into(),
     ))
 }
 
 #[ffi::bindgen]
-fn _pen_test_state_get(state: State) -> ffi::Any {
-    let inner: &StateInner = (&*state.0).try_into().unwrap();
-    let value = inner.cell.swap(default_value());
+fn _pen_test_state_freeze(state: State) {
+    let inner: &StateInner = state.0.as_ref().try_into().unwrap();
+    inner.0.frozen.store(true, Ordering::SeqCst);
+}
 
-    inner.cell.swap(value.clone());
+#[ffi::bindgen]
+fn _pen_test_state_get(state: State) -> ffi::Any {
+    let inner: &StateInner = state.0.as_ref().try_into().unwrap();
+    let value = inner.0.cell.swap(default_value());
+
+    inner.0.cell.swap(value.clone());
     value
 }
 
 #[ffi::bindgen]
 fn _pen_test_state_set(state: State, value: ffi::Any) {
-    let inner: &StateInner = (&*state.0).try_into().unwrap();
-    inner.cell.swap(value);
+    let inner: &StateInner = state.0.as_ref().try_into().unwrap();
+    inner.0.cell.swap(value);
 }
 
 fn default_value() -> ffi::Any {
