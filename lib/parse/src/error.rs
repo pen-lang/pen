de5d0743453b8d4b4ba365b2d195001413544ef9
Yota Toyama <raviqqe@gmail.com>
2023-01-31 09:34:46 +0800

    Fix more

--- lib/parse/src/error.rs
+++ lib/parse/src/error.rs
@@ -12,7 +12,7 @@ pub struct ParseError {
 }
 
 impl ParseError {
-    pub fn new<'a>(source: &str, path: &str, error: nom::Err<NomError<'a>>) -> Self {
+    pub fn new(source: &str, path: &str, error: nom::Err<NomError<'_>>) -> Self {
         match error {
             nom::Err::Incomplete(_) => Self::unexpected_end(source, path),
             nom::Err::Error(error) | nom::Err::Failure(error) => {
