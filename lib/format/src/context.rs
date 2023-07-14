c35512c7cc1d0a08adecb34c18d0118a52b59fee
Yota Toyama <raviqqe@gmail.com>
2023-07-14 19:07:04 +1000

    Fix

--- lib/format/src/context.rs
+++ lib/format/src/context.rs
@@ -1,44 +1,46 @@
 use ast::Comment;
+use std::collections::VecDeque;
 
-pub struct Context {
-    comments: Vec<Comment>,
+pub struct Context<'a> {
+    comments: VecDeque<&'a Comment>,
 }
 
-impl Context {
-    pub fn new(mut comments: Vec<Comment>) -> Self {
-        comments.sort_by_key(|comment| comment.position().line_number());
-
-        Self { comments }
+impl<'a> Context<'a> {
+    pub fn new(mut comments: &[Comment]) -> Self {
+        Self {
+            comments: comments.iter().collect(),
+        }
     }
 
     pub fn drain_comments_before(
         &mut self,
         line_number: usize,
-    ) -> impl Iterator<Item = Comment> + '_ {
-        // This is O(n) and slow. Ha ha!
-        self.comments.splice(
+    ) -> impl Iterator<Item = &'a Comment> + '_ {
+        self.comments.drain(
             ..self
                 .comments
                 .iter()
                 .position(|comment| comment.position().line_number() >= line_number)
                 .unwrap_or(self.comments.len()),
-            [],
         )
     }
 
     pub fn drain_current_comment(
         &mut self,
         line_number: usize,
-    ) -> impl Iterator<Item = Comment> + '_ {
+    ) -> impl Iterator<Item = &'a Comment> + '_ {
         self.drain_comments_before(line_number + 1)
     }
 
-    pub fn peek_comments_before(&self, line_number: usize) -> impl Iterator<Item = &Comment> {
-        self.comments[..self
-            .comments
-            .iter()
-            .position(|comment| comment.position().line_number() >= line_number)
-            .unwrap_or(self.comments.len())]
-            .iter()
+    pub fn peek_comments_before(&self, line_number: usize) -> impl Iterator<Item = &'a Comment> {
+        self.comments
+            .range(
+                ..self
+                    .comments
+                    .iter()
+                    .position(|comment| comment.position().line_number() >= line_number)
+                    .unwrap_or(self.comments.len()),
+            )
+            .copied()
     }
 }
