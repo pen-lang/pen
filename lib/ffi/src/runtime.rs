89d814f5a945b56e2858e74648bc0137fc37b8dd
Yota Toyama <raviqqe@gmail.com>
2023-08-26 14:49:18 +1000

    Fix

--- lib/ffi/src/runtime.rs
+++ lib/ffi/src/runtime.rs
@@ -1,9 +1,10 @@
+use crate::Error;
 use std::sync::RwLock;
 use tokio::runtime::Handle;
 
 static HANDLE: RwLock<Option<tokio::runtime::Handle>> = RwLock::new(None);
 
-pub fn set_handle(handle: Handle) -> Result<(), RwLockError> {
+pub fn set_handle(handle: Handle) -> Result<(), Error> {
     HANDLE.write()?.replace(handle);
 
     Ok(())
