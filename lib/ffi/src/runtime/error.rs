225e1b43755774b09b19461c193fb681a0c70ceb
Yota Toyama <raviqqe@gmail.com>
2023-08-26 15:10:21 +1000

    Fix

--- lib/ffi/src/runtime/error.rs
+++ lib/ffi/src/runtime/error.rs
@@ -13,7 +13,7 @@ impl Display for RuntimeError {
     fn fmt(&self, formatter: &mut Formatter) -> fmt::Result {
         match self {
             Self::HandleNotInitialized => write!(formatter, "runtime handle not initialized"),
-            Self::HandleLockPoisoned => write!(formatter, "handle lock poisoned"),
+            Self::HandleLockPoisoned => write!(formatter, "runtime handle lock poisoned"),
         }
     }
 }
