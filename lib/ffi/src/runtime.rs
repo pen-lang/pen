7e1d811d1f65e5eb2fb868ef16ccde2f1101563b
Yota Toyama <raviqqe@gmail.com>
2023-08-26 14:46:33 +1000

    Fix

--- lib/ffi/src/runtime.rs
+++ lib/ffi/src/runtime.rs
@@ -3,8 +3,10 @@ use tokio::runtime::Handle;
 
 static HANDLE: RwLock<Option<tokio::runtime::Handle>> = RwLock::new(None);
 
-pub fn set_handle(handle: Handle) {
-    HANDLE.write().unwrap().replace(handle);
+pub fn set_handle(handle: Handle) -> Result<(), RwLockError> {
+    HANDLE.write()?.replace(handle);
+
+    Ok(())
 }
 
 pub fn handle() -> Handle {
