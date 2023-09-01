06369f0ef14941c48a7a439e16900bffddd46e01
Yota Toyama <raviqqe@gmail.com>
2023-09-01 10:25:03 +1000

    Revert

--- lib/ffi/src/runtime.rs
+++ lib/ffi/src/runtime.rs
@@ -5,8 +5,7 @@ use error::RuntimeError;
 use std::sync::RwLock;
 use tokio::runtime::Handle;
 
-#[no_mangle]
-static _PEN_FFI_RUNTIME_HANDLE: RwLock<Option<tokio::runtime::Handle>> = RwLock::new(None);
+static RUNTIME_HANDLE: RwLock<Option<tokio::runtime::Handle>> = RwLock::new(None);
 
 pub fn set_handle(handle: Handle) -> Result<(), Error> {
     _PEN_FFI_RUNTIME_HANDLE
