d5f331e8bd6ce8d4d97bef2e6fae88f1d3a1bc86
Yota Toyama <raviqqe@gmail.com>
2023-10-06 12:38:44 +1100

    Fix PartialOrd implementation of Position

--- lib/position/src/position.rs
+++ lib/position/src/position.rs
@@ -78,8 +78,8 @@ impl Ord for Position {
 }
 
 impl PartialOrd for Position {
-    fn partial_cmp(&self, _: &Self) -> Option<Ordering> {
-        Some(Ordering::Equal)
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
     }
 }
 
