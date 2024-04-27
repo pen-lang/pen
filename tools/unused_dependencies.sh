db204e2183813e3adedce6c203c784a2f6cb965b
Yota Toyama <raviqqe@gmail.com>
2024-04-27 18:58:50 +0900

    Use `cargo machete` (#2377)

--- tools/unused_dependencies.sh
+++ tools/unused_dependencies.sh
@@ -4,7 +4,6 @@ set -e
 
 . $(dirname $0)/utilities.sh
 
-rustup install nightly
-rustup run nightly cargo install cargo-udeps
+cargo install cargo-machete
 
-$(dirname $0)/run_all_crates.sh rustup run nightly cargo udeps
+$(dirname $0)/run_all_crates.sh cargo machete
