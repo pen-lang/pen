a5e583fb36be751fa7655e46520846989af5723a
Yota Toyama <raviqqe@gmail.com>
2024-06-20 00:56:41 +0900

    Fix

--- packages/core/ffi/src/bit.rs
+++ packages/core/ffi/src/bit.rs
@@ -2,32 +2,32 @@ use core::mem::transmute;
 
 #[ffi::bindgen]
 fn _pen_core_bit_and(x: ffi::Number, y: ffi::Number) -> ffi::Number {
-    unsafe { transmute(transmute::<_, u64>(x) & transmute::<_, u64>(y)) }
+    unsafe { transmute(transmute::<ffi::Number, u64>(x) & transmute::<_, u64>(y)) }
 }
 
 #[ffi::bindgen]
 fn _pen_core_bit_or(x: ffi::Number, y: ffi::Number) -> ffi::Number {
-    unsafe { transmute(transmute::<_, u64>(x) | transmute::<_, u64>(y)) }
+    unsafe { transmute(transmute::<ffi::Number, u64>(x) | transmute::<_, u64>(y)) }
 }
 
 #[ffi::bindgen]
 fn _pen_core_bit_xor(x: ffi::Number, y: ffi::Number) -> ffi::Number {
-    unsafe { transmute(transmute::<_, u64>(x) ^ transmute::<_, u64>(y)) }
+    unsafe { transmute(transmute::<ffi::Number, u64>(x) ^ transmute::<_, u64>(y)) }
 }
 
 #[ffi::bindgen]
 fn _pen_core_bit_not(x: ffi::Number) -> ffi::Number {
-    unsafe { transmute(!transmute::<_, u64>(x)) }
+    unsafe { transmute(!transmute::<ffi::Number, u64>(x)) }
 }
 
 #[ffi::bindgen]
 fn _pen_core_bit_left_shift(x: ffi::Number, count: ffi::Number) -> ffi::Number {
-    unsafe { transmute(transmute::<_, u64>(x) << (f64::from(count) as u64)) }
+    unsafe { transmute(transmute::<ffi::Number, u64>(x) << (f64::from(count) as u64)) }
 }
 
 #[ffi::bindgen]
 fn _pen_core_bit_right_shift(x: ffi::Number, count: ffi::Number) -> ffi::Number {
-    unsafe { transmute(transmute::<_, u64>(x) >> (f64::from(count) as u64)) }
+    unsafe { transmute(transmute::<ffi::Number, u64>(x) >> (f64::from(count) as u64)) }
 }
 
 #[ffi::bindgen]
@@ -37,7 +37,7 @@ fn _pen_core_bit_to_integer_64(x: ffi::Number) -> ffi::Number {
 
 #[ffi::bindgen]
 fn _pen_core_bit_from_integer_64(x: ffi::Number) -> ffi::Number {
-    (unsafe { transmute::<_, u64>(x) } as f64).into()
+    (unsafe { transmute::<ffi::Number, u64>(x) } as f64).into()
 }
 
 #[cfg(test)]
