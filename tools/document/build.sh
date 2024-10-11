d4dd657e49a145b4106bdcc282419bf34ec66bbe
Yota Toyama <raviqqe@gmail.com>
2024-10-11 20:09:21 +0900

    Fix

--- tools/document/build.sh
+++ tools/document/build.sh
@@ -85,7 +85,6 @@ curl -fsSL https://pen-lang.s3.us-west-1.amazonaws.com/icon.svg >doc/docs/icon.s
   npm install
   npm run build
 
-  pip3 --version
   pip3 install --break-system-packages --user -r requirements.txt
 
   mkdocs build
