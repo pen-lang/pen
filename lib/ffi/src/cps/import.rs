666786cec76cae846865bdc7eddd59d6cd5ee36f
Yota Toyama <raviqqe@gmail.com>
2025-02-22 10:21:03 +0900

    Fix

--- lib/ffi/src/cps/import.rs
+++ lib/ffi/src/cps/import.rs
@@ -1,13 +1,13 @@
 #[macro_export]
 macro_rules! import {
     ($name:ident, fn($($argument_name:ident: $argument_type:ty),* $(,)?) -> $result_type:ty $(,)?) => {
-        extern "C" {
+        unsafe extern "C" {
             fn $name($($argument_name: $argument_type),*) -> $result_type;
         }
     };
     ($name:ident, async fn($($argument_name:ident: $argument_type:ty),* $(,)?) -> $result_type:ty $(,)?) => {
         async fn $name($($argument_name: $argument_type),*) -> $result_type {
-            extern "C" {
+            unsafe extern "C" {
                 // It is fine for AsyncStack to be FFI-unsafe because the compiler touches only its Stack part.
                 #[allow(improper_ctypes)]
                 fn $name(
