787c55e7a148de4425fcf6465758f1c07142b2b2
Yota Toyama <raviqqe@gmail.com>
2026-03-28 01:12:53 -0700

    Fix

--- tools/document/build.sh
+++ tools/document/build.sh
@@ -81,7 +81,7 @@ go run github.com/raviqqe/gherkin2markdown features doc/docs/examples
   cd doc
 
   pnpm install
-  pnpm run build
+  pnpm build
 
   pip3 install --break-system-packages --user -r requirements.txt
 
